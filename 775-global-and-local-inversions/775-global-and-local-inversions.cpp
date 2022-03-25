802687262913ebb62e2295fe161d0989d9b6cbd6
Sameer <sameer.barha12@gmail.com>
2022-03-25 18:17:48 +0530

    Time: 184 ms (57.64%), Space: 86.9 MB (14.74%) - LeetHub

--- 775-global-and-local-inversions/775-global-and-local-inversions.cpp
+++ 775-global-and-local-inversions/775-global-and-local-inversions.cpp
@@ -1,11 +1,54 @@
 class Solution {
 public:
-    bool isIdealPermutation(vector<int>& A) {
-        for (int i = 0; i < A.size(); i++) {
-            if (abs(A[i] - i) > 1) {
-                return false;
-            }
-        }
-        return true;
+    
+    void updateBIT(vector<int> &bit, int idx){
+	while(idx < bit.size()){
+		bit[idx] +=1;
+		idx += idx & (-idx);
+	}
+}
+
+int query(vector<int> &bit, int idx){
+
+	int sum =0;
+	while(idx>0){
+		sum += bit[idx];
+		idx -= idx & (-idx);
+	}
+
+	return sum;
+}
+    
+    bool isIdealPermutation(vector<int>& a) {
+        
+        	if(a.size() == 0)
+		return true;
+
+	int local_inversion =0;
+	for(int i=0; i<a.size()-1; i++)
+		if(a[i]>a[i+1])
+			local_inversion++;
+
+
+	vector<int> bit(a.size()+1, 0);
+
+	//1st element just update, no need to query
+	//using element as index since arrayis in the range 0-N
+	updateBIT(bit, a[0]+1);
+	int max_range_limit = a[0]+1;
+
+	long int global_inversion = 0;
+	for(int i=1; i<a.size(); i++){
+
+		updateBIT(bit, a[i]+1);
+		max_range_limit = max(max_range_limit, a[i]+1);
+
+		global_inversion += query(bit, max_range_limit) - query(bit, a[i]+1);
+	}
+
+	//cout<<local_inversion<<" * "<<global_inversion<<endl;
+	return (local_inversion==global_inversion);
+        
+        
     }
 };
