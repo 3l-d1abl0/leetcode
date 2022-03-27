db10eb05514a6dbf40adefe40915e235176c72be
Sameer <sameer.barha12@gmail.com>
2022-03-27 18:40:59 +0530

    Time: 4 ms (69.54%), Space: 7.5 MB (74.04%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,41 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        
-                int N = nums.size();
+     void subsetsRecursion(int idx, vector<int> &nums, vector<int> &ans, vector<vector<int>> &res){
 
-        unordered_set<string> subsets_string;
+        res.push_back(ans);
 
-        vector<vector<int>> res;
-        
-        //to maintain the order of keys
+                if (nums.size() == idx)
+            return;
+
+
+        for(int i=idx; i<nums.size(); i++){
+
+            if(i>idx && nums[i]==nums[i-1]) continue;
+
+            ans.push_back(nums[i]);
+            subsetsRecursion(i+1, nums, ans, res);
+            ans.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int> &nums)
+    {
+
         sort(nums.begin(), nums.end());
 
-        for (int i = 0; i < (1 << N); i++)
-        {
+        vector<vector<int>> res;
+        vector<int> ans;
 
-            vector<int> vec;
-            string key;
-
-            for (int j = 0; j < N; j++)
-            {
-
-                if (i & (1 << j))
-                {
-                    vec.push_back(nums[j]);
-                    key += to_string(nums[j]) + "-";
-                }
-            }
-
-            if (subsets_string.find(key) == subsets_string.end())
-            {
-                res.push_back(vec);
-                subsets_string.insert(key);
-            }
-        }
+        subsetsRecursion(0, nums, ans, res);
 
         return res;
     }
-        
     
 };
