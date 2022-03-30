a318cd741df48989f3ac9f65af4ecab359141059
Sameer <sameer.barha12@gmail.com>
2022-03-30 19:40:18 +0530

    Added solution - LeetHub

--- Largest number in K swaps - GFG/largest-number-in-k-swaps.cpp
+++ Largest number in K swaps - GFG/largest-number-in-k-swaps.cpp
@@ -8,39 +8,59 @@ using namespace std;
 
 class Solution
 {
-   public:
-   string s;
-   void answer(string curr,int k,int i){
-       if(k==0 || i==s.size()){
-           if(s<curr){
-               s=curr;
-           }
-           return;
-       }
-       
-       for(int j=i;j<curr.size();++j){
-           if(curr[j]<=curr[i] && i!=j){
-               continue;
-           }
-           
-           swap(curr[j],curr[i]);
-           // saving the swap when i=j;
-           if(i==j){
-               answer(curr,k,i+1);
-           }
-           answer(curr,k-1,i+1);
-           swap(curr[j],curr[i]);
-       }
-       return ;
-       
-   }
-   //Function to find the largest number after k swaps.
-   string findMaximumNum(string str, int k)
-   {
-      s=str;
-      answer(str,k,0);
-      return s;
-   }
+    public:
+    
+    void calcMaxNumAfterSwap(int idx, string number, int k, string &maxString, int N)
+    {
+
+        if (k == 0)
+            return;
+
+        char charMax = number[idx];
+
+        // Find the largest character to swap
+        for (int i = idx + 1; i < N; i++)
+        {
+            if (number[i] > charMax)
+            {
+                charMax = number[i];
+            }
+        }
+
+        if (charMax != number[idx])
+            k--;
+
+        for (int i = N - 1; i >= idx; i--)
+        {
+
+            if (charMax == number[i])
+            {
+                swap(number[idx], number[i]);
+
+                if (number.compare(maxString) > 0)
+                {
+                    maxString = number;
+                    // cout << "Maxx" << maxString << endl;
+                }
+
+                calcMaxNumAfterSwap(idx + 1, number, k , maxString, N);
+                swap(number[idx], number[i]);
+            }
+        }
+    }
+
+    // Function to find the largest number after k swaps.
+    string findMaximumNum(string str, int k)
+    {
+        // code here.
+
+        int size = str.length();
+        string maxString = str;
+        calcMaxNumAfterSwap(0, str, k, maxString, size);
+
+        return maxString;
+    }
+    
 };
 
 // { Driver Code Starts.
