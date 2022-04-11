53b09abf54dcedfdd0f3e350ea1d7f54973c5ceb
Sameer <sameer.barha12@gmail.com>
2022-04-11 20:46:45 +0530

    Time: 486 ms (5.01%), Space: 23.9 MB (20.45%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,79 @@
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+     
+        
+        int N = grid.size();
+        int M = grid[0].size();
+        
+        
+        for(int i=0; i<N; i++){
+            
+            for(int j=0; j<M; j++){
+                    
+                
+                if((i==0 || j==0 || i==N-1 || j==M-1) && (grid[i][j]==1) ){
+                   
+                    dfs(i, j, grid);
+                    
+                }
+                
+            }
+            
+        }//for
+        
+        
+        int ctr =0;
+        
+        for(int i=1; i<N-1; i++){
+            for(int j=1; j<M-1; j++){
+                
+                if(grid[i][j]==1){
+                    ctr++;
+                    
+                }
+                
+            }
+        }
+        
+        
+        printdp(grid);
+        return ctr;
+        
+    }
+    
+    void dfs(int i, int j, vector<vector<int>> &grid){
+        
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]==0)
+            return;
+        
+        
+        grid[i][j] = 0;
+        
+        
+        dfs(i-1, j, grid);
+        dfs(i, j+1, grid);
+        dfs(i+1, j, grid);
+        dfs(i, j-1, grid);
+                    
+                    
+                    
+                    
+        
+        
+        
+    }
+    
+    void printdp(vector<vector<int>> &grid){
+        
+        
+        for(auto row: grid){
+            for(int ele: row){
+                cout<<ele<<" ";
+            }
+            
+            cout<<endl;
+        }
+    }
+    
+};
