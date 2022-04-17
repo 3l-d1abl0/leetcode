93dd3a4e6ae59111da9b95468350ad4ea10b9fd2
Sameer <sameer.barha12@gmail.com>
2022-04-17 20:15:41 +0530

    Time: 20 ms (88.92%), Space: 13.8 MB (67.74%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,85 +1,69 @@
 class Solution {
+    //Graph coloring: 0->not visited...1->visited...2->visited & processed
+    bool detectCycle_util(vector<vector<int>>& adj,vector<int>& visited,int v)
+    {
+        if(visited[v]==1)
+            return true;
+        if(visited[v]==2)
+            return false;
+        
+        visited[v]=1;   //Mark current as visited
+        for(int i=0;i<adj[v].size();++i)
+            if(detectCycle_util(adj,visited,adj[v][i]))
+                return true;
+        
+        visited[v]=2;   //Mark current node as processed
+        return false;
+    }
+    
+    //Cycle detection
+    bool detectCycle(vector<vector<int>>& adj,int n)
+    {
+        vector<int> visited(n,0);
+        for(int i=0;i<n;++i)
+            if(!visited[i])
+                if(detectCycle_util(adj,visited,i))
+                    return true;
+        return false;
+    }
+    
+    //Topological sort
+    void dfs(vector<vector<int>>& adj,int v,vector<bool>& visited,stack<int>& mystack)
+    {
+        visited[v] = true;
+        for(int i=0;i<adj[v].size();++i)
+            if(!visited[adj[v][i]])
+                dfs(adj,adj[v][i],visited,mystack);
+        
+        mystack.push(v);
+    }
 public:
-    
-    void DFS(int src, vector<vector<int>> &adj, stack<int> &st, vector<bool> &vis){
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int n=prerequisites.size();
+        vector<vector<int>> adj(numCourses);
+        //Make adjacecncy list
+        for(int i=0;i<n;++i)
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         
+        //Detect CYCLE...If present then return empty array
+        vector<int> ans;
+        if(detectCycle(adj,numCourses))
+            return ans;        
         
-        vis[src] = true;
+        //Find toposort and store it in stack
+        stack<int> mystack;
+        vector<bool> visited(numCourses,false);
         
-        //Process the dependent courses
-        for(int node: adj[src]){
-            
-            if(!vis[node]){
-                DFS(node, adj, st, vis);
-            }
-            
+        //Apply DFS and find topological sort
+        for(int i=0;i<numCourses;++i)
+            if(!visited[i])
+                dfs(adj,i,visited,mystack);
+        
+        while(!mystack.empty())
+        {
+            ans.push_back(mystack.top());
+            mystack.pop();
         }
-        
-        
-        st.push(src);
-        
+        return ans;
     }
-    
-    vector<int> findOrder(int N, vector<vector<int>>& pre) {
-     
-        
-        vector<vector<int>> adj(N);
-        
-        for(auto ele: pre){
-            adj[ele[1]].push_back(ele[0]);
-            
-        }
-        
-        
-        stack<int> st;
-        vector<bool> vis(N);
-        
-        
-        for(int i=0; i<N; i++){
-            if(!vis[i]) 
-                DFS(i, adj, st, vis);
-        }
-        
-        
-        return cyclic(st, adj, N);
-    }
-    
-    vector<int> cyclic(stack<int> &st, vector<vector<int>> &adj, int n){
-        
-        	unordered_map<int, int> pos;
-	        int index = 0;
-        
-            vector<int> topo;
-        
-        
-            while(!st.empty()){
-                
-                pos[st.top()] = index;
-                
-                topo.push_back(st.top());
-                
-                st.pop();
-                
-                index++;
-                
-            }
-        
-        
-            
-        for(int par=0; par<n; par++){
-            
-            for(int node: adj[par]){
-                if(pos[par]> pos[node])
-                    return vector<int> {};
-            }
-            
-        }
-        
-        
-        //not a cycle
-        return topo;
-        
-        
-    }
-    
 };
