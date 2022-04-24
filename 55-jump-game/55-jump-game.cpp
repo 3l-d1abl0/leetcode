5ba28e0f9c48e96afda325fd6ffe36890f1530b7
Sameer <sameer.barha12@gmail.com>
2022-04-24 21:42:31 +0530

    Time: 96 ms (46.71%), Space: 48.3 MB (75.99%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -26,9 +26,26 @@ public:
         return dp[idx];
     }
     
+    bool singleLoop(vector<int>&nums){
+        
+        int idx =0;
+        int maxPos =0;
+        
+        while(idx<=maxPos){
+            maxPos = max(maxPos, idx + nums[idx]);
+            if(maxPos >= nums.size()-1)   return true;
+            idx++;
+        }
+        
+        return false;
+    }
+    
     bool canJump(vector<int>& nums) {
         
-        vector<int> dp(nums.size(), -1);
-        return jump(nums, 0, dp)==1;
+       // vector<int> dp(nums.size(), -1);
+        //return jump(nums, 0, dp)==1;
+        
+        return singleLoop(nums);
+        
     }
 };
