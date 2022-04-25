2d263d1e094f0c3f296ac72f23ee8b2f1cfa23d6
Sameer <sameer.barha12@gmail.com>
2022-04-25 14:11:30 +0530

    Time: 255 ms (38.65%), Space: 17.7 MB (11.61%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -18,7 +18,9 @@ public:
         /*vector<int> dp(nums.size(), 1e4+10);    //max possible jump - linear
         return f(nums, 0, dp);*/
         
-        return f1(nums);
+        //return f1(nums);
+        
+        return bfs(nums);
         
     }
     
@@ -43,6 +45,43 @@ public:
         
         
         return dp[0];
+    }
+    
+    int bfs(vector<int> &nums){
+        
+        queue<int> q;
+        
+        int N = nums.size();
+        
+        vector<int> dist(N, 1e4+10);
+        
+        q.push(0);
+        dist[0] = 0;
+        
+        int steps =0;
+        while(!q.empty()){
+                int len = q.size();
+                
+            for(int i=0; i<len; i++){
+                int node = q.front(); q.pop();
+                
+                if(node==N-1) return steps;
+                
+                for(int j=1; j<=nums[node]; j++){
+                    if(j+node >= N)
+                            continue;
+                    if(dist[j+node]>steps){
+                        q.push(j+node);
+                        dist[j+node] = steps;
+                    }
+                }
+            }
+            
+            steps++;
+            
+        }//while
+        
+        return steps;
     }
     
 };
