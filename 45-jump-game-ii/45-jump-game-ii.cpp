d12b86f484953dfb6d9eb5156911dc9c522c2692
Sameer <sameer.barha12@gmail.com>
2022-04-25 12:51:46 +0530

    Time: 600 ms (15.83%), Space: 17.6 MB (14.11%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -13,22 +13,36 @@ public:
         return dp[idx];
     }
     
-    /*int jump(vector<int>& nums) {
-        
-        vector<int> dp(nums.size(), 1e4+10);    //max possible jump - linear
-        return f(nums, 0, dp);
-        
-    }*/
-    
-    
     int jump(vector<int>& nums) {
-	int n = size(nums);
-	vector<int> dp(n, 10001);
-	dp[n - 1] = 0;  // start from last index. No jumps required to reach end if we are already here
-	// same as above. For each index, explore all jump sizes and use the one requiring minimum jumps to reach end
-	for(int i = n - 2; i >= 0; i--) 
-		for(int jumpLen = 1; jumpLen <= nums[i]; jumpLen++) 
-			dp[i] = min(dp[i], 1 + dp[min(n - 1, i + jumpLen)]);  // min(n-1, i + jumpLen) for bounds handling
-	return dp[0];
-}
+        
+        /*vector<int> dp(nums.size(), 1e4+10);    //max possible jump - linear
+        return f(nums, 0, dp);*/
+        
+        return f1(nums);
+        
+    }
+    
+    int f1(vector<int> nums){
+        
+        int N = nums.size();
+        
+        vector<int> dp(N, 1e4+10);
+        
+        dp[N-1] = 0;    //0 steps to reach n-1 from n-1
+        
+        for(int i= N-2; i>=0; i--){     //from n-2 to 0
+            
+            
+            for(int j=1; j<= nums[i]; j++){
+                
+                
+                dp[i] = min(dp[i], 1+ dp[min(N-1, i+j)]);
+                
+            }
+        }
+        
+        
+        return dp[0];
+    }
+    
 };
