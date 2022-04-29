cc91576f86d0103d6aa3ba2b8d54e80e56a9c7d5
Sameer <sameer.barha12@gmail.com>
2022-04-29 21:05:07 +0530

    Time: 7 ms (74.62%), Space: 13 MB (52.38%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -10,12 +10,13 @@ public:
             while(buy < days - 1 && prices[buy + 1] < prices[buy])
                 buy++; // find the valley of prices               
             
-            sell = buy;
+            sell = buy+1;
             
             while(sell < days - 1 && prices[sell + 1] > prices[sell])
                 sell++; // find the peak of prices
             
-            profit += prices[sell] - prices[buy];
+            if(sell<days)//i.e if bot buy ans sell are valid index. ifnore if only Buy
+                profit += prices[sell] - prices[buy];
                 
             buy = sell + 1;
         }
