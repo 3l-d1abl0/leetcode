84f33b7ac5848af9ee4ad64b08578b8a0f486340
Sameer <sameer.barha12@gmail.com>
2022-04-30 22:57:53 +0530

    Time: 706 ms (21.06%), Space: 207.2 MB (17.11%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,8 +1,51 @@
 class Solution {
 public:
+        int maxProfit3(vector<int>& stocks) {
+        
+        
+        int N = stocks.size();
+        
+        vector<vector<vector<int>>> dp(N+1, vector<vector<int>> (2, vector<int> (3, 0)));
+        //buy - 1 sell -0
+        //dp[N][0][0] = 0;
+        //  ...
+        //dp[N][1][1] =0
+        
+        for(int i=N-1; i>=0; i--){
+            
+            
+            for(int buy=0; buy<=1; buy++){
+                
+                for(int tx=1; tx<=2; tx++){
+                    
+                    int profit =0;
+
+                    if(buy==1){
+
+                        profit = max(-stocks[i]+ dp[i+1][0][tx], 0+ dp[i+1][1][tx]);
+
+                    }else{
+                        profit = max(stocks[i]+ dp[i+1][1][tx-1], 0 + dp[i+1][0][tx]);
+                    }
+                
+                    dp[i][buy][tx] = profit;
+                }
+                
+                
+            }
+            
+        }
+        
+        //printDP(dp);
+        
+        return dp[0][1][2];
+    }
+    
     int maxProfit(vector<int>& stocks) {
 
-
+        
+        return maxProfit3(stocks);
+        
 	    int N = stocks.size();
 	    vector<vector<int>> dp(3, vector<int> (N,0));
 
