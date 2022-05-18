2bd8ad282c8e9dbd3e4381becb0bd6cd7bc1b283
Sameer <sameer.barha12@gmail.com>
2022-05-18 18:22:29 +0530

    Time: 59 ms (91.22%), Space: 14.4 MB (66.51%) - LeetHub

--- 1115-print-foobar-alternately/1115-print-foobar-alternately.py
+++ 1115-print-foobar-alternately/1115-print-foobar-alternately.py
@@ -1,37 +1,23 @@
-from threading import Condition
-
+from threading import Lock
 class FooBar:
     def __init__(self, n):
         self.n = n
-        self.val = "F"
-        self.cv = Condition()
+        self.foo_lock = Lock()
+        self.bar_lock = Lock()
+        self.bar_lock.acquire()
 
 
     def foo(self, printFoo: 'Callable[[], None]') -> None:
-        
         for i in range(self.n):
-            
+            self.foo_lock.acquire()
             # printFoo() outputs "foo". Do not change or remove this line.
-            self.cv.acquire()
-            while self.val != "F":
-                self.cv.wait()
-            
             printFoo()
-            self.val = "B"
-            self.cv.notify(1)
-            self.cv.release()
+            self.bar_lock.release()
 
 
     def bar(self, printBar: 'Callable[[], None]') -> None:
-        
         for i in range(self.n):
-            
+            self.bar_lock.acquire()
             # printBar() outputs "bar". Do not change or remove this line.
-            self.cv.acquire()
-            while self.val != "B":
-                self.cv.wait()
             printBar()
-            self.val = "F"
-            self.cv.notify(1)
-            self.cv.release()
-        	
+            self.foo_lock.release()
