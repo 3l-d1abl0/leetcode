c64365305b1b15c8222020ff14f96d5234b9b625
Sameer <sameer.barha12@gmail.com>
2022-05-23 17:10:21 +0530

    Added solution - LeetHub

--- Ways To Tile A Floor - GFG/ways-to-tile-a-floor.cpp
+++ Ways To Tile A Floor - GFG/ways-to-tile-a-floor.cpp
@@ -9,12 +9,23 @@ class Solution {
       int mod=1e9+7;
     long long numberOfWays(long long N) {
         //code here
-        
+        /*
         if(N<=1) return 1;
         
         if(dp[N]!=0) return dp[N];
         
         return dp[N] = (numberOfWays(N-1)+numberOfWays(N-2))%mod;
+        */
+        
+        for(int i=0; i<=1; i++)
+            dp[i] = 1;
+            
+        for(int i=2; i<=N; i++){
+            dp[i] = (dp[i-1]+dp[i-2])%mod;
+        }
+        
+        
+        return dp[N];
     }
 };
 
