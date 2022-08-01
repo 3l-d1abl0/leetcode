d634c35e75bfca8405cd8bdcc92e62db70fbe5a1
Sameer <sameer.barha12@gmail.com>
2022-08-01 18:51:23 +0530

    Time: 97 ms (20.86%), Space: 45.3 MB (14.61%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,41 +1,62 @@
 class Solution {
-public:
-    
-    void printQ(priority_queue<int, vector<int>, greater<int> > pq){
+    private:
+    //Randomized Quick Select - gives better performance than normal quick select
+    int partition(vector<int> &arr, int st, int ed){
+
+        int n= ed-st+1;
+        int piv = rand() % n;
         
-        while(!pq.empty()){
-            
-            cout<<pq.top()<<" "; pq.pop();
+        swap(arr[st + piv], arr[st]);
+        int partIdx = st;
+
+        int l = st, r = ed;
+
+        while(l<r){
+
+            while(l<=ed && arr[l]<=arr[partIdx]) l++;
+
+            while(r>=st && arr[r]>arr[partIdx]) r--;
+
+            if(l<r)
+                swap(arr[l],arr[r]);
+        }
+
+
+        swap(arr[partIdx], arr[r]);
+
+
+        return r;
+
+    }
+
+    int kthLargest(vector<int> &arr, int k, int st, int ed){
+
+        int n = arr.size()-1;
+        int kth = n-(k-1);
+        while(st<=ed){
+
+            int idx = partition(arr, st, ed);
+
+            if(idx ==kth){
+                return arr[idx];
+            }else if(idx <kth ){
+                return kthLargest(arr, k, idx+1, ed);
+            }else{
+                return kthLargest(arr, k, st, idx-1);
+            }
+
         }
         
-        cout<<endl;
+        
+        return -1;
     }
     
+public:
     int findKthLargest(vector<int>& nums, int k) {
         
-        //min-Heap with maintained size K
-        priority_queue<int, vector<int>, greater<int> > pq;
         
-        for(vector<int>::iterator it = nums.begin(); it != nums.end(); it++){
-            
-            if(pq.size() < k){
-                pq.push(*it);
-            }else{
-                
-                if(*it > pq.top()){
-                    
-                    pq.pop();
-                    pq.push(*it);
-                    
-                    
-                   // printQ(pq);
-                }
-            }
-            
-        }//for
-        
-        
-        return pq.top();
+        int n = nums.size();
+        return kthLargest(nums, k, 0, n-1);
         
     }
 };
