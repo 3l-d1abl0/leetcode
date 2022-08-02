5b294856103492d9ad5c28e0d7c544190499b74f
Sameer <sameer.barha12@gmail.com>
2022-08-02 10:57:37 +0530

    Time: 22 ms (67.31%), Space: 13.6 MB (72.70%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -6,19 +6,38 @@ public:
         if (!nums.size()) return res;
         unordered_map<int, int> cnt;
         for (auto num : nums) cnt[num]++;
-        vector<vector<int>> bucket(nums.size() + 1);
+        vector<pair<int, int>> num_with_cnt;
         for (auto kv : cnt) {
-            bucket[kv.second].push_back(kv.first);
+            num_with_cnt.push_back({kv.first, kv.second});
         }
+        kselection(num_with_cnt, 0, num_with_cnt.size()-1, k);
+        for (int i = 0; i < k && i < num_with_cnt.size(); ++i) {
+            res.push_back(num_with_cnt[i].first);
+        }
+        return res;
+    }
 
-        for (int i = bucket.size() - 1; i >= 0; --i) {
-            for (int j = 0; j < bucket[i].size(); ++j){
-                res.push_back(bucket[i][j]);
-                if (res.size() == k) return res;
+    void kselection(vector<pair<int, int>>& data, int start, int end, int k) {
+
+        if (start >= end) return;
+        auto pv = data[end];
+        int i = start;
+        int j = start;
+        while (i < end) {
+            if (data[i].second > pv.second) {
+                swap(data[i++], data[j++]);
+            } else {
+                ++i;
             }
         }
-
-        return res;
+        swap(data[j], data[end]);
+        int num = j - start + 1;
+        if (num == k) return;
+        else if (num < k) {
+            kselection(data, j + 1, end, k - num);
+        } else {
+            kselection(data, start, j - 1, k);
+        }
         
     }
 };
