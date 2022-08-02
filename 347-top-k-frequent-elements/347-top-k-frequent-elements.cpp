5af2e0d38fd47c643d30d510fcfdaf7c91ecf580
Sameer <sameer.barha12@gmail.com>
2022-08-02 14:06:00 +0530

    Time: 27 ms (46.10%), Space: 13.8 MB (43.48%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,18 +1,47 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        
+        vector<int> res;
+        if (!nums.size()) return res;
         unordered_map<int, int> cnt;
         for (auto num : nums) cnt[num]++;
+        vector<pair<int, int>> num_with_cnt;
         for (auto kv : cnt) {
-            pq.push({kv.second, kv.first});
-            while (pq.size() > k) pq.pop();
+            num_with_cnt.push_back({kv.first, kv.second});
         }
-        vector<int> res;
-        while (!pq.empty()) {
-            res.push_back(pq.top().second);
-            pq.pop();
+        kselection(num_with_cnt, 0, num_with_cnt.size()-1, k);
+        for (int i = 0; i < k && i < num_with_cnt.size(); ++i) {
+            res.push_back(num_with_cnt[i].first);
         }
         return res;
+    }
+
+    void kselection(vector<pair<int, int>>& data, int start, int end, int k) {
+
+        if (start >= end) return;
+        
+        int rng = (rand() % (end-start+1) ) + start;
+        swap(data[rng], data[end]);
+        
+        auto pv = data[end];
+        int i = start;
+        int j = start;
+        while (i < end) {
+            if (data[i].second > pv.second) {
+                swap(data[i++], data[j++]);
+            } else {
+                ++i;
+            }
+        }
+        swap(data[j], data[end]);
+        int num = j - start + 1;
+        if (num == k) return;
+        else if (num < k) {
+            kselection(data, j + 1, end, k - num);
+        } else {
+            kselection(data, start, j - 1, k);
+        }
+        
     }
 };
