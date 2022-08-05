c48d11d695ba3e0fb21e1861c1a8ba6b3a4d92f1
Sameer <sameer.barha12@gmail.com>
2022-08-05 14:57:28 +0530

    Time: 48 ms (58.62%), Space: 10.6 MB (35.46%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,15 +1,71 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& nums) {
-        long long res = 0;
-        for (int i = 0; i < nums.size(); ++i) {
-            int sm = nums[i], lg = nums[i];
-            for (int j = i + 1; j < nums.size(); ++j) {
-                sm = min(sm, nums[j]);
-                lg = max(lg, nums[j]);     
-                res += lg - sm;
+    
+    struct Arr{
+        int lmin, rmin;
+        int lmax, rmax;
+    };
+    
+    void getSmallest(vector<int> &nums, struct Arr *ans){
+        
+        stack<int> st;
+        
+        for(int i=0; i<nums.size(); i++){
+            while(!st.empty() && nums[st.top()] > nums[i]){
+                ans[st.top()].rmin = i- st.top();
+                st.pop();
             }
+
+            if(!st.empty())
+                ans[i].lmin = i - st.top();
+                //ans[i].lmin = i+1;
+           // else
+                
+            st.push(i);
         }
-        return res;
+        
+    }
+    
+    
+    void getGreatest(vector<int> &nums, struct Arr *ans){
+        
+        stack<int> st;
+        
+        for(int i=0; i<nums.size(); i++){
+            while(!st.empty() && nums[st.top()] < nums[i]){
+                ans[st.top()].rmax = i- st.top();
+                st.pop();
+            }
+
+            if(!st.empty())
+                ans[i].lmax = i - st.top();
+                //ans[i].lmin = i+1;
+           // else
+                
+            st.push(i);
+        }
+    }
+    
+    long long subArrayRanges(vector<int>& nums) {
+        
+        //vector<Arr> ans(nums.size());
+        Arr ans[nums.size()];
+        
+        for(int i=0; i<nums.size(); i++){
+            ans[i].lmin = ans[i].lmax =(i+1);
+            ans[i].rmin = ans[i].rmax = nums.size()-i;
+        }
+        
+        getSmallest(nums, ans);
+        getGreatest(nums, ans);
+        
+        long int diff =0;
+        for(int i=0; i<nums.size(); i++){
+            
+                diff -= (long int)nums[i]*ans[i].lmin*ans[i].rmin;
+                diff += (long int)nums[i]*ans[i].lmax*ans[i].rmax;
+        }
+        
+        return diff;
     }
 };
