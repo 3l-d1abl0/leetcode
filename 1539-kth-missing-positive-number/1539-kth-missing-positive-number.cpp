0b2a1fb62ab6fda65a06c7f5e5ff3e53c6b914ac
Sameer <sameer.barha12@gmail.com>
2022-08-06 19:16:59 +0530

    Time: 4 ms (84.62%), Space: 9.6 MB (66.81%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int findKthPositive(vector<int>& arr, int k) {
+    /*int findKthPositive(vector<int>& arr, int k) {
         
         unordered_set<int> st;
         for(int ele: arr){
@@ -18,5 +18,19 @@ public:
         
         
         return -1;
+    }*/
+    
+     int findKthPositive(vector<int>& A, int k) {
+       int low = 0;
+       int high = A.size()-1 ;
+       int mid;
+       while (low <= high) {
+            mid = (low + high) / 2;
+            if (A[mid] - (1 + mid) < k)  //A[m]-(m+1)   --> This gives umber of missing number before m'th index
+                low = mid + 1;
+            else
+                high = mid-1;
+        }
+        return low + k;
     }
 };
