80da346b1310ed12f9e659fb320b939ad93bfc1d
Sameer <sameer.barha12@gmail.com>
2022-08-07 11:42:43 +0530

    Time: 392 ms (25.00%), Space: 83.7 MB (100.00%) - LeetHub

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
@@ -14,9 +14,23 @@ public:
         return ans;
     }
     
+    
+    long long method2(vector<int> &nums){
+        
+        unordered_map<int, int> mp;
+        long long cnt =0;
+        for(int i=0; i<nums.size(); i++){
+            cnt+= mp[nums[i]-i];
+            mp[nums[i]-i]++;
+        }
+        
+        return 1LL*nums.size()*(nums.size()-1)/2 - cnt;
+    }
+    
     long long countBadPairs(vector<int>& nums) {
         
         
-        return method1(nums);
+        //return method1(nums);
+        return method2(nums);
     }
 };
