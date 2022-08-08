f27b75c1d861494c7d7822873d7370674d09af0e
Sameer <sameer.barha12@gmail.com>
2022-08-08 22:47:54 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (43.46%) - LeetHub

---
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        
+        int N = s.size();
+        
+        vector<bool> processed(26, false);
+        vector<int> lastIndex(26, -1);
+        
+        for(int i=0; i<N; i++){
+            lastIndex[s[i]-'a'] = i;
+        }
+        
+        stack<char> st;
+        for(int i=0; i<N; i++){
+            
+            if(processed[s[i]-'a']) continue;
+
+            while(!st.empty() && st.top()> s[i]){
+                if(lastIndex[st.top()-'a'] > i){    //if the stack top  exists later in the string
+                    //process it later
+                    processed[st.top()-'a'] = false;
+                    st.pop();          
+                }else
+                    break;
+            }
+            
+                processed[s[i]-'a'] = true;
+                st.push(s[i]);
+            
+        }//for
+        
+        
+        //return "aqs";
+        
+        string ans ="";
+        while(!st.empty()){
+            ans +=st.top();
+            st.pop();
+        }
+        
+        
+        reverse(ans.begin(), ans.end());
+        
+        
+        
+        //cout<<ans<<endl;
+        return ans;
+    }
+};
