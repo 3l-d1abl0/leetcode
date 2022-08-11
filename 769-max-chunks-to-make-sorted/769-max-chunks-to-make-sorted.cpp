2692059c7df9825e6ba06bbb029b3774d6957251
Sameer <sameer.barha12@gmail.com>
2022-08-11 19:54:09 +0530

    Time: 4 ms (29.97%), Space: 7.3 MB (28.38%) - LeetHub

--- 769-max-chunks-to-make-sorted/769-max-chunks-to-make-sorted.cpp
+++ 769-max-chunks-to-make-sorted/769-max-chunks-to-make-sorted.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
-    int maxChunksToSorted(vector<int>& nums) {
+    
+    int method2(vector<int> &nums){
         
-        /*
+        //https://leetcode.com/problems/max-chunks-to-make-sorted/discuss/595709/Monotonic-stack-solution-with-detailed-explanation
+        
         int ctr =0;
         stack<int> st;
         
-        for(int i= nums.size()-1; i>=0; i--){
+        int largest;
+        for(int i= 0; i<nums.size(); i++){
             
-            while(!st.empty() && nums[i] < st.top()){
-                ctr++;
+            largest = nums[i];
+            
+            while(!st.empty() && st.top() > nums[i]){
+                largest  = max(largest, st.top());
                 st.pop();
             }
             
-            st.push(nums[i]);
+            st.push(largest);
             
         }
             
-        if(!st.empty()) ctr++;
-            
-            
-        return ctr;*/
+        return st.size();
+        
+        
+    }
+    
+    int maxChunksToSorted(vector<int>& nums) {
+        
+        return method2(nums);
         
         int maxx = INT_MIN;
         int chunk =0;
