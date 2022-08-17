8d3119c456e513d1d31d8edabc1ad672d3db5bea
Sameer <sameer.barha12@gmail.com>
2022-08-17 12:04:42 +0530

    Time: 272 ms (44.40%), Space: 22.7 MB (44.74%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -18,8 +18,7 @@ public:
         
     }
     
-    
-    string longestPalindrome(string s) {
+    string method1(string &s){
         
         int maxLen =0;
         int beg = 0;
@@ -27,14 +26,66 @@ public:
         
         for(int i=0; i<s.size(); i++){
             
-            expandCorners(s, i, i, maxLen, beg);
+            expandCorners(s, i, i, maxLen, beg);//odd length
             
-            expandCorners(s, i, i+1, maxLen, beg);
+            expandCorners(s, i, i+1, maxLen, beg);//even length
         }
         
         //cout<<beg<<" "<<maxLen<<endl;
         
         return s.substr(beg, maxLen);
+        
+    }
+    
+    string method2(string &str){
+        
+        int N = str.size();
+        
+        vector<vector<bool>> dp(N, vector<bool> (N, false));
+        
+        int beg=0, maxLen = 1;
+        
+        //len 1
+        for(int i=0; i<N; i++){
+            dp[i][i] =1;
+        }
+        
+        //len 2
+        for(int i=0; i<N-1; i++){
+
+            if(str[i]==str[i+1]){            
+                dp[i][i+1] = 1;
+                beg=i;
+                maxLen=2;
+            }
+        }
+        
+        //len 3 onwards
+        
+        for(int i=3; i<=N; i++){
+            
+            for(int j=0; j<=N-i; j++){
+                
+                if(str[j]==str[j+i-1] && dp[j+1][j+i-2]){
+                    dp[j][j+i-1] = true;
+                    maxLen = i;
+                    beg=j;
+                }
+                
+            }
+        }
+        
+        return str.substr(beg, maxLen);
+    }
+    
+    string longestPalindrome(string s) {
+        
+        ///expand around corners O(n^2)
+        //return method1(s);
+        
+        
+        //DP o(n^2)
+        return method2(s);
         
     }
 };
