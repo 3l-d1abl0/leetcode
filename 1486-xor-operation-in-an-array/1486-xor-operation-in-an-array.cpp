65054a3fc3cc3209878021c0a9b9bbe6df494568
Sameer <sameer.barha12@gmail.com>
2022-09-07 15:24:30 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (78.70%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -12,9 +12,39 @@ public:
         return ans;
     }
     
+    int method2(int n, int start){
+        
+        int last = start + 2*(n-1);
+        
+        if(start%4 <=1){
+            
+            if(n%4 ==0)
+                return 0;
+            else if(n%4==1)
+                return last;
+            else if(n%4==2)
+                return 2;
+                
+            return last^2;  //==3
+            
+        }else{
+            
+            if(n%4 ==0)
+                return last^start^2;
+            else if(n%4==1)
+                return start;
+            else if(n%4==2)
+                return last^start;
+                
+            return start^2;//==3
+            
+        }
+    }
+    
     int xorOperation(int n, int start) {
         
         
-        return method1(n, start);
+        //return method1(n, start);
+        return method2(n, start);
     }
 };
