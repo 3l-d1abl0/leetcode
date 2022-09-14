d27af1ea121548ef469198c50e51e181be6c9830
Sameer <sameer.barha12@gmail.com>
2022-09-14 20:37:01 +0530

    Time: 55 ms (27.22%), Space: 7.5 MB (44.21%) - LeetHub

---
+++ 457-circular-array-loop/457-circular-array-loop.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    
+    int nextIdx(bool dir, int idx, vector<int> &nums){
+        
+        bool curr_dir = nums[idx]>=0?true:false;
+        
+        //opp dirn
+        if(curr_dir != dir)
+            return -1;
+        
+        int next_idx = (idx + nums[idx]+nums.size())%nums.size();
+        
+        if(next_idx==idx)
+            return -1;
+        
+        return next_idx;
+    }
+    
+    bool circularArrayLoop(vector<int>& nums) {
+        
+        //true ->fwd
+        //false ->bk
+        for(int idx=0; idx<nums.size(); idx++){
+            
+            bool dir = nums[idx]>=0?true:false;
+            int slow=idx, fast=idx;
+            
+            while(1){
+
+                slow = nextIdx(dir, slow, nums);
+                fast = nextIdx(dir, fast, nums);
+
+                if(fast !=-1)
+                    fast = nextIdx(dir, fast, nums);
+
+                if(slow==-1 || fast ==-1 || fast ==slow)
+                    break;
+                
+                //cout<<slow<<" "<<fast<<endl;
+            }
+            
+            if(fast==slow && slow!=-1)
+                return true;
+            
+        }
+        
+        return false;
+    }
+};
