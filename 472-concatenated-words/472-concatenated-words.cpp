6a776712a5c09090a7ae5d9c06c29e9079e071f0
Sameer <sameer.barha12@gmail.com>
2022-09-14 13:00:29 +0530

    Time: 394 ms (48.53%), Space: 46.9 MB (60.30%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.cpp
+++ 472-concatenated-words/472-concatenated-words.cpp
@@ -34,29 +34,44 @@ public:
         
     }
     
+    
+    vector<string> method2(vector<string>& words) {
+        
+        vector<string> ans;
+        unordered_set<string> dict(words.begin(), words.end());
+        
+        for(auto w : words) {
+            
+            if(w.empty()) continue;
+            
+            vector<int> dp(w.size() + 1, 0);
+            dp[0] = 1;
+            
+            for(int st = 0 ; st<= w.size() ;  st++) {
+                
+                for(int ed = st+1 ; ed <= w.size() ; ed++) {
+                    
+                    
+                    if(ed-st < w.size() && dp[st] == 1 && dict.find(w.substr(st, ed-st)) != dict.end()) {
+                        dp[ed] = 1;
+                        //break;
+                    }
+                }
+            }
+            
+            if(dp[w.size()])
+                ans.push_back(w);
+        }
+        
+        return ans;
+        
+    }
+    
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
         
         //return method1(words);
         
-        vector<string> res;
-        unordered_set<string> dict(words.begin(), words.end());
-        for(auto w : words) {
-            if(w.empty())
-                continue;
-            vector<int> dp(w.size() + 1, 0);
-            dp[0] = 1;
-            for(int i = 1 ; i <= w.size() ;  i++) {
-                for(int j = 0 ; j < i ; j++) {
-                    if(i - j < w.size() && dp[j] == 1 && dict.find(w.substr(j, i - j)) != dict.end()) {
-                        dp[i] = 1;
-                        break;
-                    }
-                }
-            }
-            if(dp[w.size()])
-                res.push_back(w);
-        }
-        return res;
-        
+        return method2(words);
     }
+        
 };
