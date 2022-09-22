9c612e47a32cfd8b4f4fb83492563831156825b0
Sameer <sameer.barha12@gmail.com>
2022-09-22 10:41:13 +0530

    Time: 396 ms (59.26%), Space: 56 MB (87.54%) - LeetHub

--- 2411-smallest-subarrays-with-maximum-bitwise-or/2411-smallest-subarrays-with-maximum-bitwise-or.cpp
+++ 2411-smallest-subarrays-with-maximum-bitwise-or/2411-smallest-subarrays-with-maximum-bitwise-or.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    vector<int> smallestSubarrays(vector<int>& nums) {
+    
+    vector<int> method1(vector<int>& nums) {
         
         int N = nums.size();
         
@@ -22,5 +23,50 @@ public:
         }
         
         return ans;
+        
+    }
+    
+    
+    vector<int> method2(vector<int> &nums){
+        
+                
+            vector<int> ans;
+            int N=nums.size();
+            vector<int> nearest (32, -1);
+
+            for (int i = N-1; i >= 0; i--){
+                
+                for (int j = 0; j < 32; j++) {
+
+                    // Nearest index where jth
+                    // bit is set 
+                    if (nums[i] & (1 << j))
+                            nearest[j] = i;
+
+                }
+
+                int last_set_bit_index = i;
+
+                // Finding Maximum index of all set bits
+                for (int j = 0; j < 32; j++)
+                last_set_bit_index = max(last_set_bit_index, nearest [j]);
+
+                ans.push_back(last_set_bit_index - i + 1);
+
+            }//for
+
+            // Reversing the answer vector 
+            reverse (ans.begin(), ans.end()); return ans;
+        
+        
+            return ans;
+        
+    }
+    
+    vector<int> smallestSubarrays(vector<int>& nums) {
+        
+        
+        return method1(nums);
+        //return method2(nums);
     }
 };
