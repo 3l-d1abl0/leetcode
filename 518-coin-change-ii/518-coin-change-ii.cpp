14e22f806f3fb251ab47aaa14f81859f10b47bc6
Sameer <sameer.barha12@gmail.com>
2022-09-25 00:31:10 +0530

    Time: 112 ms (13.61%), Space: 18.7 MB (29.90%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.cpp
+++ 518-coin-change-ii/518-coin-change-ii.cpp
@@ -51,15 +51,50 @@ public:
     }
     
     
+    int bottomUp(int sum, vector<int> &arr){
+
+        int N = arr.size();
+        vector< vector<int> > dp(N+1, vector<int> (sum+1));
+
+        //i==0 =>0ways
+        for(int j=0; j<=sum; j++)
+            dp[0][j] = 0;
+
+        for(int i=0; i<=N; i++)
+            dp[i][0] = 1;
+
+
+        for(int i=1; i<=N; i++){
+
+            for(int j=1; j<=sum; j++){
+
+                if(j<arr[i-1])//exclude
+                    dp[i][j] = dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j] + dp[i][j-arr[i-1]];
+
+            }
+
+        }
+
+
+        return dp[N][sum];
+    }
+    
+    
+    
+    
     
     int change(int amount, vector<int>& coins) {
         
         //recursion
         //return coinsWays(coins.size()-1, amount, coins);
         
-        //memoization
-        vector< vector<int>> memo(coins.size(), vector<int> (amount+1, -1));
-        return topDown(coins.size()-1, amount, coins, memo);
+        //memoization - AC
+        // vector< vector<int>> memo(coins.size(), vector<int> (amount+1, -1));
+        // return topDown(coins.size()-1, amount, coins, memo);
     
+        //dp
+        return bottomUp(amount, coins);
     }
 };
