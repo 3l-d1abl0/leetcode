1af9a030d45e424e3fbdf82b81a6f99d3d48aa3f
Sameer <sameer.barha12@gmail.com>
2022-10-01 17:49:46 +0530

    Time: 461 ms (54.01%), Space: 6.5 MB (88.73%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -31,10 +31,45 @@ public:
         
     }
     
+    int minPartDp(string &s){
+        
+        if(s.size()==0) return 0;
+        
+        int N = s.size();
+        vector<int> dp(N+1, 1e8);
+        
+        
+        dp[N] = 0;
+        
+        for(int i=N-1; i>=0; i--){
+            
+            int minn = INT_MAX;
+            
+            for(int j=i; j<N; j++){
+
+                if(isPalin(i, j, s)){
+
+                    minn = min(minn, dp[j+1]+1);
+                }
+            }
+        
+            dp[i]=minn;
+        }
+        
+        /*for(int ele: dp){
+            cout<<ele<<" ";
+        }cout<<endl;
+        */
+        return dp[0]-1;
+        
+    }
+    
     int minCut(string s) {
         
         
-        vector<int> memo(s.size(), -1);
-        return minpart(0, s, memo)-1;
+        //vector<int> memo(s.size(), -1);
+        //return minpart(0, s, memo)-1;
+        
+        return minPartDp(s);
     }
 };
