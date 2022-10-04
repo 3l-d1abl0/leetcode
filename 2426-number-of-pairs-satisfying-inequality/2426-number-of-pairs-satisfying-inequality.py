fb43613611f58a6b30c2f091d18d405c9e08c01e
Sameer <sameer.barha12@gmail.com>
2022-10-04 12:39:04 +0530

    Time: 1297 ms (96.07%), Space: 33 MB (48.17%) - LeetHub

---
+++ 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.py
@@ -0,0 +1,26 @@
+from bisect import bisect_right
+from sortedcontainers import SortedList
+
+class Solution:
+    def numberOfPairs(self, nums1: List[int], nums2: List[int], diff: int) -> int:
+        
+        '''N = len(nums1)
+        
+        sl = SortedList()
+        ans = 0
+        for idx in range(0, N):
+            
+            ans += bisect_right(sl, nums1[idx]-nums2[idx]+diff)
+            sl.add(nums1[idx]-nums2[idx])
+            
+            
+        return ans'''
+    
+        deltas = SortedList()
+        pairs = 0
+        for num1, num2 in zip(nums1, nums2):
+            delta = num1 - num2
+            pairs += deltas.bisect_right(delta + diff)
+            deltas.add(delta)
+        return pairs
+            
