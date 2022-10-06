b2a1c4660c5654c2235859c60a7cceb5cb092623
Sameer <sameer.barha12@gmail.com>
2022-10-06 17:56:13 +0530

    Time: 317 ms (90.81%), Space: 140.6 MB (26.14%) - LeetHub

--- 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.cpp
+++ 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.cpp
@@ -26,6 +26,88 @@ class Fenwick{
     
 };
 
+class SegmentTree{
+
+    private:
+        vector<int> st;
+        int N;
+    public:
+
+    SegmentTree(int n){
+        this->N = n;
+        st.resize(4*N, 0);
+    }
+
+    void build(int start, int ending, int node, vector<int> &v){
+
+        if(start==ending){
+            st[node] = v[start];
+            return;
+        }
+
+        int mid = (start+ending)/2;
+        
+        //leftSubtree
+        build(start, mid, 2*node+1, v);
+
+        //rightSubtree
+        build(mid+1, ending, 2*node+2, v);
+
+        st[node] = st[2*node+1]+st[2*node+2];
+    }
+
+    void build(vector<int> &v){
+        build(0, N-1, 0, v);
+    }
+
+    int query(int start, int ending, int l, int r, int node){
+        //non overlapping
+        if(ending<l || start >r){
+            return 0;
+        }
+
+        //complete overlap- ql start ending qr
+        if(l<=start && ending <=r)
+            return st[node];
+
+        //partial overlapp
+        int mid = (start+ending)/2;
+        int q1 = query(start, mid, l, r, 2*node+1);
+        int q2 = query(mid+1, ending, l, r, 2*node+2);
+        
+        return q1+q2;
+    }
+
+    int query(int l, int r){
+        return query(0, N-1, l, r, 0);
+    }
+
+    void update(int start, int ending, int node, int idx, int val){
+        //start case
+        if(start==ending){
+            st[node]++;
+            return;
+        }
+
+        int mid = (start+ending)/2;
+        if(idx<=mid){
+            update(start, mid, 2*node+1, idx, val);
+        }else{
+            update(mid+1, ending, 2*node+2, idx, val);
+        }
+
+        st[node] = st[2*node+1]+st[2*node+2];
+
+        return;
+    }
+
+    void update(int idx, int val){
+        update(0, N-1, 0, idx, val);
+    }
+
+};
+
+
 class Solution {
 public:
     
@@ -50,9 +132,30 @@ public:
         return pairs;
     }
     
+    long long method2(vector<int> &nums1, vector<int> &nums2, int diff){
+        
+        int offset = 3*1e4+5;
+        
+        SegmentTree tree(2*offset);
+        tree.update(offset+ nums1[0]-nums2[0], 0);
+        
+        int N = nums1.size();
+        long long int pairs =0;
+        for(int i=1; i<N; i++){
+            
+            pairs += tree.query(0, offset+nums1[i]-nums2[i]+diff);
+            tree.update(offset+ nums1[i]-nums2[i], 0);
+        }
+        
+        return pairs;
+        
+    }
+    
     long long numberOfPairs(vector<int>& nums1, vector<int>& nums2, int diff) {
         
-        return method1(nums1, nums2, diff);
+        //return method1(nums1, nums2, diff);
+        
+        return method2(nums1, nums2, diff);
         
     }
 };
