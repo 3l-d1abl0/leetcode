d89a068528fb97a2b110255cb39b764cc965a3ad
Sameer <sameer.barha12@gmail.com>
2022-10-13 22:43:20 +0530

    Time: 3 ms (52.31%), Space: 7.2 MB (61.10%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void solve(vector<string> &ans, int idx, string &s, string str, vector<string> &wd){
+
+        if(idx==s.size()){
+            if(str.size()!=0)
+                str.pop_back();
+                ans.push_back(str);
+        }
+        
+        
+
+        for(int i=0; i<wd.size(); i++){
+
+            int len = wd[i].size();
+
+            if(wd[i].compare(s.substr(idx, len))==0){
+
+                solve(ans,idx+len, s, str+wd[i]+" ", wd);
+            }
+
+        }//for
+
+    }
+    
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        
+        vector<string> ans;
+        solve(ans, 0, s, "", wordDict);
+        
+        return ans;
+    }
+};
