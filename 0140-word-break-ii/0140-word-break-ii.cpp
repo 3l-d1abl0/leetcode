ae0d801e007f8b2c4fe508fc3f65a00417149a50
Sameer <sameer.barha12@gmail.com>
2022-10-14 19:33:02 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (82.89%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -3,9 +3,11 @@ public:
     void solve(vector<string> &ans, int idx, string &s, string str, vector<string> &wd){
 
         if(idx==s.size()){
-            if(str.size()!=0)
+            if(str.size()!=0){
                 str.pop_back();
                 ans.push_back(str);
+                return;
+            }
         }
         
         
