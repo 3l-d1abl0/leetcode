cd0b22284077f3e8a891d38b3d95484dcfc4e902
Sameer <sameer.barha12@gmail.com>
2022-10-23 09:40:56 +0530

    Time: 502 ms (44.44%), Space: 86.8 MB (44.44%) - LeetHub

--- 2449-minimum-number-of-operations-to-make-arrays-similar/2449-minimum-number-of-operations-to-make-arrays-similar.cpp
+++ 2449-minimum-number-of-operations-to-make-arrays-similar/2449-minimum-number-of-operations-to-make-arrays-similar.cpp
@@ -1,71 +1,51 @@
 class Solution {
 public:
     long long makeSimilar(vector<int>& nums, vector<int>& target) {
-       #define ll long long
-        long long res = 0;
-        long long inter = 0,odd1=0,odd2=0;
-        vector<ll> on,en,et,ot;
-        for(int i = 0 ; i < nums.size() ; i++){
+        /*2 10 14
+        6 8 12
+        
+        2 2  5
+        4   1 3
+         */  
+            
+        sort(nums.begin(), nums.end());
+        sort(target.begin(), target.end());
+        
+        int N = nums.size();
+        long long int diff =0;
+        
+        vector<int> eve1, odd1;
+        vector<int> eve2, odd2;
+        
+        for(int i=0; i<N; i++){
+            
             if(nums[i]&1){
-                odd1 += nums[i];
-                on.push_back(nums[i]);
-            }   else en.push_back(nums[i]);
-                if(target[i]&1){
-                    odd2 += target[i];
-                    ot.push_back(target[i]);
-                }
-                else    et.push_back(target[i]);
-        }
-    
-        inter = odd2 - odd1;
-        res += abs(inter)/2;
-        sort(ot.begin(),ot.end());
-        sort(en.begin(),en.end());
-        sort(on.begin(),on.end());
-        sort(et.begin(),et.end());
-        
-        ll crr = inter;
-        for(int i = 0; i < ot.size();i++){
-            if(ot[i] > on[i]){
-                if(crr > 0){
-                    int remove = min(crr,(ll)ot[i] - on[i]);
-                    crr -= remove;
-                    on[i] += remove;
-                }
-                res += (ot[i]-on[i])/2;
-                crr -= ot[i]-on[i];
+                odd1.push_back(nums[i]);
             }else{
-                if(crr < 0){
-                    int remove = min(abs(crr),on[i]-ot[i]);
-                    crr += remove;
-                    on[i] -= remove;
-                }
-                res += (on[i]-ot[i])/2;
-                crr += on[i]-ot[i];
+                eve1.push_back(nums[i]);
             }
+            
+            if(target[i]&1){
+                odd2.push_back(target[i]);
+            }else{
+                eve2.push_back(target[i]);
+            }
+            
         }
         
-        inter *= -1;
-        crr = inter;
-        for(int i = 0; i < et.size();i++){
-            if(et[i] > en[i]){
-                if(crr > 0){
-                    int remove = min(crr,(ll)et[i] - en[i]);
-                    crr -= remove;
-                    en[i] += remove;
-                }
-                res += (et[i]-en[i])/2;
-                crr -= et[i]-en[i];
-            }else{
-                if(crr < 0){
-                    int remove = min(abs(crr),en[i]-et[i]);
-                    crr += remove;
-                    en[i] -= remove;
-                }
-                res += (en[i]-et[i])/2;
-                crr += en[i]-et[i];
-            }
+        int O =odd1.size();
+        int E = eve1.size();
+        
+        
+        for(int i=0; i<O; i++){
+            diff += abs(odd1[i]-odd2[i]);
         }
-        return res;
+        
+        
+        for(int i=0; i<E; i++){
+            diff += abs(eve1[i]-eve2[i]);
+        }
+        
+        return diff/4;
     }
 };
