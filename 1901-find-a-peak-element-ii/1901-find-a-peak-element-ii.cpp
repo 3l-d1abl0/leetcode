91fed2388aa03511cfa5c2597fac0a580640d0e5
Sameer <sameer.barha12@gmail.com>
2022-11-27 21:58:15 +0530

    Time: 463 ms (40.98%), Space: 46.4 MB (44.65%) - LeetHub

---
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+    
+    int max_element(vector<vector<int>> &mat, int midRow, int cols){
+        
+        int maxx = -1;
+        int col =0;
+        
+        for(int i=0; i<cols; i++){
+            
+            if(mat[midRow][i]>=maxx){
+                maxx = mat[midRow][i];
+                col=i;
+            }
+        }//for
+        
+        return col;
+        
+    }
+    
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        
+        int rows = mat.size();
+        int cols = mat[0].size();
+        
+       cout<<rows<<"x"<<cols<<endl;
+        int topRow = 0;
+        int botRow = rows-1;
+        
+        while(topRow <= botRow){
+            
+            
+            int midRow = topRow +(botRow-topRow)/2;
+            
+            int col = max_element(mat, midRow, cols);
+            
+            cout<<"midRow = "<<midRow<<" "<<col<<endl;
+            cout<<"1.----"<<endl;
+            //1. Top most Row
+            if(midRow==0){
+                
+                if(mat[midRow+1][col] < mat[midRow][col])
+                    return {midRow, col};
+                else{
+                    topRow= midRow+1;
+                    continue;
+                }
+            }
+          cout<<"2.----"<<endl;
+            //2. Bottom Row
+            if(midRow == rows-1){
+                
+                //cout<<mat[midRow-1][col]<<" < "<<mat[midRow][col]<<endl;
+                if(mat[midRow-1][col] < mat[midRow][col])
+                    return {midRow, col};
+                
+            }
+            
+           cout<<"3.----"<<endl;
+            //3. if this ele is the one
+            if(mat[midRow-1][col] < mat[midRow][col] && mat[midRow+1][col] < mat[midRow][col])
+                return {midRow, col};
+            
+            //4. elsse go to the greates neighour
+            if(mat[midRow+1][col] > mat[midRow][col])
+                topRow = midRow+1;
+            else
+                botRow= midRow-1;
+        }//while
+        
+        
+        return {-1, -1};
+        
+    }
+};
