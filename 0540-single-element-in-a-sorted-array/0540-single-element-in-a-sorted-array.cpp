6104b434e0d7d62c8342500908f2a7c5df18381c
Sameer <sameer.barha12@gmail.com>
2022-11-27 16:15:39 +0530

    Time: 76 ms (31.90%), Space: 22.4 MB (55.91%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,32 +1,53 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-     
-            int n=nums.size();
-            int low = 0;
-            int high = n - 2;
         
-            while (low <= high) {
-                int mid = (low + high) / 2;
+        int N = nums.size();
+        
+        int l=0, r= N-1;
+        
+        if(r==0)    //arrary size 1
+            return nums[r];
+        else if(nums[l]!=nums[l+1])
+            return nums[l];
+        else if(nums[r-1]!=nums[r])
+            return nums[r];
+        
+        
+        while(l<=r){
+            
+            int mid = l+(r-l)/2;
+            
+            /*before:
+                even - 1st idx
+                odd- end idx
                 
-                if (mid % 2 == 0) {
-                    if (nums[mid] != nums[mid + 1]) 
-                    //Checking whether we are in right half
-
-                        high = mid - 1; //Shrinking the right half
-                    else
-                        low = mid + 1; //Shrinking the left half
-                } else {
-
-                    //Checking whether we are in right half
-                    if (nums[mid] == nums[mid + 1]) 
-                        high = mid - 1; //Shrinking the right half
-                    else
-                        low = mid + 1; //Shrinking the left half
-                }
-            }
-
-            return nums[low];
+            single -  even idx
+            
+            after:
+                even - 2nd idx 
+                odd - 1st Idx
+            */
+            
+            
+            if(nums[mid]!= nums[mid-1] && nums[mid]!=nums[mid+1])
+                return nums[mid];
+            
+            if(mid%2==0 && nums[mid] == nums[mid+1])
+                l= mid+1;
+            else if(mid%2==0 && nums[mid] != nums[mid+1])
+                r= mid-1;
+            
+            
+            if(mid%2==1 && nums[mid] == nums[mid-1])
+                l= mid+1;
+            else if(mid%2==1 && nums[mid] != nums[mid-1])
+                r= mid-1;
+            
+            
+        }//while
         
+        
+        return -1;
     }
 };
