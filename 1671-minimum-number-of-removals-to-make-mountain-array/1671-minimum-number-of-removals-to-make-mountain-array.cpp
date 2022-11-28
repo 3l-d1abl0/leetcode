3360f395a18df95764902a502999806e6e62d4c0
Sameer <sameer.barha12@gmail.com>
2022-11-28 17:27:24 +0530

    Time: 378 ms (64.76%), Space: 12.1 MB (84.72%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -1,28 +1,45 @@
 class Solution {
 public:
-    int minimumMountainRemovals(vector<int>& n) {
-        int res = INT_MAX, sz = n.size();
-    vector<int> l, r, dp(sz);
-    for (int i = 0; i < sz; ++i) {
-        auto it = lower_bound(begin(l), end(l), n[i]);
-        if (it == l.end())
-            l.push_back(n[i]);
-        else 
-            *it = n[i];
-        dp[i] = l.size();
-    }
-    for (auto n : dp)
-        cout << n << " ";
-    cout << endl;
-    for (int i = n.size() - 1; i > 0; --i) {
-        auto it = lower_bound(begin(r), end(r), n[i]);
-        if (it == r.end())
-            r.push_back(n[i]);
-        else 
-            *it = n[i];
-        if (dp[i] > 1 && r.size() > 1)
-            res = min(res, sz - dp[i] - (int)r.size() + 1);
-    }
-    return res;
+    
+int lbs(vector<int> &arr,int n ){
+        
+  
+   /* Allocate memory for LIS[] and initialize LIS values as 1 for
+      all indexes */
+    vector<int> lis(n,1);
+  
+   /* Compute LIS values from left to right */
+   for (int i = 1; i < n; i++)
+      for (int j = 0; j < i; j++)
+         if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
+            lis[i] = lis[j] + 1;
+  
+   /* Allocate memory for lds and initialize LDS values for
+      all indexes */
+    vector<int> lds(n, 1);
+  
+   /* Compute LDS values from right to left */
+   for (int i = n-2; i >= 0; i--)
+      for (int j = n-1; j > i; j--)
+         if (arr[i] > arr[j] && lds[i] < lds[j] + 1)
+            lds[i] = lds[j] + 1;
+  
+  
+   /* Return the maximum value of lis[i] + lds[i] - 1*/
+   int maxx = 0;
+   for (int i = 1; i < n-1; i++){
+    
+       if(lis[i]>1 && lds[i]>1)
+         maxx = max( maxx, lis[i] + lds[i] - 1);
+   }
+    
+   return n-maxx;
+}
+    
+    
+    int minimumMountainRemovals(vector<int>& nums) {
+        
+        int N = nums.size();
+        return lbs(nums, N);
     }
 };
