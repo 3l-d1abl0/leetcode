de38d90a471c0b4a5fb860974aca5877201202c0
Sameer <sameer.barha12@gmail.com>
2022-11-28 18:05:10 +0530

    Time: 20 ms (98.96%), Space: 12.5 MB (9.55%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -1,28 +1,139 @@
 class Solution {
 public:
-    int minimumMountainRemovals(vector<int>& n) {
-        int res = INT_MAX, sz = n.size();
-    vector<int> l, r, dp(sz);
-    for (int i = 0; i < sz; ++i) {
-        auto it = lower_bound(begin(l), end(l), n[i]);
-        if (it == l.end())
-            l.push_back(n[i]);
-        else 
-            *it = n[i];
-        dp[i] = l.size();
+
+    
+int method1(vector<int> &arr, int n){
+   
+    /* Allocate memory for LIS[] and initialize LIS values as 1 for
+      all indexes */
+    vector<int> lis(n,1);
+  
+   /* Compute LIS values from left to right */
+   for (int i = 1; i < n; i++)
+      for (int j = 0; j < i; j++)
+         if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
+            lis[i] = lis[j] + 1;
+  
+   /* Allocate memory for lds and initialize LDS values for
+      all indexes */
+    vector<int> lds(n, 1);
+  
+   /* Compute LDS values from right to left */
+   for (int i = n-2; i >= 0; i--)
+      for (int j = n-1; j > i; j--)
+         if (arr[i] > arr[j] && lds[i] < lds[j] + 1)
+            lds[i] = lds[j] + 1;
+  
+  
+   /* Return the maximum value of lis[i] + lds[i] - 1*/
+   int maxx = 0;
+   for (int i = 1; i < n-1; i++){
+    
+       if(lis[i]>1 && lds[i]>1)
+         maxx = max( maxx, lis[i] + lds[i] - 1);
+   }
+    
+   return n-maxx;
+    
+}
+
+    int ceilIdx(vector<int> &lis, int key){
+        
+        if(lis.size()==0)
+            return 0;
+        
+        int left = 0, right = lis.size()-1;
+        
+        while(right > left+1){
+            
+            int mid = left + (right-left)/2;
+            
+            (lis[mid] >= key ? right : left) = mid;
+        }
+        
+        return right;
+        
+        
     }
-    for (auto n : dp)
-        cout << n << " ";
-    cout << endl;
-    for (int i = n.size() - 1; i > 0; --i) {
-        auto it = lower_bound(begin(r), end(r), n[i]);
-        if (it == r.end())
-            r.push_back(n[i]);
-        else 
-            *it = n[i];
-        if (dp[i] > 1 && r.size() > 1)
-            res = min(res, sz - dp[i] - (int)r.size() + 1);
+    
+    
+    int method2(vector<int> &nums,int n ){
+    
+        vector<int> l;
+        l.push_back(nums[0]);
+        int len =1;
+        
+        vector<int> dp1(n, 0);
+        dp1[0] =len;
+        
+        //LIS   
+        for(int i=1; i<nums.size(); i++){
+            
+            if(nums[i]<=l[0])
+                l[0]= nums[i];
+            else if(nums[i]>l[len-1]){
+                l.push_back(nums[i]);
+                len++;
+            }else{
+                //int idx = smallestAmongGreater(lis, nums[i]);
+                int idx = ceilIdx(l, nums[i]);
+                l[idx] = nums[i];
+            }
+            
+            dp1[i] =len;
+        }
+        
+        
+        vector<int> r;
+        r.push_back(nums[n-1]);
+        len =1;
+        vector<int> dp2(n, 0);
+        dp2[n-1] = len;
+        
+        //LDS 
+        for(int i=n-2; i>=0; i--){
+            
+            if(nums[i]<=r[0])
+                r[0]= nums[i];
+            else if(nums[i]>r[len-1]){
+                r.push_back(nums[i]);
+                len++;
+            }else{
+                //int idx = smallestAmongGreater(lis, nums[i]);
+                int idx = ceilIdx(r, nums[i]);
+                r[idx] = nums[i];
+            }
+            
+            dp2[i] =len;
+        }
+        
+
+        /* Return the maximum value of lis[i] + lds[i] - 1*/
+       int maxx = 0;
+    
+       //cout<<dp1[0]<<", "<<dp2[0]<<endl;
+       for (int i = 1; i < n-1; i++){
+
+           //cout<<dp1[i]<<" "<<dp2[i]<<endl;
+           
+           if(dp1[i]>1 && dp2[i]>1)
+             maxx = max( maxx, dp1[i] + dp2[i] - 1);
+       }
+        
+        //cout<<dp1[n-1]<<", "<<dp2[n-1]<<endl;
+        
+        
+        return n-maxx;
     }
-    return res;
+    
+    
+    int minimumMountainRemovals(vector<int>& nums) {
+        
+        int N = nums.size();
+        
+        //
+        //return method1(nums, N); //O(n^2)
+        
+        return method2(nums, N); //O(2NlonN + N)
     }
 };
