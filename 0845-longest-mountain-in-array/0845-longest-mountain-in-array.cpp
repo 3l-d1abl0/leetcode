cdafeb306bfd22a6a5dc793b247093b54c2f4f73
Sameer <sameer.barha12@gmail.com>
2022-11-28 16:35:24 +0530

    Time: 24 ms (85.25%), Space: 19.6 MB (22.68%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
@@ -4,11 +4,23 @@ public:
         
         int N = A.size(), res = 0;
         vector<int> up(N, 0), down(N, 0);
-        for (int i = N - 2; i >= 0; --i) if (A[i] > A[i + 1]) down[i] = down[i + 1] + 1;
-        for (int i = 0; i < N; ++i) {
-            if (i > 0 && A[i] > A[i - 1]) up[i] = up[i - 1] + 1;
-            if (up[i] && down[i]) res = max(res, up[i] + down[i] + 1);
+        
+        for (int i = N - 2; i >= 0; --i)
+            if (A[i] > A[i + 1])
+                down[i] = down[i + 1] + 1;
+        
+        
+        for (int i = 1; i < N; ++i) {
+            if (A[i] > A[i - 1])
+                up[i] = up[i - 1] + 1;
         }
+        
+        for(int i=0; i<N; i++){
+            
+            if (up[i] && down[i])
+                res = max(res, up[i] + down[i] + 1);
+        }
+        
         return res;
         
     }
