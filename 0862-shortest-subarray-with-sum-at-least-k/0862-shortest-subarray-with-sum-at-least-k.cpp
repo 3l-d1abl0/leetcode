50153641153f747e2dfa86aa562446308f61ec19
Sameer <sameer.barha12@gmail.com>
2022-11-29 23:31:02 +0530

    Time: 1447 ms (5.01%), Space: 162.6 MB (5.01%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.cpp
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.cpp
@@ -1,49 +1,44 @@
-struct Node{
-    long long int sum;
-    int idx;
-};
-
 class Solution {
 public:
-    int shortestSubarray(vector<int>& nums, int k) {
+    int shortestSubarray(vector<int>& A, int K) {
         
-        int N = nums.size();
-        deque<Node> dq;
+        int n = A.size();
+                
+        // use multimap to store previous prefix sums. using set has
+        // advantage of being sorted, so it is possible find 
+        // sums in range in logN rather than N (with unsorted container)
+        // we also store index of the sum so subarray len can be calculated       
+        multimap<long long int, int> prefSumsSoFar;
+        prefSumsSoFar.insert({0, -1});
         
-        long long int cSum =0;
-        int minn = INT_MAX;
-        
-        for(int i=0; i<N; i++){
-            
-            cSum+= nums[i];
-            
-            if(cSum >= k)
-                minn = min(minn, i+1);
-            
-            //cout<<"minn "<<minn<<endl;
-            //1. Add the new element to queue
-            while(!dq.empty() && dq.back().sum >= cSum){
-                dq.pop_back();
-            }
-            dq.push_back({cSum, i});
-            
-            
-            //2. Shrink the dq id sum of range >=k
-            while(!dq.empty() && cSum-dq.front().sum >= k){
-                minn = min(minn, i-dq.front().idx);
-                dq.pop_front();
-            }
-            
-            //if(!dq.empty())
-              //  cout<<dq.front().sum<<" "<<dq.back().sum<<endl;
-            
-            //cout<<"minn= "<<minn<<endl<<endl;
+        long long int prefSumI = 0;
+        int res = INT_MAX;
+        for(int i = 0; i < n; i++)
+        {
+            // calculate pref sum so far and store it
+            prefSumI += A[i];
+            prefSumsSoFar.insert({prefSumI, i});
+                
+            // since prefSumI - prefSumJ >= K, prefSumJ <= prefSumI - K,
+            // so we find all prefSums that satisfy this condition
+            auto prefSumJ_uppBnd = prefSumsSoFar.upper_bound(prefSumI - K);
+            // if no sum satisfies above condition, continue to next element
+            if(prefSumJ_uppBnd == prefSumsSoFar.begin()) continue;
+
+            // loop trough all sums that are <= prefSumI - K and find one with 
+            // min distance to current element
+            for(auto it = prefSumsSoFar.begin(); it != prefSumJ_uppBnd; it++)
+                res = min(res, i - it->second);
+
+            // sums that we found we dont need to consider anymore, since even
+            // if they can satisfy prefSumI - prefSumJ >= K for latter prefSumI (ie next elements)
+            // resulting subarray will be larger than current result. So, delete them.
+            // This is crucial to not have N^2 buth rather NlogN time complexity
+            prefSumsSoFar.erase(prefSumsSoFar.begin(), prefSumJ_uppBnd);       
         }
         
-        if(minn==INT_MAX)
-            return -1;
-        else
-            return minn;
+        if(res == INT_MAX) return -1;
+        else return res;
         
     }
 };
