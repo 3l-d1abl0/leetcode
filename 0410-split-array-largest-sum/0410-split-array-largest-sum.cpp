0afd5b8e253bbfbe3a3233c4ea9dc4d1ab1c1178
Sameer <sameer.barha12@gmail.com>
2022-11-29 14:37:15 +0530

    Time: 3 ms (81.85%), Space: 7.3 MB (78.15%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    
+    int func(vector<int> &nums, int m, int maxSum){
+        
+        int subS = 1;
+        int sum =0;
+        for(int ele: nums){
+            
+            //if(ele>maxSum) return false;
+            
+            if(sum+ele<=maxSum){
+                sum +=ele;
+            }else{
+                subS++;
+                //if(subS > m) return false;
+                sum = ele;
+            }
+        }
+        return subS;
+        //return subS <= m;
+        
+    }
+    
+    int splitArray(vector<int>& nums, int m) {
+        
+        int l = *max_element(nums.begin(), nums.end())-1;
+        int r = accumulate(nums.begin(), nums.end(), 0)+1;
+        
+        
+        while(r-l>1){
+        
+            //mid is the largest sum subarray can have
+            //we need to minimize this
+            int mid = l+(r-l)/2;
+            //predicate : mid <= max sum of a subset
+            //calculate how many sub arrays we can make with mid
+            /*
+                 mid(less)               mid(more)
+                  k(more)                k(less)
+            
+            */
+            
+            
+            if(func(nums, m, mid)<=m){//it can be divided into <=m subarrays with max subarray sum mid , also there is a chance that it might det divided into m subarrays wth max subarray sum less than mid, as mid dec m inc
+                r=mid;  //try to go more left so that m increases
+               // cout<<"mid :: "<<mid<<endl;
+            }else{
+                l=mid;
+            }
+            
+        }
+        
+        //cout<<":: "<<l<<" "<<r<<endl;
+        
+        return r;
+        
+    }
+};
