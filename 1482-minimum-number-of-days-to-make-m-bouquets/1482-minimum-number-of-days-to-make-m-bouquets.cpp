4f8eb7860c3a8012950dc0129e58afde4d8fa07f
Sameer <sameer.barha12@gmail.com>
2022-11-29 14:12:35 +0530

    Time: 490 ms (31.25%), Space: 66.5 MB (65.21%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int calBou(vector<int> &bloomDay, int m, int k, int mid){
+        
+        int bo =0, idx =0;
+        while(idx<bloomDay.size()){
+            
+            if(bloomDay[idx]>mid){
+                idx++;
+                continue;
+            }
+            
+            int kk=0;
+            while(kk<k && idx<bloomDay.size()){
+                
+                if(bloomDay[idx]>mid)
+                    break;
+                
+                kk++; idx++;
+                
+            }
+            
+            if(kk==k)
+                bo++;
+        }
+        
+        //cout<<"bo= "<<bo<<endl;
+        return bo;
+    }
+    
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        
+        
+        int llim = *min_element(bloomDay.begin(), bloomDay.end())-1;
+        int rlim = *max_element(bloomDay.begin(), bloomDay.end())+1;
+        
+        int l = llim;
+        int r = rlim;
+        
+        while(r-l>1){
+            
+            int mid = l+(r-l)/2;
+            
+            //cout<<l<<" "<<mid<<" "<<r<<endl;
+            if(calBou(bloomDay, m, k, mid) >= m)
+                r=mid;
+            else
+                l=mid;
+        }
+        
+        //cout<<l<<" , "<<r<<endl;
+        if(r==rlim)
+            return -1;
+        
+        return r;
+    }
+};
