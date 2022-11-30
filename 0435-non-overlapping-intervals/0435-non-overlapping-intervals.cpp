3947b4dadbcb63b4a23c9eab0536bd9c15eaaa61
Sameer <sameer.barha12@gmail.com>
2022-11-30 19:17:46 +0530

    Time: 1064 ms (21.81%), Space: 89.9 MB (47.59%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,49 +1,49 @@
-bool comp(vector<int> &a,vector<int> &b) {
-    return a[1]<b[1];
-}
-
+    bool comp(vector<int> &a, vector<int> &b){
+        return a[1]<b[1];
+    }
 
 class Solution {
 public:
     
 
-    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-     
-        int N=intervals.size();
-        
-        if(N==0)
-            return N;
-        
-        sort(intervals.begin(), intervals.end(), comp);
+    
+    int eraseOverlapIntervals(vector<vector<int>>& iv){
         
         
-        /*for(auto ele: intervals){
-            cout<<ele[0]<<", "<<ele[1]<<endl;
-        }*/
+        int N = iv.size();
+        
+        if(N==0) return 0;
+        
+        sort(iv.begin(), iv.end(), comp);
+        /*for(auto ele: iv)
+            cout<<ele[0]<<" "<<ele[1]<<endl;
+        */
         
         
-        pair<int, int> intv ={intervals[0][0], intervals[0][1]};
         
-        //cout<<"-----"<<endl;
+        pair<int, int> prevInterval = {iv[0][0], iv[0][1]} ;
         
-        int ctr=0; 
+        int ctr=0;
         for(int i=1; i<N; i++){
         
-            //cout<<intv.first<<" "<<intv.second<<endl;
-            
-            if(intv.second<= intervals[i][0]){ //non-overlapping
-                intv = { intervals[i][0], intervals[i][1]};
-                ctr++;
+            if(prevInterval.second<= iv[i][0]){ //doesn't Overlap
+                prevInterval = { iv[i][0], iv[i][1] };
+            }else{
                 
+                //overlap - remove the interval with max end time
+                // as it can cause omre conflict
+                int low = min(prevInterval.first, iv[i][0]);
+                int high = min(prevInterval.second, iv[i][1]);
+                
+                prevInterval ={low, high};
+                
+                ctr++;
             }
             
         }
         
-        //cout<<intv.first<<" "<<intv.second<<endl;
-        
         //cout<<ctr<<endl;
         
-        return N-ctr-1;
-        
+        return ctr;
     }
 };
