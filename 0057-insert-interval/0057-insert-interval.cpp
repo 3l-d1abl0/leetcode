a4e95a8ddf5997564f33995aecc2c9e5d4b14990
Sameer <sameer.barha12@gmail.com>
2022-11-30 19:19:58 +0530

    Time: 13 ms (96.54%), Space: 17.2 MB (21.25%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -3,32 +3,71 @@ public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& ni) {
         
         int N= intervals.size();
-        int idx;
-        vector<vector<int>> ans;
+        vector<vector<int>> list;
+        bool flag = false;
         
-        int i=0;
-        //push the meets that occur before newInterval start time
-        while(i<N && intervals[i][1]<ni[0]){
-            ans.push_back({intervals[i][0], intervals[i][1]});
-            i++;
+        if(N==0)
+            return {ni};
+        
+        
+        int i, idx;
+        if(intervals[i][0]<= ni[0]){
+            list.push_back({intervals[i][0], intervals[i][1]});
+            i=1;
+        }else{
+            list.push_back({ni[0], ni[1]});
+            i=0; flag =true;
         }
         
-        //new endtime is greater than starting time of meets
-        while(i<N && ni[1]>= intervals[i][0]){
-            ni[0] = min(ni[0], intervals[i][0]);
-            ni[1] = max(ni[1], intervals[i][1]);
+        
+        idx=0;
+        
+        while(flag==false){
+            
+            if(i<N && intervals[i][0]<= ni[0]){
+                
+                if(list[idx][0] <= intervals[i][0] && intervals[i][0] <= list[idx][1]){
+                    list[idx][0] = min(list[idx][0], intervals[i][0] );
+                    list[idx][1] = max(list[idx][1], intervals[i][1] );
+                }else{
+                    list.push_back({intervals[i][0], intervals[i][1]});
+                    idx++;
+                }
+                
+                i++;
+                
+            }else{
+                
+                if(list[idx][0] <= ni[0] && ni[0] <= list[idx][1]){
+                    list[idx][0] = min(list[idx][0], ni[0] );
+                    list[idx][1] = max(list[idx][1], ni[1] );
+                }else{
+                    list.push_back({ni[0], ni[1]});
+                    idx++;
+                }
+                
+                flag = true;
+            }
+            
+        }
+        
+        
+        while(i<N){
+            
+            if(list[idx][0] <= intervals[i][0] && intervals[i][0] <= list[idx][1]){
+                list[idx][0] = min(list[idx][0], intervals[i][0] );
+                list[idx][1] = max(list[idx][1], intervals[i][1] );
+                
+            }else{
+                list.push_back({intervals[i][0], intervals[i][1]});
+                idx++;
+            }
             
             i++;
+            
         }
         
-        ans.push_back({ni[0], ni[1]});
         
-        //push the rest
-        while(i<N){
-            ans.push_back({intervals[i][0], intervals[i][1]});
-            i++;
-        }
-        
-        return ans;
+        return list;
     }
 };
