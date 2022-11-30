78d537c7ce3ade8df67d9cddfbd83a09ce60c1e7
Sameer <sameer.barha12@gmail.com>
2022-11-30 15:17:27 +0530

    Time: 1089 ms (5.03%), Space: 162.5 MB (5.15%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.cpp
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.cpp
@@ -1,44 +1,30 @@
 class Solution {
 public:
-    int shortestSubarray(vector<int>& A, int K) {
+    int shortestSubarray(vector<int>& nums, int k) {
         
-        int n = A.size();
-                
-        // use multimap to store previous prefix sums. using set has
-        // advantage of being sorted, so it is possible find 
-        // sums in range in logN rather than N (with unsorted container)
-        // we also store index of the sum so subarray len can be calculated       
-        multimap<long long int, int> prefSumsSoFar;
-        prefSumsSoFar.insert({0, -1});
+        int n = nums.size();
         
-        long long int prefSumI = 0;
+        map<long, int> map;
+        map.insert({0, -1});
+        
+        long sum = 0;
         int res = INT_MAX;
-        for(int i = 0; i < n; i++)
-        {
-            // calculate pref sum so far and store it
-            prefSumI += A[i];
-            prefSumsSoFar.insert({prefSumI, i});
+        for(int i = 0; i < n; i++)  {
+            sum += nums[i];
+            map[sum] = i;
                 
-            // since prefSumI - prefSumJ >= K, prefSumJ <= prefSumI - K,
-            // so we find all prefSums that satisfy this condition
-            auto prefSumJ_uppBnd = prefSumsSoFar.upper_bound(prefSumI - K);
-            // if no sum satisfies above condition, continue to next element
-            if(prefSumJ_uppBnd == prefSumsSoFar.begin()) continue;
+            auto itUB = map.upper_bound(sum - k);
 
-            // loop trough all sums that are <= prefSumI - K and find one with 
-            // min distance to current element
-            for(auto it = prefSumsSoFar.begin(); it != prefSumJ_uppBnd; it++)
+            if(itUB == map.begin()) continue;
+
+            for(auto it = map.begin(); it != itUB; it++)
                 res = min(res, i - it->second);
 
-            // sums that we found we dont need to consider anymore, since even
-            // if they can satisfy prefSumI - prefSumJ >= K for latter prefSumI (ie next elements)
-            // resulting subarray will be larger than current result. So, delete them.
-            // This is crucial to not have N^2 buth rather NlogN time complexity
-            prefSumsSoFar.erase(prefSumsSoFar.begin(), prefSumJ_uppBnd);       
+            map.erase(map.begin(), itUB);       
         }
         
-        if(res == INT_MAX) return -1;
-        else return res;
+        return res == INT_MAX ? -1 : res;
+        
         
     }
 };
