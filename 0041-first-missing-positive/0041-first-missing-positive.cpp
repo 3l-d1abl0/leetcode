493b319519fc44ded80f5839fbd81cb4fb1053b3
Sameer <sameer.barha12@gmail.com>
2022-12-01 22:34:57 +0530

    Time: 55 ms (88.51%), Space: 31.4 MB (46.57%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -2,29 +2,33 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& arr) {
         
-        int N =0;
-        
-        for(int i=0; i<arr.size(); i++){
-            
-            if(arr[i]>0){
-                swap(arr[i], arr[N]);
-                N++;
+        int N = arr.size();
+
+        int i=0;
+        while(i<N){
+
+            long long int idx = arr[i]-1ll;
+
+            if(arr[i]>0 && arr[i]<= N && arr[i]!= arr[idx]){
+                swap(arr[i], arr[idx]);
+            }else{
+                i++;
+            }
+        }//while
+
+        /*for(int ele: arr){
+            cout<<ele<<" ";
+        }cout<<endl;*/
+
+
+        vector<int> ans;
+        for(int i=0; i<N; i++){
+            long long int idx = arr[i]-1ll;
+            if(i!= idx){
+                return (i+1);
             }
         }
         
-        for(int i=0; i<N; i++){
-            
-            int idx = abs(arr[i]) - 1;
-            if(idx < N){
-                arr[idx] =  -abs(arr[idx]);
-            }
-        }
-        
-        for(int i=0; i<N; i++){
-            if(arr[i] > 0)
-                return i+1;
-        }
-        
-        return N+1;
+        return arr.size()+1;
     }
 };
