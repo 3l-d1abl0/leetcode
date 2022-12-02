a8508c62cab7be76bf72c076112d9ac80c126647
Sameer <sameer.barha12@gmail.com>
2022-12-02 22:37:50 +0530

    Time: 585 ms (19.90%), Space: 84.3 MB (20.14%) - LeetHub

---
+++ 0502-ipo/0502-ipo.cpp
@@ -0,0 +1,40 @@
+class Compare
+{
+    public:
+        bool operator()(pair<int,int> a, pair<int,int> b){
+            return a.second>b.second;
+        }
+};
+
+class Solution {
+public:
+    int findMaximizedCapital(int k, int W, vector<int>& profits, vector<int>& capital) {
+        
+        priority_queue< pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > cap; //min  heap
+        priority_queue< pair<int, int> > prof; //max
+        
+        int N = profits.size();
+        for(int i=0; i<N; i++)
+            cap.push({capital[i], profits[i]});    
+        
+        
+        while(k--){
+            
+            while(!cap.empty() && cap.top().first <= W){
+                prof.push({cap.top().second, cap.top().second});
+                
+                cap.pop();
+            }
+            
+            if(prof.empty())
+                return W;
+            
+            W+= prof.top().first;
+            prof.pop();
+            
+        }
+        
+        
+        return W;
+    }
+};
