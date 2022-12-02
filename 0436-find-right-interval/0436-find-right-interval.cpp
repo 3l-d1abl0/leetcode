c6d7aa6f8d5e1d7ad6f94d53c0bdaf15bf865365
Sameer <sameer.barha12@gmail.com>
2022-12-02 20:45:27 +0530

    Time: 161 ms (57.31%), Space: 26.5 MB (75.85%) - LeetHub

---
+++ 0436-find-right-interval/0436-find-right-interval.cpp
@@ -0,0 +1,50 @@
+struct Node{
+    int val, id;
+};
+
+struct CompareVal {
+    bool operator()(Node const& n1, Node const& n2){
+        //n1 ordered before n2
+        return n1.val != n2.val ? n1.val > n2.val : n1.id >n2.id;
+    }
+};
+
+
+class Solution {
+public:
+    vector<int> findRightInterval(vector<vector<int>>& intervals) {
+        
+        int N = intervals.size();
+        if(N==1)
+            return {-1};
+        
+        priority_queue<Node, vector<Node>, CompareVal > start;
+        priority_queue<Node, vector<Node>, CompareVal > end;
+        
+        vector<int> ans (N, -1);
+        
+        for(int i=0; i<N ; i++){
+            start.push({intervals[i][0], i});
+            end.push({intervals[i][1], i});
+        }
+        
+        
+        /*while(!start.empty()){
+            cout<<start.top().val<<" "<<start.top().id<<endl;
+            
+            start.pop();
+        }*/
+        
+        
+        while(!start.empty() && !end.empty()){
+            if(start.top().val >= end.top().val){
+                ans[end.top().id] = start.top().id;
+                end.pop();
+            }else{
+                start.pop();
+            }
+        }
+        
+        return ans;
+    }
+};
