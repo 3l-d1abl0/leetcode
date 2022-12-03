e9406e8ed9ff968c169de3c67ec7ad071ba3958d
Sameer <sameer.barha12@gmail.com>
2022-12-04 00:07:47 +0530

    Time: 1560 ms (20.19%), Space: 206.9 MB (38.69%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+        int maxProfit3(vector<int>& stocks) {
+        
+        
+        int N = stocks.size();
+        
+        vector<vector<vector<int>>> dp(N+1, vector<vector<int>> (2, vector<int> (3, 0)));
+        //buy - 1 sell -0
+        //dp[N][0][0] = 0;
+        //  ...
+        //dp[N][1][1] =0
+        
+        for(int i=N-1; i>=0; i--){
+            
+            
+            for(int buy=0; buy<=1; buy++){
+                
+                for(int tx=1; tx<=2; tx++){
+                    
+                    int profit =0;
+
+                    if(buy){
+
+                        profit = max(-stocks[i]+ dp[i+1][0][tx], 0+ dp[i+1][1][tx]);
+
+                    }else{
+                        profit = max(stocks[i]+ dp[i+1][1][tx-1], 0 + dp[i+1][0][tx]);
+                    }
+                
+                    dp[i][buy][tx] = profit;
+                }
+                
+                
+            }
+            
+        }
+        
+        //printDP(dp);
+        
+        return dp[0][1][2];
+    }
+    
+    int maxProfit(vector<int>& stocks) {
+
+        
+        return maxProfit3(stocks);
+        
+	    int N = stocks.size();
+	    vector<vector<int>> dp(3, vector<int> (N,0));
+
+
+        for(int i=1; i<=2; i++){
+
+            int prev_profit = INT_MIN;
+            //cant make any transaction at j=0
+            for(int j=1; j<N; j++){
+
+                //1. not making any transaction on jth day
+                dp[i][j] = dp[i][j-1];
+
+                prev_profit = max(prev_profit, dp[i-1][j-1] - stocks[j-1] ); 
+                
+                dp[i][j] =max(dp[i][j], prev_profit+stocks[j]);
+            }
+        }
+        
+        
+        return dp[2][N-1];
+    }
+};
