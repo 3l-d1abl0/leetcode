31a0d28a981f60976890f45ca3c99d2ac85e0b25
Sameer <sameer.barha12@gmail.com>
2022-12-03 23:02:53 +0530

    Time: 26 ms (59.51%), Space: 11.7 MB (67.56%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,46 +1,32 @@
 class Solution {
 public:
-    int maxProfit(int k, vector<int>& stocks) {
+    
+    
+    	int memo[1001][2][101];
+	int dp(int i, int buy, int k, vector<int>& prices) {
+		if (i == prices.size()) return 0;
+		if (memo[i][buy][k] != -1) return memo[i][buy][k];
+		// dont buy/sell
+		
+        if (k == 0) return 0;
         
-                
-        int N = stocks.size();
-        
-        vector<vector<vector<int>>> dp(N+1, vector<vector<int>> (2, vector<int> (k+1, 0)));
-        //buy - 1 sell -0
-        //dp[N][0][0] = 0;
-        //  ...
-        //dp[N][1][1] =0
-        
-        for(int i=N-1; i>=0; i--){
-            
-            
-            for(int buy=1; buy>=0; buy--){
-                
-                for(int tx=1; tx<=k; tx++){
-                    
-                    int profit =0;
+        int ans = dp(i + 1, buy, k, prices);
+		
+		// buy
+		if (buy) {
+			ans = max(ans, -prices[i] + dp(i + 1, false, k, prices));
+		}
+		else {
+			ans = max(ans, prices[i] + dp(i + 1, true, k - 1, prices));
+		}
 
-                    if(buy){
-
-                        profit = max(-stocks[i]+ dp[i+1][0][tx], 0+ dp[i+1][1][tx]);
-
-                    }else{
-                        profit = max(stocks[i]+ dp[i+1][1][tx-1], 0 + dp[i+1][0][tx]);
-                    }
-                
-                    dp[i][buy][tx] = profit;
-                }
-                
-                
-            }
-            
-        }
+		return memo[i][buy][k] = ans;
+	}
+    
+    int maxProfit(int k, vector<int>& prices) {
         
-        //printDP(dp);
-        
-        return dp[0][1][k];
-        
-        
+        memset(memo, -1, sizeof(memo));
+		return dp(0, true, k, prices);
         
     }
 };
