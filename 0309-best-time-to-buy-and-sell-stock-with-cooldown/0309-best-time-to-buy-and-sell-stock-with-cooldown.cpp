1b41fa2a7bf872a820e227e7dcee39c7cfd59692
Sameer <sameer.barha12@gmail.com>
2022-12-04 11:14:10 +0530

    Time: 12 ms (34.31%), Space: 11.9 MB (48.19%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -22,13 +22,49 @@ public:
         return memo[idx][buy] = profit;
     }
     
+    int topDown(vector<int> &prices, int N){
+        
+        vector<vector<int>> dp(N+1, vector<int>(2, 0));
+        
+        //base case
+        dp[N][0] = dp[N][1] = 0;
+        
+        
+        for(int idx = N-1; idx>=0; idx--){
+            
+            for(int buy =0; buy<=1; buy++){
+                
+                
+                dp[idx][buy] = dp[idx+1][buy];
+        
+                if(buy==0){
+                    dp[idx][buy] = max(dp[idx][buy], -prices[idx] + dp[idx+1][1]);    
+                }else{
+                    //adding cool down
+                    if(idx+2<=N-1)
+                        dp[idx][buy] = max(dp[idx][buy], prices[idx] +dp[idx+2][0] ); 
+                    else
+                        dp[idx][buy] = max(dp[idx][buy], prices[idx] +0 ); 
+                }
+                
+            }
+            
+        }//for
+        
+        
+        return dp[0][0];
+        
+    }
+    
     int maxProfit(vector<int>& prices) {
         
         int N= prices.size();
         
-        vector<vector<int>> memo(N, vector<int> (2, -1));
-        
+        //vector<vector<int>> memo(N, vector<int> (2, -1));
         //0 - can but ,1 - can't buy
-        return recur(0, 0, prices, N, memo);
+        //return recur(0, 0, prices, N, memo);
+        
+        
+        return topDown(prices, N);
     }
 };
