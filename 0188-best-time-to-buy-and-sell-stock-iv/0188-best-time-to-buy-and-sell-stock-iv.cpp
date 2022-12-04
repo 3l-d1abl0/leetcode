b80853244c36d2e05bc65c9a9fa8be72d1db42fa
Sameer <sameer.barha12@gmail.com>
2022-12-04 10:01:45 +0530

    Time: 186 ms (9.52%), Space: 29.6 MB (6.33%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -23,10 +23,88 @@ public:
 		return memo[i][buy][k] = ans;
 	}
     
-    int maxProfit(int k, vector<int>& prices) {
+    
+    int topDown(vector<int> &stocks, int k){
         
-        memset(memo, -1, sizeof(memo));
-		return dp(0, true, k, prices);
+        int N = stocks.size();
+        
+        vector<vector<vector<int>>> dp(N+1, vector<vector<int>> (k+1, vector<int> (2, 0)));
+        
+        //Base case N == 0 and K==0 all 0
+        
+        for(int idx = N-1; idx>=0; idx--){
+            for(int K=1; K <=k; K++){
+                for(int buy = 0; buy <=1; buy++){
+                        
+        
+                dp[idx][K][buy] = dp[idx+1][K][buy]; //recur(idx+1, buy, K, stocks, N, memo);  //skip
+
+                if(buy){
+                    dp[idx][K][buy] = max(dp[idx][K][buy] , -stocks[idx] +dp[idx+1][K][0]);
+
+                }else{
+                    dp[idx][K][buy] = max(dp[idx][K][buy] , stocks[idx] +dp[idx+1][K-1][1]);
+                }
+                    
+                    
+                    
+                }
+            }
+            
+        }//for
+        
+        return dp[0][k][1];
+        
+    }
+    
+    int topDownSpaceOpti(vector<int> &stocks, int k){
+        
+        int N = stocks.size();
+        
+        vector<vector<int>> prev (k+1, vector<int> (2, 0));
+        vector<vector<int>> curr (k+1, vector<int> (2, 0));
+        
+        //Base case N == 0 and K==0 all 0
+        
+        for(int idx = N-1; idx>=0; idx--){
+            for(int K=1; K <=k; K++){
+                for(int buy = 0; buy <=1; buy++){
+                        
+        
+                curr[K][buy] = prev[K][buy]; //recur(idx+1, buy, K, stocks, N, memo);  //skip
+
+                if(buy){
+                    curr[K][buy] = max(curr[K][buy] , -stocks[idx] +prev[K][0]);
+
+                }else{
+                    curr[K][buy] = max(curr[K][buy] , stocks[idx] +prev[K-1][1]);
+                }
+                    
+                    
+                    
+                }
+            }
+            
+            prev = curr;
+            
+        }//for
+        
+        return curr[k][1];
+        
+        
+    }
+    
+    
+    
+    int maxProfit(int K, vector<int>& prices) {
+        
+        
+        //1.Recur +memo
+        //memset(memo, -1, sizeof(memo));
+		//return dp(0, true, K, prices);
+        
+        //2. TopDown DP
+        return topDown(prices, K);
         
     }
 };
