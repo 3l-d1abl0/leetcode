328044f1716cf97ed810a644fdc5d537aad8ff60
Sameer <sameer.barha12@gmail.com>
2022-12-05 17:17:42 +0530

    Time: 82 ms (39.07%), Space: 30.1 MB (26.71%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     
+    void print2D(vector<vector<int>> &dp, string msg){
+        
+        cout<<msg<<endl;
+        
+        for(auto row: dp){
+            for(int ele: row){
+                cout<<ele<<" ";
+            }
+            
+            cout<<endl;
+        }
+        
+    }
+    
     int recur(int idx1, string &s, int N, int idx2, string &t, int M, vector<vector<int>> &memo){
         
         if(idx2==M) return 1;
@@ -23,13 +37,54 @@ public:
         return memo[idx1][idx2] = include + exclude;
     }
     
+    int bottomUp(string &s, int N, string &t, int M){
+        
+        vector<vector<double>> dp(N+1, vector<double>(M+1, 0));
+        
+        //base cases
+        //all idx ==M =1
+        
+        /*for(int idx2 =0 ; idx2<=M; idx2++ ){
+            dp[N][idx2] =0;
+        }*/
+        
+        for(int idx1=0; idx1<=N; idx1++){
+            dp[idx1][M] =1;
+        }
+        
+        //rint2D(dp,"1.");
+        //cout<<"------"<<N+1<<M+1<<endl;
+        for(int idx1 = N-1; idx1>=0; idx1--){
+            
+            for(int idx2= M-1; idx2>=0; idx2--){
+                
+                //1.exclude /skip
+                dp[idx1][idx2] = dp[idx1+1][idx2];
+                
+                //2. Include
+                if(s[idx1]==t[idx2])
+                    dp[idx1][idx2] += dp[idx1+1][idx2+1];
+                
+            }
+        }
+        
+        //print2D(dp,"2.");
+        return dp[0][0];
+        
+    }
+    
     int numDistinct(string s, string t) {
         
         int N = s.size();
         int M = t.size();
         
         //1. recursion + memo
-        vector<vector<int>> memo(N, vector<int> (M, -1));
-        return recur(0, s, N, 0, t, M, memo);
+        //vector<vector<int>> memo(N, vector<int> (M, -1));
+        //return recur(0, s, N, 0, t, M, memo);
+        
+        
+        //2. Bottom Up DP
+        return bottomUp(s, N, t, M);
+        
     }
 };
