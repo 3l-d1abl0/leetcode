36a214c9019a50b51dc0116110321352f2d82c7f
Sameer <sameer.barha12@gmail.com>
2022-12-06 16:05:35 +0530

    Time: 82 ms (49.05%), Space: 13.2 MB (64.09%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -41,35 +41,8 @@ public:
         return sum;
     } 
     
-    int kthSmallestheap(vector<vector<int>> &matrix, int k){
-        
-        int rows = matrix.size();
-        priority_queue<Node, vector<Node>, CompareVal > minHeap;
-        for(int i=0; i<rows; i++){
-            minHeap.push({matrix[i][0], i, 0});
-        }
-        
-        int r=0, c=0;
-        while(k>0 && !minHeap.empty()){
-            
-            r= minHeap.top().row;
-            c= minHeap.top().col;
-            //cout<<r<<" "<<c<<" => "<<matrix[r][c]<<endl;
-            minHeap.pop();
-            
-            if(c+1 < matrix[r].size()){
-                minHeap.push({matrix[r][c+1], r, c+1});    
-            }
-            k--;
-        }
-        
-        return matrix[r][c];
-    }
     
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
-        
-        //1. Method1 - heap
-        return kthSmallestheap(matrix, k);
+    int binarySearch(vector<vector<int>> &matrix, int k){
         
         
         long long int l = INT_MAX; long long int r = INT_MAX;
@@ -101,5 +74,42 @@ public:
         
        // cout<<l<<" -- "<<r<<endl;
         return r;
+        
+        
+    }
+    
+    int kthSmallestheap(vector<vector<int>> &matrix, int k){
+        
+        int rows = matrix.size();
+        priority_queue<Node, vector<Node>, CompareVal > minHeap;
+        for(int i=0; i<rows; i++){
+            minHeap.push({matrix[i][0], i, 0});
+        }
+        
+        int r=0, c=0;
+        while(k>0 && !minHeap.empty()){
+            
+            r= minHeap.top().row;
+            c= minHeap.top().col;
+            //cout<<r<<" "<<c<<" => "<<matrix[r][c]<<endl;
+            minHeap.pop();
+            
+            if(c+1 < matrix[r].size()){
+                minHeap.push({matrix[r][c+1], r, c+1});    
+            }
+            k--;
+        }
+        
+        return matrix[r][c];
+    }
+    
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        
+        //1. Method1 - heap
+        //return kthSmallestheap(matrix, k);
+        
+        //2. Method2 - BinarySearch
+        return binarySearch(matrix, k);
+        
     }
 };
