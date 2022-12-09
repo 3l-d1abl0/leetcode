5cb22e1977fbdbc84d338c3ec676d2fd3034123f
Sameer <sameer.barha12@gmail.com>
2022-12-09 14:35:02 +0530

    Time: 11 ms (32.89%), Space: 9.5 MB (51.72%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -24,12 +24,48 @@ public:
         
     }
     
+    int method2(vector<vector<int>> &matrix, int key){
+        
+        int N = matrix.size();
+        int M = matrix[0].size();
+        
+        int total = N*M;
+        int l = -1;
+        int r= total;
+        
+        
+        while(r-l>1){
+            
+            int mid = l+(r-l)/2;
+            
+            //cout<<mid<<" "<<mid/total<<" "<<mid%total<<endl;
+            //cout<<matrix[mid/M][mid%M]<<" "<<endl;
+            if(matrix[mid/M][mid%M] < key){
+                
+                l = mid;
+                
+            }else{
+                r=mid;
+            }
+            
+        }
+       
+        //cout<<l<<" "<<r<<endl;
+        if (r==total)
+            return false;
+        else
+            return matrix[r/M][r%M] == key;
+        
+    }
+    
     bool searchMatrix(vector<vector<int>>& matrix, int key) {
         
         
         //1. method1
-        return method1(matrix, key);
+        //return method1(matrix, key);
     
+        //2. method2 - Binary Search
+        return method2(matrix, key);
 	
     }
 };
