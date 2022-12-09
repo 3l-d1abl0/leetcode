3bd80873154004c30a12e0dab69641a160983211
Sameer <sameer.barha12@gmail.com>
2022-12-09 18:17:10 +0530

    Time: 41 ms (28.42%), Space: 13.7 MB (47.10%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -23,10 +23,31 @@ public:
         
     }
     
+    
+    vector<int> method2(vector<int> &nums, int k){
+        
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        unordered_map<int, int> cnt;
+        for (auto num : nums) cnt[num]++;
+        for (auto kv : cnt) {
+            pq.push({kv.second, kv.first});
+            while (pq.size() > k) pq.pop();
+        }
+        vector<int> res;
+        while (!pq.empty()) {
+            res.push_back(pq.top().second);
+            pq.pop();
+        }
+        return res;
+        
+    }
     vector<int> topKFrequent(vector<int>& nums, int k) {
         
         //1. method1
-        return method1(nums, k);
+        //return method1(nums, k);
+        
+        //2. method2
+        return method2(nums, k);
         
         
         vector<int> res;
