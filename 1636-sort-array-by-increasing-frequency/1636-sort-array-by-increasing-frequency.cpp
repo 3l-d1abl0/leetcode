5f5e451e376aca8c979ad79423d1aca7b4cbdaa6
Sameer <sameer.barha12@gmail.com>
2022-12-10 21:09:44 +0530

    Time: 3 ms (98.89%), Space: 11 MB (98.22%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -17,11 +17,11 @@ vector<int> freq(201, 0);
     
 bool compare(int &a, int &b){
         //cout<<freq[a+100]<<" "<<freq[b+100]<<endl;
-    
+        //if frequency eaual
         if(freq[a+100]==freq[b+100])
-            return a >b;
+            return a >b;    ///decreasing order
         
-        return freq[a+100] < freq[b+100];
+        return freq[a+100] < freq[b+100];   //increasing order
         
 }
 
@@ -30,13 +30,17 @@ class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
         
-        fill(freq.begin(), freq.end(), 0);
+        vector<int> freq(201, 0);
+        //fill(freq.begin(), freq.end(), 0);
         
         for(int ele: nums){
             freq[ele+100]++;
         }
         
-        sort(nums.begin(), nums.end(), compare);
+        //sort(nums.begin(), nums.end(), compare);
+        sort(nums.begin(), nums.end(), [&](int a, int b) {
+            return freq[a + 100] == freq[b + 100] ? a > b : freq[a + 100] < freq[b + 100];
+        });
         
         return nums;
     }
