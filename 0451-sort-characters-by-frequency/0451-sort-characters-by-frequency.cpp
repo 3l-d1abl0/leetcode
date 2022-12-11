11a7be69296766a66bed8b1eda0247a35dcb8237
Sameer <sameer.barha12@gmail.com>
2022-12-11 20:36:39 +0530

    Time: 19 ms (69.29%), Space: 14.1 MB (10.90%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -73,16 +73,45 @@ public:
         return ans;
     }
     
+    string method4(string &s){
+        
+        unordered_map<char, int> mp;
+        
+        for(char c: s){
+            mp[c]++;
+        }
+        
+        vector<vector<char> > bucket(s.size());
+        
+        for(auto &[c, f]: mp){
+            bucket[f-1].push_back(c);
+        }
+        
+        string ans;
+        for(int idx= s.size()-1; idx>=0; idx--){
+            
+            for(char c: bucket[idx]){
+                ans.append(idx+1, c);
+            }
+        }
+        
+        return ans;
+        
+    }
+    
     string frequencySort(string s) {
         
         
         //1. sort based on frequency - TC:O(NlogN) SC: O(logN)
         //return method1(s);
         
-        //2. sort by freq, char pair - TC O(N) + )(NlogN) SC: O(N)
+        //2. sort by freq, char pair - TC O(N) +(KlogK) SC: O(N)
         //return method2(s);
         
-        //3. heap sort
-        return method3(s);
+        //3. heap sort - TC: O(N)+ O(KlogK), SC : O(N)+O(N)
+        //return method3(s);
+        
+        //4.Bucket Sort
+        return method4(s);
     }
 };
