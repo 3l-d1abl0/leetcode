2829e175150bee69e251678faf8ef2a010525ece
Sameer <sameer.barha12@gmail.com>
2022-12-13 19:02:58 +0530

    Time: 359 ms (62.44%), Space: 61.4 MB (91.37%) - LeetHub

---
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+        
+        
+        int N = arr.size();
+        
+        if(N<=k)
+    	    return 0; //all removed
+    	    
+    	
+    	unordered_map<int, int> freq;
+    	for(int i=0; i<N; i++){
+    	    freq[arr[i]]++;
+    	}
+    	
+    	int sz = freq.size();
+        
+    	priority_queue< pair<int, int> , vector<pair<int, int> >, greater<pair<int, int>> > minHeap;
+    	
+    	int dist =0;
+    	for(auto ele: freq){
+    	    
+    	    if(ele.second==1){
+    	        dist++;
+    	    }else{
+    	        minHeap.push({ele.second, ele.first});
+    	    }
+    	}
+        
+        if(k<=dist){
+            return (dist-k)+(sz-dist);
+        }
+    	
+        k-=dist;
+    	
+        //cout<<"K = "<<k<<" "<<minHeap.size()<<endl;
+    	while(k>0 && !minHeap.empty()){
+    	    
+    	    
+    	    auto ele = minHeap.top();
+    	    
+    	    
+    	    if( ele.first > k){
+    	        return minHeap.size();
+    	    }
+            
+            k-=ele.first;
+            minHeap.pop();
+            
+    	}//while
+    	
+        
+        return minHeap.size();
+    }
+};
