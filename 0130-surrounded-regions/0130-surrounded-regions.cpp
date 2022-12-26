818d82599c54d8ca07a2f2c028dfd67c690ffb81
Sameer <sameer.barha12@gmail.com>
2022-12-26 21:21:02 +0530

    Time: 25 ms (59.85%), Space: 15.5 MB (10.14%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    void solve(vector<vector<char>>& board) {
+        
+        int N = board.size();
+	
+	int M = board[0].size();
+	
+	
+	vector<vector<bool> > visited (N, vector<bool> (M, false));
+	
+	
+	//row 0
+	for(int j=0; j<M; j++){
+		if(board[0][j]=='O' && visited[0][j]== false)
+			dfs(0, j, visited, board, N, M);
+	}
+	
+
+	//row N-1
+	for(int j=0; j<M; j++){
+		if(board[N-1][j]=='O' && visited[N-1][j]== false)
+		
+			dfs(N-1, j, visited, board, N, M);
+	}
+	
+	
+	//col 0
+	for(int i=1; i<=N-2; i++){
+		if(board[i][0]=='O' && visited[i][0]== false)
+			dfs(i, 0, visited, board, N, M);
+	}
+
+	//col M-1
+	for(int i=1; i<=N-2; i++){
+		if(board[i][M-1]=='O' && visited[i][M-1]== false)
+			dfs(i, M-1, visited, board, N, M);
+	}
+	
+	
+	for(int i=1; i<N-1; i++){
+		for(int j=1; j<M-1; j++){
+			
+			if(board[i][j] =='O' && visited[i][j]==false)
+				board[i][j] = 'X';
+			
+		}
+	}
+	
+	//return board;
+        
+        
+    }
+    
+    void dfs(int row, int col, vector<vector<bool> > &visited, vector<vector<char>> &board, int N, int M){
+	//cout<<row<<"  "<<col<<endl;
+	visited[row][col] = true;
+	
+	vector<vector<int>> dir{{-1,0},{0,1},{1,0},{0,-1}};
+	
+	for(int i=0; i<4; i++){
+		
+		if(row+dir[i][0] <0 || col+dir[i][1]<0 || row+dir[i][0]>= N || col+dir[i][1]>= M)
+			continue;
+		
+		if(board[row+dir[i][0]][col+dir[i][1]]=='O' && visited[row+dir[i][0]][col+dir[i][1]]== false){
+			dfs(row+dir[i][0], col+dir[i][1], visited, board, N, M);
+		}
+	}
+	
+	
+}
+};
