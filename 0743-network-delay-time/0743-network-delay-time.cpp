95e53990ffdc8711b8b07552df8ad40c7d119fac
Sameer <sameer.barha12@gmail.com>
2023-01-06 13:22:58 +0530

    Time: 282 ms (31.80%), Space: 43.6 MB (29.68%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -0,0 +1,101 @@
+class Solution {
+public:
+    
+    int bfs(int src, vector<vector<pair<int,int>>> &adj, int n){
+        
+        queue<int> q;
+        
+        vector<int> dist(n+1, 1e7);
+        
+        dist[src] = 0;
+        q.push(src);
+        
+        while(!q.empty()){
+            
+            int node = q.front(); q.pop();
+            
+            for(auto adjNode: adj[node]){
+                int vtx = adjNode.first, t = adjNode.second;
+                
+                if(dist[vtx] > dist[node]+t){
+                    dist[vtx] = dist[node]+t;
+                    q.push(vtx);
+                }
+            }
+            
+        }
+        
+        
+        int maxx = *max_element(dist.begin()+1, dist.end()); 
+        
+        return maxx == 1e7 ? -1 : maxx;
+        
+    }
+    
+    int dijkstra(int src, vector<vector<pair<int,int>>> &adj, int n){
+        
+        
+        vector<int> dist (n+1, 1e7); dist[src] =0;
+        
+        priority_queue< pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > pq;
+        
+        pq.push({0, src});
+        
+        while(!pq.empty()){
+            
+            auto ele = pq.top(); pq.pop();
+            int time = ele.first, node= ele.second;
+            //cout<<"Processing : "<<node<<" with time "<<time<<endl;
+            
+            for(auto adjNode: adj[node]){
+                
+                int vtx = adjNode.first, t = adjNode.second;
+                
+                if(dist[node]+t < dist[vtx]){
+                    
+                    dist[vtx] = dist[node]+t;
+                    pq.push({ dist[vtx], vtx});
+                    
+                   // cout<<"To heap : "<<vtx<<" with time "<<dist[vtx]<<endl;
+                    
+                }
+            }
+            
+        }
+        
+        
+        /*for(int ele: dist){
+            cout<<ele<<" ";
+        }*/
+        
+        int maxx = *max_element(dist.begin()+1, dist.end()); 
+        if (maxx == 1e7){
+            return -1;
+        }else{
+            return maxx;
+        }
+        
+    }
+    
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        
+        
+        vector<vector<pair<int,int>>> adj(n+1);
+        
+        for(auto ele: times){
+            adj[ele[0]].push_back({ele[1], ele[2]});
+        }
+            
+        
+        
+        //return dijkstra(k, adj, n);
+        
+        
+        return bfs(k, adj, n);
+        
+        
+        
+        
+        
+    }
+};
