a51b85cd91bc2662167830118c9d9296b265b6a1
Sameer <sameer.barha12@gmail.com>
2023-08-13 23:53:16 +0530

    Time: 31 ms (10.50%), Space: 28 MB (8.03%) - LeetHub

--- 0968-binary-tree-cameras/0968-binary-tree-cameras.cpp
+++ 0968-binary-tree-cameras/0968-binary-tree-cameras.cpp
@@ -19,8 +19,10 @@ public:
     */
     
     int totalCam = 0;
-    
     unordered_set<TreeNode* > covered;
+    
+    map<pair<TreeNode*,pair<int,int>>, int> dp;
+    
     int minCameraCover(TreeNode* root) {
         
         
@@ -29,20 +31,97 @@ public:
         
         
         //2. Method2
-        return method2(root);
+        //return method2(root);
         
         
-        //3.
+        //3. Method3
+        return method3(root);
         
     }
     
+    /*===================Method3====================*/
+    int method3(TreeNode *root){
+        
+        //solve(node, camera/no camera, parent camera/no)
+        return min(solve(root, 1, 0), solve(root, 0, 0));
+    }
+    
+    
+    int solve(TreeNode* root, int  camera, int parentCamera){
+        
+        if(root==NULL){
+            
+            if(camera) return 1e7; //can't place a camera at NULL - return high value so an alternate way can be chosen
+            else return 0;
+        }
+        
+        
+        if(root->left == NULL && root->right == NULL){  //leafnode
+            
+            if(camera){ //place a camera
+                return 1;
+            }else{  //camera not placed at leaf node
+                
+                if(parentCamera)    //parent already has camera
+                    return 0;
+                else
+                    return 1e7;     //not possible - return high value so an alternate way can be chosen
+            }
+        }
+        
+        
+        if( dp.find( {root, {camera, parentCamera}} ) != dp.end() ) 
+            return dp[ {root, {camera, parentCamera} }];
+            
+        //if(dp.count({root, {camera, parentCamera}}) != 0 ) return dp[{root, {camera, parentCamera}}];
+        
+        if(camera){
+            
+            /* place a camera at curretn node
+            from left node choose which option gives min ans (place camera/ do not place camera)
+            from right node choose which option gives min ans (place camera/ do not place camera)
+            pass parent camera as 1
+            */
+            
+            return dp[{ root, {camera, parentCamera} }] = 1+ min( solve(root->left, 0, 1), solve(root->left, 1, 1) ) + min(solve(root->right, 0, 1), solve(root->right, 1, 1));
+            
+        }else{
+            
+            if(parentCamera){ 
+                
+                /* When parent has a camera, the current node is already covered
+                   from left node choose which option gives min ans (place camera/ do not place camera)
+                   from right node choose which option gives min ans (place camera/ do not place camera)
+                   pass parent camera as 1
+                */
+                return dp[{ root, {camera, parentCamera} }] = min( solve(root->left, 0, 0), solve(root->left, 1, 0) ) + min(solve(root->right, 0, 0), solve(root->right, 1, 0));
+                
+            }else{
+                
+                /* condn: Parent does not have a camera, and no camera to be placed at current node
+                    Case 1: Place a camera at left node + for right node which ever option gives min ans    
+                    Case 2: Place a camera at right node + for left node which ever option gives min ans    
+                */
+                
+                int case1 = solve(root->left, 1, 0) + min(solve(root->right, 0, 0), solve(root->right, 1, 0));
+                
+                int case2 = solve(root->right, 1, 0) + min(solve(root->left, 0, 0), solve(root->left, 1, 0));
+                
+                
+                return dp[{ root, {camera, parentCamera} }] = min(case1, case2);
+            }
+            
+        }
+        
+        
+        return dp[{ root, {camera, parentCamera} }];
+    }
+    /*===================Method2====================*/
     int method2(TreeNode *root){
         covered.insert(NULL);
         dfsMethod2(root, NULL);
         return totalCam;
     }
-    
-    
     void dfsMethod2(TreeNode *root, TreeNode *par){
         
         if(root==NULL)
@@ -73,6 +152,10 @@ public:
         
     }
     
+    
+    
+    
+    /*===================Method1====================*/
     int method1(TreeNode *root){
         
         int status = dfs(root);
@@ -84,7 +167,6 @@ public:
         return totalCam;
         
     }
-    
     int dfs(TreeNode* root){
         
         if(root==NULL)
