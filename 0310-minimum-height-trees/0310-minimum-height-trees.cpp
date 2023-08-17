93f82d1cf18e2c321cd4acbec7440ebdb8159410
Sameer <sameer.barha12@gmail.com>
2023-08-18 00:03:23 +0530

    Time: 159 ms (64.67%), Space: 65.5 MB (18.73%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.cpp
+++ 0310-minimum-height-trees/0310-minimum-height-trees.cpp
@@ -52,9 +52,109 @@ public:
         return ans;
     }
     
+        vector<int> method1(int n, vector<vector<int>> &edges){
+        return minHeightTree(n, edges);
+    }
+    
+    
+        void dfsFindHeight(int node, int parent, vector<vector<int>> &adj, vector<int> &height){
+
+        height[node] = 0;
+
+        for(int child: adj[node]){
+
+            if(child!=parent){
+
+                dfsFindHeight(child, node, adj, height);
+
+                height[node] = max(height[node], 1+height[child]);
+            }
+        }
+
+    }
+    
+    
+        void dfsReRoot(int node, int parent, vector<vector<int>> &adj, vector<int> &height, vector<int> &maxHeight){
+
+        //1. For the particular node, find 2 max heights from child
+        int max1 = -1, max2=-1;
+        for(int child: adj[node]){
+
+            if(child!=parent){
+                cout<<height[child]<<" ";
+                if(height[child]> max1){
+                    max2 = max1;
+                    max1 = height[child];
+                }else if(height[child] > max2){
+                    max2 = height[child];
+                }
+            }
+        }
+
+        
+        //2. Travers each child
+        for(int child: adj[node]){
+
+            if(child!=parent){
+
+                //1. If the child itself lies in the max height path from the parent, try the second max height
+                if(height[child]==max1){
+                        maxHeight[child] = max(maxHeight[node]+1, max2+2);
+                }else{
+                    maxHeight[child] = max(maxHeight[node]+1, max1+2);
+                }
+
+                dfsReRoot(child, node, adj, height, maxHeight);
+
+            }
+        }
+
+
+
+    }
+
+    vector<int> method2(int N, vector<vector<int>> &edges){
+
+        vector<vector<int>> adj(N, vector<int>());
+
+        for(auto row: edges){
+            adj[row[1]].push_back(row[0]);
+            adj[row[0]].push_back(row[1]);
+        }
+
+        vector<int> height(N, 0);
+        vector<int> maxHeight(N, 0);
+
+
+        dfsFindHeight(0, -1, adj, height);
+
+        dfsReRoot(0, -1, adj, height, maxHeight);
+
+        int minn = 1e7;
+        int maxLen = -1;
+        vector<int> ans;
+        for(int i=0; i<N; i++){
+
+            maxLen = max(height[i], maxHeight[i]);
+            if(maxLen<minn){
+                ans = {i};
+                minn = maxLen;
+            }else if(maxLen==minn){
+                ans.push_back(i);
+            }
+        }
+
+        return ans;
+    }
+    
     
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
   
-            return minHeightTree(n, edges);
+                        //1. topo Sort
+            //return method1(n, edges);
+
+            //2. Re rooting
+            return method2(n, edges);
     }
+    
 };
