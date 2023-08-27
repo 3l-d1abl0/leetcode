b122aa40058fa7279d344e6afc44514865e26a2e
Sameer <sameer.barha12@gmail.com>
2023-08-27 19:10:39 +0530

    Time: 491 ms (10.32%), Space: 33.7 MB (19.41%) - LeetHub

--- 2719-count-of-integers/2719-count-of-integers.cpp
+++ 2719-count-of-integers/2719-count-of-integers.cpp
@@ -46,7 +46,54 @@ public:
         return memo[idx][last][dig_sum] = ans;
     }
     
+    
+    int digitDP(string s, int minSum, int maxSum) {
+    
+    
+    int N = s.length();
+
+    vector<vector<vector<int>>> dp(N+1, vector<vector<int>>(2, vector<int>(maxSum+20, 0)));
+
+    //idx, last, sum
+    // Empty prefix with sum=0
+    dp[0][1][0] = 1;
+
+    for (int i = 0; i < N; ++i) {
+        for (int last = 0; last < 2; ++last) {
+            for (int sm = 0; sm <= maxSum; ++sm) {
+                
+                int limit = last ? (s[i] - '0') : 9;
+                for (int digit = 0; digit <= limit; ++digit) {
+                    
+                    
+                    dp[i+1][last && (digit == limit)][sm+digit] += dp[i][last][sm];
+                    dp[i+1][last && (digit == limit)][sm+digit] %= MOD;
+                }
+            }
+        }
+    }
+
+    
+    int res = 0;
+    for (int i = minSum; i <= maxSum; ++i) {
+        res = (res + dp[N][1][i] + dp[N][0][i]) % MOD;
+    }
+
+    return res % MOD;
+}
+    
+    
+    int method2(string num1, string num2, int minSum, int maxSum){
+        
+                int res = digitDP(num2, minSum, maxSum); 
+                subtract(num1);
+                res -= digitDP(num1, minSum, maxSum);
+                return (res + MOD) % MOD;
+    }
+    
     int count(string num1, string num2, int min_sum, int max_sum) {
+        
+        return method2(num1, num2, min_sum, max_sum);
         
         subtract(num1);
         //cout<<num2<<" "<<num1<<endl;
