8e44cb99dc57f2b3455ecd8081ad897658857186
Sameer <sameer.barha12@gmail.com>
2023-09-11 16:09:40 +0530

    Time: 8 ms (85.79%), Space: 7.3 MB (8.53%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -1,29 +1,82 @@
 class Solution {
-public:
-    int distributeCookies(vector<int>& cookies, int k) {
-        int n = cookies.size();
-        vector<vector<int>>dp(k + 1, vector<int>(1ll << n, INT_MAX));
+public:    
+    
+    int choose(int i, int availMask, int k, vector<vector<int>> &memo, vector<int> &sum) { 
         
-        vector<int>sum(1ll << n);
-        for(int mask = 0;mask<(1ll << n); mask++){
-            int total = 0;
-            for(int i = 0;i<n;i++){
-                if(mask & (1ll << i)){
-                    total += cookies[i];
+        if (i == 1)
+            return sum[availMask];
+        
+         //if (i == k) return (mask == ((1 << n) - 1)) ? 0 : 1e9;
+        
+        
+        if(memo[i][availMask]!=-1)
+            return memo[i][availMask];
+        
+        
+        int ans = 1e9;
+        
+        for (int new_mask = availMask; new_mask > 0; new_mask = (new_mask - 1) & availMask) {
+            
+            int remainingMask = new_mask ^ availMask;
+            int nextMaxSubsetSum = choose(i- 1, remainingMask, k, memo, sum);
+            
+            nextMaxSubsetSum = max(sum[new_mask], nextMaxSubsetSum);
+            ans = min(ans, nextMaxSubsetSum);
+        }
+        
+        return memo[i][availMask] = ans;
+    }
+    
+    int bottomUp(vector<int> &cookies, vector<int> &sum, int K){
+        
+        int N = cookies.size();
+        int sz = 1<<N;
+        
+        vector<vector<int>> dp(K+1, vector<int> (1<<N, INT_MAX));
+        
+        //Base: for k==1, sum is the sum of elements of mask
+        for(int mask=1; mask<sz; mask++)
+            dp[1][mask] = sum[mask];
+        
+        for(int k=2; k<=K; k++){
+            
+            for(int availMask = 0; availMask <sz; availMask++ ){
+            
+                for (int new_mask = availMask; new_mask > 0; new_mask = (new_mask - 1) & availMask) {
+
+                    int remainingMask = new_mask ^ availMask;
+                    int nextMaxSubsetSum = dp[k-1][remainingMask];
+
+                    nextMaxSubsetSum = max(sum[new_mask], nextMaxSubsetSum);
+                    dp[k][availMask] = min(dp[k][availMask], nextMaxSubsetSum);
+                }
+        
+            }
+        }
+        
+        
+        return dp[K][sz-1];
+    }
+    
+    int distributeCookies(vector<int>& cookies, int k) {
+        
+        
+        int N = cookies.size();
+        
+        //Calculate sum of every mask;
+        vector<int> sum (1 << N, 0); 
+        
+        for (int i = 0; i < (1 << N); i++) {
+            for (int j = 0; j < N; j++) {
+                if (i & (1 << j)) {
+                    sum[i] += cookies[j];
                 }
             }
-            sum[mask] = total;
         }
-
-        dp[0][0] = 0;
-        for(int person = 1;person<=k;person++){
-            for(int mask = 0;mask<(1ll << n);mask++){
-                for(int submask=mask;submask;submask=(submask - 1)&mask){
-                    dp[person][mask] = min(dp[person][mask], max(sum[submask], dp[person - 1][mask ^ submask]));
-                }
-            }
-        }
-
-        return dp[k][(1ll << n) - 1];
-    }   
+        
+        vector<vector<int>> memo(k+1, vector<int> (1<<N, -1));
+        //return choose(k, (1 << N) - 1, k, memo, sum); //all 1s, every bag is available
+        
+        return bottomUp(cookies, sum, k);
+    }
 };
