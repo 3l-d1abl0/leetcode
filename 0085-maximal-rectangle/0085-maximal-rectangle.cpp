f329a7c99e2099dc27b61a0a2d12d38744b50a4d
Sameer <sameer.barha12@gmail.com>
2023-09-28 23:34:01 +0530

    Time: 33 ms (75.10%), Space: 15.4 MB (48.05%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,93 @@
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        
+        /*
+        Prepare a cumulative height array for every Row
+        
+        For every row calculate largest area in the histogram 
+        */
+        
+        
+        int R = matrix.size();
+        int C = matrix[0].size();
+        
+        int maxx = -1;
+        vector<int> row(C);
+        
+        vector<int> defaultLeft(C);
+        vector<int> defaultRight(C);
+        
+        
+        /*
+        If no element is found on left/right 
+        distance is equal to the border
+        */
+        for(int i=0; i<C; i++){
+            defaultLeft[i] = i+1;
+            defaultRight[i]= C-i;
+        }
+        
+        for(int i=0; i<R; i++){
+            
+            
+            for(int j=0; j<C; j++){
+                
+                if(i==0){
+                    row[j] = matrix[i][j]-'0';
+                }else{
+                    if(matrix[i][j]=='0')
+                        row[j] = 0;
+                    else
+                        row[j] +=matrix[i][j]-'0';
+                }
+                
+            }
+            
+            //calculate for individual cumulative Row
+            maxx = max(maxx, findLargestInHistogram(row, defaultLeft, defaultRight));
+            
+        }//for
+        
+        
+        
+        return maxx;
+        
+    }
+    
+    
+    int findLargestInHistogram(vector<int> &row, vector<int> &defaultLeft, vector<int> &defaultRight){
+        
+        int N = row.size();
+        
+        vector<int> left(defaultLeft.begin(), defaultLeft.end());
+        vector<int> right(defaultRight.begin(), defaultRight.end());
+        
+        stack<int> st;
+        for(int i=0; i<N; i++){
+            
+            while(!st.empty() && row[st.top()] > row[i]){
+                right[st.top()] = i-st.top();
+                st.pop();
+            }
+            
+            if(!st.empty()){
+                left[i] = i-st.top();
+            }
+            
+            st.push(i);
+            
+            
+            
+        }
+        
+        
+        int maxx = INT_MIN;
+        for(int i=0; i<N; i++){
+            maxx = max(maxx, row[i]*(right[i]+left[i]-1));
+        }
+        
+        return maxx;
+        
+    }
+};
