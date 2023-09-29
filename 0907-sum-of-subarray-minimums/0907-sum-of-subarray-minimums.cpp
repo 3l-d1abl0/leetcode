82a8333040a3fbf85ca90688b7ce51d98a8d6a4e
Sameer <sameer.barha12@gmail.com>
2023-09-29 14:18:56 +0530

    Time: 78 ms (55.61%), Space: 45.6 MB (14.31%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -4,13 +4,27 @@ public:
     int minn;
     
     int sumSubarrayMins(vector<int>& arr) {
+    
         
-        vector<long int> left(arr.size());
-        vector<long int> right(arr.size());
+        /*
+            1. For every 'i' fidn the just lesser number on iots left and right
+            That 'i' is minimum in that left-right Range.
+            
+            2. Find the number of sub ranges in left-right which contains 'i'
+            'i' will be the minimum in all the subranges.
+            
+            3. 
         
-        for(int i=0; i<arr.size(); i++){
+        */
+        
+        int N = arr.size(); 
+        vector<long int> left(N);
+        vector<long int> right(N);
+        
+        //If no greater element founf on left and right
+        for(int i=0; i<N; i++){
             left[i] =(i+1);
-            right[i]= arr.size()-i;
+            right[i]= N-i;
         }
         
         //left 1->2->3->4
@@ -20,18 +34,48 @@ public:
         
         
         for(int i=0; i<arr.size(); i++){
-            
+            /*
+            If 'i' is less than [st.top()]
+            for st.top() 'i' is the leser element
+            */
             while(!st.empty() && arr[st.top()] >arr[i]){
                 right[st.top()] = i - st.top();
                 st.pop();
             }
             
-            if(st.empty()) left[i] = i+1;
-            else left[i] = i-st.top();
+            /* 'i' is greater than st.top()
+                so for 'i' , st.top() is the leser element on its left
+            */
+            if(!st.empty())
+                left[i] = i-st.top();
             
             st.push(i);
         }
         
+        
+        /*
+            eg:
+            2 9 7 8 [3] 4 6 1
+            
+            for element 3, left-min : 2, right-min: 1
+            
+            len [9-3] = 4 (M)
+            len [3-6] = 3 (N)
+            
+            len [9-6] = M+N-1
+            
+            Totla number of Subarray in the range M+N-1 which does not contain 3:
+            
+            [9 7 8] 3 [4 6]
+            (M-1)       (N-1)
+            
+            ((M-1) * M) /2      + ((N-1)*N)/2   ------ (I)
+        
+            Total number of subarray where 3 is the minimum in the range M+N-1
+            
+            = (M+n-1) (M+N)  /2            - (I) 
+            = boils down to M*N
+        */
         int sum = 0;
         for(int i=0; i<arr.size(); i++){
             sum = (sum + (left[i]* arr[i]* right[i])%MOD)%MOD;
