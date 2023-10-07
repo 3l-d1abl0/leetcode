0099b928fc8ff9d1af2f691456ef4f80221bf2b5
Sameer <sameer.barha12@gmail.com>
2023-10-07 22:57:27 +0530

    Time: 0 ms (100.00%), Space: 10 MB (23.66%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& arr, int x) {
+        
+        	int left =-1, right =arr.size();
+
+        while (right-left>1) {
+        
+			int mid = left + (right-left)/2;
+
+                        if (arr[mid] >= x) {
+							right = mid;
+                        } else {
+							left =mid;
+						}
+        }
+
+	return right;
+        
+    }
+};
