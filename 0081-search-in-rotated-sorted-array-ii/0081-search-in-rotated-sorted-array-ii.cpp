5c38e55b3fece874a8ef9c9a5ef59f310419ea61
Sameer <sameer.barha12@gmail.com>
2023-10-08 20:27:35 +0530

    Time: 3 ms (82.37%), Space: 14.3 MB (5.44%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,47 +1,53 @@
 class Solution {
 public:
     
-    bool rotatedBSItr(vector<int> &nums, int target, int l, int r){
+        int rotatedBSItr(vector<int> &nums, int target, int l, int r){
         
-        while(l<r){
+        while(l<=r){
             
             int mid = l+(r-l)/2;
             
+            //cout<<mid<<" "<<nums[mid]<<" tar"<<target<<endl;
             if(nums[mid]==target)
                 return true;
             
+            /*
+                [1,0,1,1,1]
+                0
+                Shrink the search Space
+            */
             if(nums[l]==nums[mid] && nums[mid]==nums[r]){
-                l++;
-                r--;
-            }else if(nums[l]<=nums[mid]){//ledt side is sorted
+                l = l+1;
+                r = r-1;
+                continue;
+            }
+            
+            if(nums[l]<=nums[mid]){//ledt side is sorted
                 
-                if(nums[l]<=target && target< nums[mid])
+                if(nums[l]<=target && target<= nums[mid])
                     r= mid-1;
                 else
                     l=mid+1;
                 
             }else{  //right side is sorted
                 
-                if(nums[mid]<target && target <=nums[r])
+                if(nums[mid]<=target && target <=nums[r])
                     l=mid+1;
                 else
                     r=mid-1;
             }
             
-        }
-        //l==r
-        //cout<<l<<" "<<r<<endl;
-        if(nums[l]==target)
-            return true;
-
-        else
-            return false;
+        }//while
+        
+         return false;
     }
     
     bool search(vector<int>& nums, int target) {
         
-        int l=0, r=nums.size()-1;
+        int st = 0, ed = nums.size();
         
-        return rotatedBSItr(nums, target, l, r);
+        
+        return rotatedBSItr(nums, target, st, ed-1);
+        
     }
 };
