4fcc3be24b327f11cf105c62a9580a32bdf9c2d2
Sameer <sameer.barha12@gmail.com>
2023-10-10 12:15:00 +0530

    Time: 36 ms (61.90%), Space: 19.3 MB (20.64%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -26,8 +26,10 @@ public:
         while(r-l>1){
             
             long long int k = l+(r-l)/2;
+            /* Find number of hours taken at a given rate 'k' of eating*/
             
             if(calculateHrs(piles, k) <=h){ //try to get more less till the condition satifies
+            /*If it take less than = h hrs to eat at a rate k, then try to decrease k. h willl increase*/
                 r=k;
             }else{
                 l=k;
