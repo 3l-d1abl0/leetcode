2c5dfeedfb8269afde4dd8ed2eb1e131a5349dc8
Sameer <sameer.barha12@gmail.com>
2023-10-10 18:34:33 +0530

    Time: 3 ms (75.90%), Space: 9.9 MB (40.37%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -21,22 +21,65 @@ public:
     
     int method2(vector<int> &arr, int k){
         
-        int low = 0, high = arr.size() -1;
+        /*   0 1 2 3 4
+            [2,3,4,7,11]
+             1 2 3 4  5  6
+   missing-        3  6
+        */  
+        
+        /*int low = 0, high = arr.size() -1;
         
         while(low <= high){
             
             int mid = low +(high-low)/2;
             
-            int missing = arr[mid] - (mid+1);
+            int expected = mid+1;
+            int missing = arr[mid] - expected;
             
-            if(k>missing){
+            if(missing<k){  //try to get closer to k
                 low = mid+1;
             }else{
                 high = mid-1;
             }
-        }
+        }*/
         
-        return low+k;
+        //int expected = l+1;
+        //int missing = arr[l] - expected;
+        /*
+            arr[high] + ( k- missing)
+            arr[high] + ( k- arr[high] + expected)
+            arr[high] + ( k- arr[high] + high+1)
+            
+            high+1+k
+            high+1  +k
+            low+k
+        */
+        //return high+1+k;
+        
+        int low = -1, high = arr.size();
+        
+        while(high-low > 1){
+            
+            int mid = low +(high-low)/2;
+            
+            int expected = mid+1;
+            int missing = arr[mid] - expected;
+            
+            if(missing<k){  //try to get closer to k
+                low = mid;
+            }else{
+                high = mid;
+            }
+        }//while
+        
+        /*middingMore = k - arr[low] + low +1
+            arr[low] + missing more
+            arr[low] + k - arr[low] + low +1
+            low+1+k
+        */
+        
+        //cout<<low<<" "<<high<<endl;
+        return low+1+k;
     }
     
      int findKthPositive(vector<int>& A, int k) {
