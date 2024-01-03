08cdbc292b4fc419446cf35e2f9a438eb7cb2184
Sameer <sameer.barha12@gmail.com>
2024-01-03 20:36:40 +0530

    Time: 252 ms (53.03%), Space: 110.9 MB (19.24%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,43 +1,72 @@
 
 class Solution {
 public:
-    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+    
+    vector<int> method1(int N, vector<vector<int>>& edges){
+        
+        /*
+            Start traversal from every node that is unvisited.
+            The nodes which remains unvisited are the one.
+        */
+        
         vector<int> ans;
-        vector<std::vector<int>> adj(n);
+        vector<vector<int>> adj(N);
         
         for (int i = 0; i < edges.size(); i++) {
-            int f = edges[i][0];
-            int s = edges[i][1];
-
-            adj[f].push_back(s);
+            //from -> to
+            adj[edges[i][0]].push_back(edges[i][1]);
         }
         
-        queue<int> q;
-        vector<bool> visited(n, false);
         
-        for (int i = 0; i < n; i++) {
-            if (visited[i]) continue;
+        //Start BFS traversal from each Node
+        queue<int> q;
+        vector<bool> visited(N, false);
+        
+        for (int i = 0; i < N; i++) {
             
+            //already visited
+            if (visited[i])
+                continue;
+            
+            //Strart BFS traversal from this Node
             q.push(i);
             
+            
+            //BFS
             while (!q.empty()) {
-                int rem = q.front();
-                q.pop();
                 
-                for (int node : adj[rem]) {
-                    if (visited[node]) continue;
-                    visited[node] = true;
-                    q.push(node);
-                }
-            }
-        }
+                int node = q.front(); q.pop();
+                
+                //visit all the neighbouring nodes and make it visited
+                for (int neighbour : adj[node]) {
+                    
+                    //already visited, no need to visit
+                    if (visited[neighbour])
+                        continue;
+                    
+                    visited[neighbour] = true;
+                    q.push(neighbour);
+                }//for
+                
+            }//while
+            
+        }//for
  
-        for (int i = 0; i < n; i++) {
+        //check the ones couldn't be visted
+        for (int i = 0; i < N; i++) {
             if (!visited[i]) {
                 ans.push_back(i);
             }
         }
         
         return ans;
+        
+    }
+    
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        
+        //1. Method1 : visit nodes
+        return method1(n, edges);
+        
     }
 };
