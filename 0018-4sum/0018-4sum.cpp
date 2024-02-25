29b71c873300f92db429351d08da6fa49674f89a
Sameer <sameer.barha12@gmail.com>
2024-02-25 21:34:23 +0530

    Time: 26 ms (71.63%), Space: 15.7 MB (69.55%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& arr, int target) {
+
+        //TC: O(NlogN + N63)
+        
+                int N = arr.size();
+
+        sort(arr.begin(), arr.end());
+
+        vector<vector<int>> ans;
+        //              N-3  N-2  N-1
+        for(int i=0; i<N-3; i++){
+
+            if(i>0 && arr[i]==arr[i-1])
+                continue;
+
+            for(int j=i+1; j<N-2; j++){
+
+
+                if(j>i+1 && arr[j]==arr[j-1])
+                    continue;
+
+
+                int lf = j+1;
+                int rt = N-1;
+                
+                long long int remain = target -arr[i];
+                remain += - arr[j];
+                while(lf<rt){
+                    
+                    if(arr[lf] + arr[rt] == remain){
+
+                        ans.push_back({arr[i], arr[j], arr[lf], arr[rt]});
+                        lf++;
+                        rt--;
+                        
+                        while( lf<N && arr[lf] == arr[lf-1]) lf++;
+                        while( rt>=0 && arr[rt] == arr[rt+1]) rt--;
+
+                    }else if (arr[lf] + arr[rt] < remain){
+                        lf++;
+                    }else{
+                        rt--;
+                    }
+                }//while
+
+            }//for
+
+        }//for
+
+      return ans;
+    }
+};
