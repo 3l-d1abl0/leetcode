b73f49c3f69b11c988411168cdaf0de7ab90576f
Sameer <sameer.barha12@gmail.com>
2024-02-27 00:49:18 +0530

    Time: 15 ms (25.99%), Space: 9.5 MB (14.87%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    
+    int getMaxFrex(unordered_map<char, int> &mp){
+        
+        int maxx=0;
+        for(auto pp: mp){
+            maxx= max(pp.second, maxx);
+        }
+        
+        return maxx;
+    }
+    
+    int method1(string &s, int k){
+        
+        
+        unordered_map<char, int> mp;
+        
+        int lf=0, rt=0, maxx=-1;
+        for(rt=0; rt<s.size(); rt++){
+            
+            
+            mp[s[rt]]++;
+            
+            int currLen = rt-lf+1;
+            while((currLen - getMaxFrex(mp)) >k){
+                
+                mp[s[lf]]--;
+                lf++;
+                
+                currLen = rt-lf+1;
+            }
+            
+            
+            maxx = max(maxx, rt-lf+1);
+        }//for
+        
+        return maxx;
+    }
+    
+    int characterReplacement(string s, int k) {
+        
+        
+        return method1(s, k);
+        
+        
+        int maxLen = INT_MIN;
+        int maxFreq = INT_MIN;
+        unordered_map<int,int> mp;
+        
+        int rt=0, lf=0;
+        for(rt=0; rt < s.size(); rt++){
+            
+            mp[s[rt]]++;
+            
+            maxFreq = max(maxFreq, mp[s[rt]]);
+            
+            int replaceLen = (rt-lf+1)-maxFreq;
+            if(replaceLen > k){ //cant replace as > k, hence make window small
+                mp[s[lf]]--;
+                lf++;
+            }else{
+                maxLen = max(maxLen, rt-lf+1);
+            }
+            
+            
+        }//for
+        
+        return maxLen;
+    }
+};
