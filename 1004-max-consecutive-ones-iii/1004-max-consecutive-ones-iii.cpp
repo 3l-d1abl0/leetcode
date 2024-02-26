714e7fdc7c2ec0e78427f9964c1b4031d9835c65
Sameer <sameer.barha12@gmail.com>
2024-02-27 02:04:11 +0530

    Time: 41 ms (68.59%), Space: 57.6 MB (64.20%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
-    
-    /*
-    Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
-    Output: 6
-    [1,1,1,0,0,{0,1,1,1,1,0}]
-    
-    
-    Input: nums = [0,0,{1,1,0,0,1,1,1,0,1,1},0,0,0,1,1,1,1], k = 3
-    Output: 10
-    
-    look for longes length with k zero in it
-    */
-    
     int longestOnes(vector<int>& nums, int k) {
-       
+     
         
-        int maxx =0;
-        int lf=0;
-        unordered_map<int, int> mp;
-        for(int i=0; i<nums.size(); i++){
+        int maxLen = INT_MIN;
+        
+        int onesCount =0;
+        
+        int lf=0, rt=0;
+        for(int rt=0; rt<nums.size(); rt++){
             
-            mp[nums[i]]++;
+            if(nums[rt]==1)
+                onesCount++;
             
-            while(mp[0]>k){
+            
+            //if widow needs to be shortened
+            int zeros = (rt-lf+1) - onesCount;
+            if(zeros>k){
                 
-                mp[nums[lf]]--;
+                if(nums[lf]==1)
+                    onesCount--;
+                
                 lf++;
             }
             
             
-            maxx = max(maxx, i-lf+1);
+            //Check for the lenth if zeros under replacement
+            zeros = (rt-lf+1) - onesCount;
+            if(zeros<=k){
+                maxLen = max(maxLen, rt-lf+1);
+            }
             
             
         }//for
         
-        return maxx;
         
+        return maxLen;
     }
 };
