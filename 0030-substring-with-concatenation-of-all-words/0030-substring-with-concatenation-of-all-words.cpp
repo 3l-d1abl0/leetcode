b1f53443feff79dfe50110f6afdaaacaddf25795
Sameer <sameer.barha12@gmail.com>
2024-02-28 22:29:37 +0530

    Time: 2237 ms (24.00%), Space: 380.1 MB (5.97%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -40,11 +40,74 @@ public:
     return false;
 }
     
+    vector<int> findWordConcatenation(const string &str, const vector<string> &words) {
+    unordered_map<string, int> wordFrequencyMap;
+    for (auto word : words) {
+      wordFrequencyMap[word]++;
+    }
+
+    vector<int> resultIndices;
+    int wordsCount = words.size(), wordLength = words[0].length();
+
+    for (int i = 0; i <= int(str.length()) - wordsCount * wordLength; i++) {
+      unordered_map<string, int> wordsSeen;
+      for (int j = 0; j < wordsCount; j++) {
+        int nextWordIndex = i + j * wordLength;
+        // get the next word from the string
+        string word = str.substr(nextWordIndex, wordLength);
+        if (wordFrequencyMap.find(word) ==
+            wordFrequencyMap.end()) {  // break if we don't need this word
+          break;
+        }
+
+        wordsSeen[word]++;  // add the word to the 'wordsSeen' map
+
+        // no need to process further if the word has higher frequency than required
+        if (wordsSeen[word] > wordFrequencyMap[word]) {
+          break;
+        }
+
+        if (j + 1 == wordsCount) {  // store index if we have found all the words
+          resultIndices.push_back(i);
+        }
+      }
+    }
+
+    return resultIndices;
+  }
     
+    
+        bool helper(string s,int word,unordered_map<string,int> mp){
+        for(int i = 0; i < s.size(); i += word){
+            string a = s.substr(i,word);
+            if(mp.find(a) != mp.end()){
+                if(--mp[a] < 0)return false;
+            }else return false;
+        }
+        return true;
+    }
+    
+    //driver
     vector<int> findSubstring(string s, vector<string>& words) {
         
+                vector<int>v; 
+        int word = words[0].size();
+        int len = words.size()*word;
+        unordered_map<string,int>mp;
+        for(auto it : words){
+            mp[it]++;
+        }
+        int a = 0;
+        while(a + len <= s.size()){
+            if(helper(s.substr(a,len),word,mp))v.push_back(a);
+            a++;
+        } 
+        return v;
         
-        	int listLength = words.size();
+    
+    return findWordConcatenation(s, words);
+        
+    /*int listLength = words.size();
 	int wordLength = words[0].size();
 
 	int totalSubstringlength = listLength*wordLength;
@@ -91,6 +154,6 @@ public:
 
 	return pos;//vector<int> (pos.begin(), pos.end());
         
-        
+      */  
     }
 };
