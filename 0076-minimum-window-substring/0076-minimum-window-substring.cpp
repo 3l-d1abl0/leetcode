590f1af5cab79f1107627f8a4137ef094bec075f
Sameer <sameer.barha12@gmail.com>
2024-02-28 19:23:16 +0530

    Time: 23 ms (43.18%), Space: 10.5 MB (36.50%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -6,6 +6,8 @@ public:
         vector<int> MP(58, 0);
         vector<int> M(58, 0);
         
+        
+        //Create freq array and count number of unique characters
         int unq =0;
         for(char ch: t){
             if(MP[ch-'A']==0)
@@ -19,16 +21,23 @@ public:
         int minn = INT_MAX, pos = -1;
         
         for(int rt=0; rt<s.size(); rt++){
+            
+            //process incoming element
             //cout<<unq<<" "<<ctr<<" "<<lf<<"-"<<rt<<endl;
             r = s[rt]-'A';
+            //continue if doesnt exist in pattern
             if(MP[r] == 0) continue;
             
+            
             M[r]++;
+            //same number of occurance as pattern
             if(M[r]==MP[r]){
                 ctr++;
             }
             
+            //if we have same number of unique charactrs as the pattern
             while(ctr==unq && lf<=rt){
+                cout<<lf<<" "<<rt<<endl;
                 if(minn>rt-lf+1){
                     minn = rt-lf+1;
                     pos= lf;
@@ -58,8 +67,51 @@ public:
             return s.substr(pos, minn);
     }
     
+    string findSubstring(const string &str, const string &pattern) {
+        int windowStart = 0, matched = 0, minLength = str.length() + 1, subStrStart = 0;
+        unordered_map<char, int> charFrequencyMap;
+        for (auto chr : pattern) {
+          charFrequencyMap[chr]++;
+        }
+
+        // try to extend the range [windowStart, windowEnd]
+        for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+          char rightChar = str[windowEnd];
+          if (charFrequencyMap.find(rightChar) != charFrequencyMap.end()) {
+            charFrequencyMap[rightChar]--;
+            if (charFrequencyMap[rightChar] >= 0) {  // count every matching of a character
+              matched++;
+            }
+          }
+
+          // shrink the window if we can, finish as soon as we remove a matched character
+          while (matched == pattern.length()) {
+            if (minLength > windowEnd - windowStart + 1) {
+              minLength = windowEnd - windowStart + 1;
+              subStrStart = windowStart;
+            }
+
+            char leftChar = str[windowStart++];
+            if (charFrequencyMap.find(leftChar) != charFrequencyMap.end()) {
+              // note that we could have redundant matching characters, therefore we'll decrement the
+              // matched count only when a useful occurrence of a matched character is going out of the
+              // window
+              if (charFrequencyMap[leftChar] == 0) {
+                matched--;
+              }
+              charFrequencyMap[leftChar]++;
+            }
+          }
+        }
+
+        return minLength > str.length() ? "" : str.substr(subStrStart, minLength);
+  }
+    
     string minWindow(string s, string t) {
         
-        return m1(s, t);
+        //return m1(s, t);
+        
+        
+        return findSubstring(s, t);
     }
 };
