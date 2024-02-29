c2aaf42133901e2a1139d685f758d21d15a1e9ac
Sameer <sameer.barha12@gmail.com>
2024-02-29 12:49:11 +0530

    Time: 184 ms (63.28%), Space: 94.8 MB (57.35%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,46 +1,21 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-     
         
+        unordered_map<int ,int> m;
+        long long ans = 0, s = 0, i = 0;
         
-        unordered_set<int> mp;
-        int N = nums.size();
-        
-        int lf =0;
-        long long int ans=0, sum =0;
-                          
-        for(int rt =0; rt< N; rt++){
-            
-            int inEle = nums[rt];
-            
-            
-            //If the incoming window already exists, shrink th window, till its last occurance
-            while(lf < rt && mp.count(inEle) !=0){
-                mp.erase(nums[lf]);
-                sum -= nums[lf];
-                lf++;
+        for(int j=0; j<nums.size(); j++) {
+            m[nums[j]]++;
+            s += nums[j];
+            if(k == j - i + 1) {
+                if(m.size() == k) ans = max(ans, s);
+                s -= nums[i];
+                m[nums[i]]--;
+                if(!m[nums[i]]) m.erase(nums[i]);
+                i++;
             }
-            
-            //Include it in the current window
-            sum += inEle;
-            mp.insert(inEle);
-            
-            
-            //churn elemets from left if  the window size is >=k    
-            while(lf<rt && mp.size() >k  ){
-                mp.erase(nums[lf]);
-                sum -= nums[lf];
-                lf++;
-            }
-            
-            
-            if(mp.size() ==k)
-                ans = max(ans, sum);
-        }//for
-                              
-        
-        
+        }
         return ans;
     }
 };
