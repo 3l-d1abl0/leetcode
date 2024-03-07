0bfe741891d10a6ef640fc4926ccb00a5c335c94
Sameer <sameer.barha12@gmail.com>
2024-03-07 15:53:09 +0530

    Time: 172 ms (70.28%), Space: 136.8 MB (80.00%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -5,19 +5,29 @@ public:
         
         deque<int> dq;
         vector<int> ans;
+        /*
+            use a dqque to maintain the window.
+            as soon a greater elements enter the window from the right,
+            the lesser  elements on the left has no chance to become the
+            max number because of a greater number on the right
+            
+            so keep popping the numbers from left if the are less than in coming element
+        */
         for(int i=0; i<nums.size(); i++){
             
-            //remove out of range elements
+            //1. remove out of range elements
             while(!dq.empty() && dq.front() <= i-k)
                 dq.pop_front();
             
-            //remove the smaller number before ele
+            //2. remove the smaller number before ele
             while(!dq.empty() && nums[dq.back()] <nums[i]){
                 dq.pop_back();
             }
-            //put the element
+            
+            //3. put the element
             dq.push_back(i);
             
+            //4. limit to start print the window
             if(i>=k-1)
                 ans.push_back(nums[dq.front()]);
         }
