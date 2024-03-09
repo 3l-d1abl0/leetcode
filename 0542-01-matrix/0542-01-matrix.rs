2c11f65a891a83679ff0b05573aa26ec2ef51bf8
Sameer <sameer.barha12@gmail.com>
2024-03-09 14:18:07 +0530

    Time: 20 ms (67.57%), Space: 3 MB (72.97%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.rs
+++ 0542-01-matrix/0542-01-matrix.rs
@@ -2,14 +2,17 @@ use std::collections::VecDeque;
 
 impl Solution {
     pub fn update_matrix(mat: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-        let m = mat.len();
-        let n = mat[0].len();
+        
+        
+        let R = mat.len();
+        let C = mat[0].len();
+        
         let mut queue = VecDeque::new();
-        let max_value = m * n as usize;
+        let max_value = R * C as usize;
         let mut result = mat.clone();
         
-        for i in 0..m {
-            for j in 0..n {
+        for i in 0..R {
+            for j in 0..C {
                 if mat[i][j] == 0 {
                     queue.push_back((i, j));
                 } else {
@@ -18,14 +21,18 @@ impl Solution {
             }
         }
         
-        let directions = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+        let directions = [(1, 0), (0, -1),  (-1, 0),  (0, 1)];
         
         while let Some((row, col)) = queue.pop_front() {
+            
             for (dr, dc) in &directions {
+                
                 let r = row as i32 + dr;
                 let c = col as i32 + dc;
-                if r >= 0 && r < m as i32 && c >= 0 && c < n as i32 && result[r as usize][c as usize] > result[row][col] + 1 {
+                
+                if r >= 0 && r < R as i32 && c >= 0 && c < C as i32 && result[r as usize][c as usize] > result[row][col] + 1 {
                     queue.push_back((r as usize, c as usize));
+                    
                     result[r as usize][c as usize] = result[row][col] + 1;
                 }
             }
