d6e715cea325286cd0f8a3359f649d6105897d34
Sameer <sameer.barha12@gmail.com>
2024-03-09 14:42:43 +0530

    Time: 133 ms (11.33%), Space: 57.3 MB (9.33%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -62,6 +62,61 @@ public:
         
     }
     
+    vector<vector<int>> method2(vector<vector<int>>& mat) {
+        
+        int R= mat.size();
+        int C= mat[0].size();
+        
+        int MAXVAL = R+C;
+        vector<vector<int>> result(R, vector<int>(R, MAXVAL));
+        
+        
+        for(int i=0;i<R; i++){
+            for(int j=0; j<C; j++){
+                
+                if(mat[i][j]==0)
+                    continue;
+                
+                int left=0, up=0;
+                if(i-1>=0)
+                    left = mat[i-1][j];
+                
+                if(j-1>=0)
+                    up = mat[i][j-1];
+                
+                
+                result[i][j] = min(left, up)+1;
+                
+            }
+        }//for i
+        
+        
+        for(int i=R-1; i>=0; i--){
+            for(int j=C-1; j>=0; j--){
+                
+                if(mat[i][j]==0)
+                    continue;
+                
+                int right=0, down =0;
+                
+                if(i+1 <=R)
+                    right = mat[i+1][j];
+                
+                if(j+1<=C)
+                    down = mat[i][j+1];
+                
+                
+                result[i][j] += (result[i][j], min(right, down)+1);
+                
+            }
+        }//for i
+        
+        
+        
+        return result;
+    }
+    
+    
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         
         /*
@@ -77,6 +132,13 @@ public:
         //Mehthod1:BFS  TC:O(M*N)
         //SC: O(2*(M*N))
         return method1(mat);
+        
+        
+        /*
+        Method2: DP, TC: O(M*N)
+        SC: O()
+        */
+        return method2(mat);
         
     }
 };
