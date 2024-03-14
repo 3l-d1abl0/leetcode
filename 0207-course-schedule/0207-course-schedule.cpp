4e0872cb4a94b1a5622d1039cbeac4e653c48ce4
Sameer <sameer.barha12@gmail.com>
2024-03-14 19:54:36 +0530

    Time: 20 ms (34.62%), Space: 16.8 MB (74.99%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -21,7 +21,7 @@ public:
 	
         for(int i=0; i<prerequisites.size(); i++){
 
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
 
         }
         
