ad42c2147fe3d7e468232a81db9dd100f533ffa5
Sameer <sameer.barha12@gmail.com>
2024-03-16 21:10:10 +0530

    Time: 21 ms (25.86%), Space: 17.3 MB (22.36%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
     
-    void BFS(int src, int dst, vector<int> &visited, vector<vector<pair<int, int>>> &adj, int k){
+    struct Node{
+        
+        int node;
+        int price;
+        int k;
+    };
+    
+    void BFS(int src, int dst, vector<int> &price, vector<vector<pair<int, int>>> &adj, int k){
      
         
-        queue<vector<int>> q;   //row,price, k
-        vector<int> vec{src, 0, k+1};
+        queue<Node> q;   //node,price, k
+        //vector<int> vec{src, 0, k+1};
         
-        q.push(vec);
-        visited[src] = 0;
+        q.push(Node{src, 0, k+1});
+        price[src] = 0;
         
         //cout<<"Loop"<<endl;
         while(!q.empty()){
             
-            vec = q.front(); q.pop();
+            Node state = q.front(); q.pop();
             
-            if(vec[2]==0 || dst==vec[0]) continue;
+            //at k+1 the node is dst
+            if(state.k==0 || dst==state.node)
+                continue;
             
             /*//visited[vec[0]] = vec[1];
             
@@ -29,14 +38,13 @@ public:
             //cout<<vec[2]<<" "<<vec[0]<<" "<<vec[1]<<endl;
             
             
-            for(auto row: adj[vec[0]]){
+            for(auto row: adj[state.node]){
             
-                if(vec[1]+row.second <= visited[row.first]){
+                if(state.price+row.second <= price[row.first]){
                     
-                    visited[row.first] = vec[1]+row.second;
+                    price[row.first] = state.price+row.second;
                     
-                    vector<int> node{row.first, vec[1]+row.second, vec[2]-1 };
-                    q.push(node);
+                    q.push( Node{row.first, price[row.first], state.k-1 } );
                 }
             
             
@@ -48,63 +56,30 @@ public:
         
     }
     
-    void DFS(int src, int dst, int curr_price, vector<bool> &visited, vector<vector<pair<int, int>>> &adj, int k, int &max_price){
-        
-        
-        //cout<<"K: "<<k<<" "<<src<<endl;
-        
-        //k stops have been exhausted, it has to be the destination or stop
-        if(k==0 && src!=dst)
-            return;
-        
-        // If you meet the destination while K>0
-        if(src==dst){
-            max_price = min(max_price, curr_price);
-            return;
-        }
-        
-        visited[src] = true;
-        
-        for(auto row: adj[src]){
-            
-            if(visited[row.first]==false && curr_price+row.second <= max_price){
-                
-                DFS(row.first, dst, curr_price+row.second, visited, adj, k-1, max_price);
-            }
-            
-            
-            
-        }
-        
-        visited[src] = false;
-        
-    }
     
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         
-        //ios_base::sync_with_stdio(false);
-        //cin.tie(NULL);
-        //cout.tie(NULL);
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         
         
         vector<vector<pair<int, int>>> adj(n);
         
-        for(auto ele: flights){
-            
+        for(auto ele: flights)            
             adj[ele[0]].push_back({ele[1], ele[2]});
             
-        }
+        //Default maxx Price
+        vector<int> price(n, 1e7);
         
-        vector<int> visited(n, 1e7);
         
-        
-                BFS(src, dst, visited, adj, k);
+        BFS(src, dst, price, adj, k);
                 
         
-        /*for(int ele: visited){
+        /*for(int ele: price){
             cout<<ele<<" ";
         }cout<<endl;
         */
-        return visited[dst]==1e7 ? -1: visited[dst];
+        return price[dst]==1e7 ? -1: price[dst];
     }
 };
