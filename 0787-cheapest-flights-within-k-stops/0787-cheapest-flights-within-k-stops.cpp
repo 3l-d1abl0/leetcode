f584bac82421525c40a9d599e1c882f96e81bb44
Sameer <sameer.barha12@gmail.com>
2024-03-17 00:09:44 +0530

    Time: 20 ms (31.34%), Space: 17.7 MB (16.15%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,76 +1,64 @@
 class Solution {
 public:
     
-    struct Node{
+    int dijkstra(int src, int dst, int k, vector<vector<pair<int, int>>> &adj, int n){
         
-        int node;
-        int price;
-        int k;
-    };
-    
-    void BFS(int src, int dst, vector<int> &price, vector<vector<pair<int, int>>> &adj, int k){
-     
         
-        queue<Node> q;   //node,price, k
+        priority_queue< vector<int>, vector<vector<int>>, greater<vector<int>> >  pq;
+        //smallest distance first
         
-        q.push(Node{src, 0, k+1});
-        price[src] = 0;
         
-        //cout<<"Loop"<<endl;
-        while(!q.empty()){
+        vector<int> dist(n+1, 1e7); dist[src]=0;
+        vector<int> hops(n+1, 1e7); hops[src]=0;
+        
+        
+        pq.push({ 0, 0, src});    //cost, hops, vertex
+        
+        
+        while(!pq.empty()){
             
-            Node state = q.front(); q.pop();
+            auto ele = pq.top(); pq.pop();
+            int currDist = ele[0], currHops = ele[1], node = ele[2];
+           
+           // cout<<"c:"<<currDist<<" h:"<<currHops<<" v:"<<node<<endl;
+            if(node==dst) return currDist;
             
-            //at k+1 the node is dst
-            if(state.k==0 || dst==state.node)
-                continue;
+            if(currHops==k+1) continue;
             
-            //If k stops have passed and not reached the destination, no point in going further
-            if(state.k==0)
-                continue;
-            
-            for(auto row: adj[state.node]){
-            
-                if(state.price+row.second <= price[row.first]){
+            for(auto row: adj[node]){
+                
+                if(currDist + row.second < dist[row.first] || currHops+1 <hops[row.first]){
+                    //cout<<node<<"->"<<row.first<<endl;    
+                    dist[row.first] = currDist +row.second;
+                    hops[row.first] = currHops+1;
                     
-                    price[row.first] = state.price+row.second;
-                    
-                    q.push( Node{row.first, price[row.first], state.k-1 } );
+                    pq.push({dist[row.first], hops[row.first], row.first});
                 }
-
-            }//for
+                
+            }
             
-        }//while
+            
+        }
         
         
-    }
-    
-    //return method1(n, adj, src, dst, k);
-    int method1(int n, vector<vector<pair<int, int>>> &adj, int src, int dst, int k) {
+        /*for(int i=0; i<n; i++){
+            cout<<dist[i]<<","<<hops[i]<<" ";
+        }*/
         
-        //Default maxx Price
-        vector<int> price(n, 1e7);
-        
-        BFS(src, dst, price, adj, k);
-        
-        return price[dst]==1e7 ? -1: price[dst];
+        return -1;
         
     }
     
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
         
         
-        vector<vector<pair<int, int>>> adj(n);
+        vector<vector<pair<int, int>>> adj (n);
+        for(auto row: flights){
+            adj[row[0]].push_back({row[1], row[2]}); //v, wt
+        }
         
-        for(auto ele: flights)            
-            adj[ele[0]].push_back({ele[1], ele[2]});
-    
         
-        return method1(n, adj, src, dst, k);    //16ms BFS
-        
+        return dijkstra(src, dst, k, adj, n);
     }
 };
