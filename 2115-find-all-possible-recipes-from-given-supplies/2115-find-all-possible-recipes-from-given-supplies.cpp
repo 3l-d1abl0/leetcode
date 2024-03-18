7aead36878de45801c0008a27dea880ac73dc254
Sameer <sameer.barha12@gmail.com>
2024-03-18 16:51:18 +0530

    Time: 1155 ms (8.94%), Space: 97.2 MB (96.79%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
@@ -1,54 +1,56 @@
 class Solution {
 public:
-    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+    
+    vector<string> method1(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies){
         
-        int N = recipes.size();
-        
-        unordered_map<string, int> indegree;
-        unordered_map<string, vector<string> > adj;
-        
-        unordered_set<string> ress;
-        for(string res: recipes){
-            ress.insert(res);
-        }
-        
-        for(int i=0; i<N;i ++){
-            
-            for(string ingr: ingredients[i]){
-                
-                
-                adj[ingr].push_back(recipes[i]);
-                indegree[recipes[i]]++;
-            }
-        }
-        
-        
-        queue<string>q;
-        //all ingredient in the supies has indegree 0
-        for(string sup: supplies){
-            q.push(sup);
-        }
+        //create a set of all supplies
+        unordered_set<string> suppliesSet(supplies.begin(), supplies.end());
         
         vector<string> ans;
-        //process the indegree 0
-        while(!q.empty()){
+        
+        int N = recipes.size();
+        bool newSupply = false;
+        do{
             
-            string ing = q.front(); q.pop();
-            
-            if(ress.find(ing)!=ress.end())
-                ans.push_back(ing);
-            
-            
-            for(string in: adj[ing]){
-                indegree[in]--;
-                
-                
-                if(indegree[in]==0){
-                    q.push(in);
+            newSupply = false;
+            for(int idx=0; idx<N; idx++){
+
+                //if recepie alread exist
+                if(suppliesSet.find(recipes[idx])!= suppliesSet.end())
+                    continue;
+
+
+                bool found = true;
+                for(string ingredient: ingredients[idx]){
+
+                    //does not exists
+                    if(suppliesSet.find(ingredient)==suppliesSet.end()){
+                        found = false;
+                        break;
+                    }
+
                 }
-            }
-        }//while
+
+                //all ingredients for recipe was found
+                if(found==true){
+                    suppliesSet.insert(recipes[idx]);
+                    ans.push_back(recipes[idx]);
+                    newSupply = true;
+                }
+
+            }//for
+            
+        }while(newSupply==true);
+        
         
         return ans;
+    }
+    
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+        
+        
+        //Method1: Using Sets
+        return method1(recipes, ingredients, supplies);
+        
     }
 };
