ae37fa2b255446dba3698d4c4915a2ad58899c84
Sameer <sameer.barha12@gmail.com>
2024-03-19 14:06:34 +0530

    Time: 42 ms (23.64%), Space: 19.9 MB (16.26%) - LeetHub

---
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,126 @@
+class Solution {
+public:
+    
+    int floydWarshall(int n, vector<vector<int>>& edges, int distanceThreshold){
+        
+        vector<vector<int>> dist(n, vector<int> (n, INT_MAX));
+		for (auto it : edges) {
+			dist[it[0]][it[1]] = it[2];
+			dist[it[1]][it[0]] = it[2];
+		}
+		
+        for (int i = 0; i < n; i++) dist[i][i] = 0;
+        
+        
+		for (int via = 0; via < n; via++) {
+		
+            for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					if (dist[i][via] == INT_MAX || dist[via][j] == INT_MAX)
+						continue;
+					dist[i][j] = min(dist[i][j], dist[i][via] + dist[via][j]);
+				}
+			}
+		}
+
+		int cntCity = n;
+		int cityNo = -1;
+		for (int city = 0; city < n; city++) {
+			int cnt = 0;
+			for (int adjCity = 0; adjCity < n; adjCity++) {
+				if (dist[city][adjCity] <= distanceThreshold)
+					cnt++;
+			}
+
+			if (cnt <= cntCity) {
+				cntCity = cnt;
+				cityNo = city;
+			}
+		}
+        
+		return cityNo;
+        
+        
+    }
+    
+    void dijks(int source, vector<int> &dist, vector<vector<pair<int,int>>> &adj){
+        
+        //Min Heap - weight, node
+        priority_queue< pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> >pq;
+        
+        dist[source] = 0;
+        
+        pq.push({0, source});
+        
+        
+        while(!pq.empty()){
+            
+            pair<int, int> node = pq.top();
+            pq.pop();
+            
+            for(pair<int, int> ele: adj[node.second]){
+                
+                if(dist[ele.first] > dist[node.second]+ele.second){
+                    dist[ele.first] = dist[node.second]+ele.second;
+                    
+                    pq.push({dist[ele.first], ele.first});
+                }
+                
+            }
+            
+        }
+        
+        
+        
+    }
+    
+    int Dijkstra(int n, vector<vector<int>>& edges, int distanceThreshold){
+        
+        vector<vector<pair<int,int>>> adj(n);//vector<pair<int, int>>);
+        
+        for(int i=0;i<edges.size();i++){
+            int u = edges[i][0], v = edges[i][1], w = edges[i][2];   
+            adj[u].push_back({v,w});
+            adj[v].push_back({u,w});
+        }
+        
+        int MAXCITY = n;
+        int city = -1;
+        
+        //For every node as source, apply Dijkstra
+        for(int i=0; i<n; i++){
+            
+            vector<int> dist(n, INT_MAX);
+            dijks(i, dist, adj);
+            
+            
+            /*
+            Check how many are reachable from the given source,
+            and how many have the Threashold
+            */
+            int ctr =0;
+            for(int j=0; j<n; j++){
+                //cout<<dist[j]<<" ";
+                if(i==j || dist[j]==INT_MAX) continue;
+                if(dist[j]<=distanceThreshold)
+                    ctr++;
+            }
+            //cout<<endl;
+            if(ctr<=MAXCITY){
+                MAXCITY = ctr;
+                city = i;
+            }
+        }//for
+        
+        return city;
+           
+    }
+    
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        
+        //return floydWarshall(n, edges, distanceThreshold);  //O(n^3)
+        
+        return Dijkstra(n, edges, distanceThreshold);   //O(ElogV)
+        
+    }
+};
