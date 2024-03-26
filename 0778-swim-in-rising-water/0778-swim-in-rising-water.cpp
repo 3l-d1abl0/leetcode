0fedcf0cdbb97a26eb98a743c6dc94c1af14d048
Sameer <sameer.barha12@gmail.com>
2024-03-27 03:01:39 +0530

    Time: 14 ms (62.51%), Space: 12.4 MB (80.66%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -1,11 +1,25 @@
 class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
+        /*
+        You can travel from one cell to another cell with zero time if they are below or equal to current
+        water level.
         
+        if a neighbour's height is greater than the current time ,
+        you will have to wait for (neigh's height - t) to swim to neight.
+        
+        Basically, you have
+        
+        For every cell store the wait time to reach the node.
+        If you can reach a cell with smaller wait tiem,
+        preocess it again.
+        Basically Dijkastra.
+        
+        */
         
         int N = grid.size();
         
-        
+        //<wait_time, cell_coords>
         priority_queue< pair<int, pair<int, int>>, vector<pair<int, pair<int, int>> >,  greater<pair<int, pair<int, int>> > >minHeap;
         
         vector<vector<bool>> vis(N, vector<bool> (N, false));
@@ -13,6 +27,8 @@ public:
         
         //start fron top left
         vis[0][0] = true;
+        
+        //maxHeightClimbed, cell coords 
         minHeap.push( {grid[0][0], {0, 0} });   //height, {x, y}
         
         
@@ -41,6 +57,8 @@ public:
                 
                 
                 vis[xx][yy] = true;
+                
+                //If the height climbed is greater than the next grid heifht - just move 
                 if(maxHeight >= grid[xx][yy]){
                     minHeap.push({maxHeight, {xx, yy}});
                 }else{
