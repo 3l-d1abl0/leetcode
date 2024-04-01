c3409f67118caa8c3bfb08825e4e00e66b5f383e
Sameer <sameer.barha12@gmail.com>
2024-04-01 23:06:48 +0530

    Time: 286 ms (93.15%), Space: 116.5 MB (81.51%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
         
-        
+        //Sort the intervals by start time - ascending
         sort(intervals.begin(), intervals.end(), [](const vector<int> &a, const vector<int> &b){
             return a[0]<b[0];
         });
@@ -10,17 +10,20 @@ public:
         vector<pair<int, int>> qi;
         for(int i=0; i<queries.size(); i++)
             qi.push_back({queries[i], i});
+        
+        //Sort the Queries in Ascending Order
         sort(qi.begin(), qi.end());
         
         //auto comp = []( pair<int, int> a, pair<int, int> b ) { return a.second > b.second; };
         
         auto comp = [](const pair<int, int> &a, const pair<int, int> &b){
             
+            //If bot interval is of same size
             if((a.second -a.first) == (b.second - b.first))
-                return a.first > b.first;
+                return a.first > b.first;   //smallest sart time first
 
             
-            return (a.second -a.first) > (b.second - b.first); //smallest first
+            return (a.second -a.first) > (b.second - b.first); //smallest interval first
             
         };
         
@@ -36,6 +39,7 @@ public:
         
             int minn = INT_MAX;
             
+            //all the Interval whose start time is lesss than query - put them in heap
             while(i<N && intervals[i][0]<= pp.first){
                 pq.push({intervals[i][0], intervals[i][1]});
                 //cout<<"<-- "<<intervals[i][0]<<"  , "<<intervals[i][1]<<endl;
@@ -44,7 +48,7 @@ public:
             
             
             //pop out the ranges which ends before the Query
-            while(!pq.empty()  && pp.first > pq.top().second){
+            while(!pq.empty()  && pq.top().second < pp.first){
                 //cout<<"--> "<<pq.top().first<<"   "<<pq.top().second<<endl;
                 pq.pop();
             }
