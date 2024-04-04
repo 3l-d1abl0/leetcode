62d3c04d3e2949f655d7eb9942283e2d98d1e693
Sameer <sameer.barha12@gmail.com>
2024-04-04 22:24:15 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (45.75%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
+    
+    int countWaysUtil(int i, int j, vector<vector<int>>& memo) {
+    // Base case: If we reach the top-left corner (0, 0), there is one way.
+    if (i == 0 && j == 0)
+        return 1;
+
+    // If we go out of bounds or reach a blocked cell, there are no ways.
+    if (i < 0 || j < 0)
+        return 0;
+
+    // If we have already computed the number of ways for this cell, return it.
+    if (memo[i][j] != -1)
+        return memo[i][j];
+
+    // Calculate the number of ways by moving up and left recursively.
+    int up = countWaysUtil(i - 1, j, memo);
+    int left = countWaysUtil(i, j - 1, memo);
+
+    // Store the result in the dp table and return it.
+    return memo[i][j] = up + left;
+}
+    
     int uniquePaths(int m, int n) {
+     
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+
+        // Call the utility function with the bottom-right cell as the starting point.
+        return countWaysUtil(m - 1, n - 1, dp);
         
-        vector<int> dp (n, 1);
-        
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                
-                dp[j] += dp[j-1];
-            }
-        }
-        
-        //print(dp);
-        return dp[n-1];
-        
-    }
-    void print(vector<vector<int>> &dp){
-        
-        for(auto row: dp){
-            for(int ele: row){
-                cout<<ele<<" ";
-            }
-            cout<<endl;
-        }
     }
 };
