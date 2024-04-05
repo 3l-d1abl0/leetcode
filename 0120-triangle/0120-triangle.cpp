ca53e7810a4d26678893db761915baa338bcb453
Sameer <sameer.barha12@gmail.com>
2024-04-05 13:07:14 +0530

    Time: 4 ms (73.58%), Space: 11.4 MB (26.78%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -18,14 +18,50 @@ public:
         return memo[i][j] = min(down, diagonal) + triangle[i][j];
     }
     
+    
+    int bottomUp(vector<vector<int>>& triangle) {
+        
+        int N = triangle.size();
+        
+        vector<vector<int>> dp(N, vector<int> (N, INT_MAX));
+        
+        // Last Row is the Starting Point
+        for (int j = 0; j < N; j++) {
+            dp[N - 1][j] = triangle[N - 1][j];
+        }
+
+        //From Second Last row to first
+        for (int i = N - 2; i >= 0; i--) {
+            
+            //right to left
+            for (int j = i; j >= 0; j--) {
+                
+                // j+1 always valid
+                int down =  dp[i + 1][j];
+                int diagonal = dp[i + 1][j + 1];
+
+                // Store the minimum of the two possible paths in dp
+                dp[i][j] = min(down, diagonal) +triangle[i][j];
+            }
+        }
+
+        // The top-left cell of dp now contains the minimum path sum
+        return dp[0][0];
+        
+        
+    }
+    
     int minimumTotal(vector<vector<int>>& triangle) {
         
         //1. Recursion+Memoization
         
-        int N = triangle.size();
+        //int N = triangle.size();
         
-         vector<vector<int> > memo(N, vector<int>(N, -1));
-        return recMemo(0, 0, triangle, N, memo);
+        //vector<vector<int> > memo(N, vector<int>(N, -1));
+        //return recMemo(0, 0, triangle, N, memo);
+        
+        //2. Tabulation
+        return bottomUp(triangle);
         
     }
 };
