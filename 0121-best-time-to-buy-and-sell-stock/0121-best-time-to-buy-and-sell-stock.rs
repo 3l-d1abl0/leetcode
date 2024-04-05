fc97d435d3714a6112815fa231ec8e7c2bfaa144
Sameer <sameer.barha12@gmail.com>
2024-04-05 22:59:46 +0530

    Time: 11 ms (49.81%), Space: 2.8 MB (100.00%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.rs
@@ -0,0 +1,22 @@
+impl Solution {
+    
+    
+    fn way1(prices: Vec<i32>) -> i32{
+        
+        let (mut profit, mut buy) = (0, prices[0]);
+
+        for i in 1..prices.len() {
+            profit = i32::max(profit, prices[i] - buy);
+            buy = i32::min(buy, prices[i]);
+        }
+
+        return profit;
+        
+    }
+    
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        
+        return Solution::way1(prices);
+        
+    }
+}
