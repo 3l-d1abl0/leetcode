0afbb2d8711f4a5ac87eb4cd04cd783c424959e9
Sameer <sameer.barha12@gmail.com>
2024-04-05 12:33:21 +0530

    Time: 7 ms (59.30%), Space: 12.5 MB (38.55%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -25,13 +25,49 @@ public:
         
     }
     
-    int minPathSum(vector<vector<int>>& grid) {
+    int bottomUp(vector<vector<int>> &grid){
         
-        //1. Recursion+Memo
         int M = grid.size();    //row
         int N = grid[0].size();    //col
         
-        vector<vector<int>> memo(M, vector<int>(N, -1));
-        return recMemo(M-1, N-1, grid, memo);
+        vector<vector<int>> dp(M, vector<int>(N, INT_MAX));
+        
+        for(int i=0; i<M; i++){
+            for(int j=0; j<N; j++){
+                
+        
+                if(i==0 && j==0){
+                    dp[i][j] = grid[0][0];
+                    continue;
+                }
+
+                int up=INT_MAX;
+                int left = INT_MAX;
+                if(i-1>=0)
+                    up = dp[i-1][j];
+
+                if(j-1>=0)
+                    left = dp[i][j-1];
+
+
+                dp[i][j] = min(up, left)+grid[i][j];
+                
+            }
+        }//for i
+        
+        return dp[M-1][N-1];
+    }
+    
+    int minPathSum(vector<vector<int>>& grid) {
+        
+        //1. Recursion+Memo
+        //int M = grid.size();    //row
+        //int N = grid[0].size();    //col
+        
+        //vector<vector<int>> memo(M, vector<int>(N, -1));
+        //return recMemo(M-1, N-1, grid, memo);
+        
+        
+        return bottomUp(grid);
     }
 };
