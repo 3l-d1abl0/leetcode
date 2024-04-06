be7127b6102379f27fae112d488c1d136b76e75e
Sameer <sameer.barha12@gmail.com>
2024-04-06 15:51:09 +0530

    Time: 12 ms (44.08%), Space: 16.4 MB (34.32%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,28 +1,6 @@
 class Solution {
 public:
     
-    int memo[1000][2][100];
-    
-	int dp(int i, int buy, int k, vector<int>& prices) {
-		if (i == prices.size()) return 0;
-		if (memo[i][buy][k] != -1) return memo[i][buy][k];
-		// dont buy/sell
-		
-        if (k == 0) return 0;
-        
-        int ans = dp(i + 1, buy, k, prices);
-		
-		// buy
-		if (buy) {
-			ans = max(ans, -prices[i] + dp(i + 1, false, k, prices));
-		}
-		else {
-			ans = max(ans, prices[i] + dp(i + 1, true, k - 1, prices));
-		}
-
-		return memo[i][buy][k] = ans;
-	}
-    
     int recMemo(int idx, int buy, int tran, int K, vector<int> &prices, vector<vector<vector<int>>> &memo){
         
         if(idx== prices.size() || tran==K){
@@ -47,6 +25,55 @@ public:
         return memo[idx][buy][tran] = max(skipping, transaction);
     }
     
+    
+     int bottomUp(vector<int> &prices, int K){
+        
+         int N = prices.size();
+         
+         /*
+         [N][buy][K]    --> K transaction remaining
+         
+         */
+         vector<vector<vector<int>>> dp(N+1, vector<vector<int>> (2, vector<int> (K+1, -1e7)));
+         
+         //Basecase- when idx == N 
+         for(int buy=0; buy<=1; buy++){
+             for(int k=0; k<=K; k++){
+                 dp[N][buy][k] = 0;
+             }
+         }
+         //base case - when transaction =K
+         for(int idx=N-1; idx>=0; idx--){
+             for(int buy=0; buy<=1; buy++){
+                 dp[idx][buy][K] = 0;
+             }
+         }
+         
+         for(int idx=N-1; idx>=0; idx--){
+             
+             
+             for(int k=K-1; k>=0; k--){
+                 for(int buy=0; buy<=1; buy++){
+                     
+                     int skipping = dp[idx+1][buy][k];
+                     
+                     int transaction;
+                     //can buy
+                     if(buy){
+                         transaction = -prices[idx]+dp[idx+1][0][k];
+                     }else{
+                         transaction = prices[idx]+dp[idx+1][1][k+1];
+                     }
+                     
+                     dp[idx][buy][k]= max(skipping, transaction);
+                 }
+             }
+         }
+         
+         
+         //can buy, and o transaction remaining
+         return dp[0][1][0];
+    }
     
     
     int topDown(vector<int> &stocks, int k){
@@ -198,18 +225,24 @@ public:
     
     int maxProfit(int K, vector<int>& prices) {
         
+        /*
+        A transaction constitutes of buy ans sell,
+        
+        buy then sell - transaction 1
+        
+        buy then sell - transaction 2
+        ...
+        buy then sell - transaction K
+        
+        */
+        
         int N = prices.size();
-        
-        //1.Recur +memo
-        //memset(memo, -1, sizeof(memo));
-		//return dp(0, true, K, prices);
-        
         //1.Recur+ Memoization
-        vector<vector<vector<int>>> memo(N, vector<vector<int>> (2, vector<int> (K, -1)));
-        return recMemo(0, true, 0, K, prices, memo);
+        //vector<vector<vector<int>>> memo(N, vector<vector<int>> (2, vector<int> (K, -1)));
+        //return recMemo(0, true, 0, K, prices, memo);
         
-        //2. TopDown DP
-        //return topDown(prices, K);
+        //2. bottomUp
+        return bottomUp(prices, K);
         
         //Optimizign to 2D DP
         //return topDownSpaceOpti(prices, K);
