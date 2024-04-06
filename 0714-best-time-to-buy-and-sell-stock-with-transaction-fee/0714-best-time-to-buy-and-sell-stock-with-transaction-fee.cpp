ecc089410bf142c833b95cd9fe09879e87cc7450
Sameer <sameer.barha12@gmail.com>
2024-04-06 18:46:33 +0530

    Time: 159 ms (33.58%), Space: 79.6 MB (54.23%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -121,7 +121,7 @@ public:
         
         int N = stocks.size();
         
-        //1.Crest Trough
+        //1.Crest Trough - WA
         //return maxProfit2(stocks, fee);
         
         
@@ -136,35 +136,7 @@ public:
         
         //Bottom Up - Space Optimization
         return bottomUpOpti(stocks, fee);
-        
-        vector<vector<int>> dp(N+1, vector<int> (2,0));
-        
-        //dp[N] = 0;
-        
-        for(int i=N-1; i>=0; i--){
-            
-            
-            for(int buy=1; buy>=0; buy--){
-                
-                
-                int profit =0;
-                
-                if(buy){
-                    
-                    profit = max(-stocks[i]+ dp[i+1][0], 0+ dp[i+1][1]);
-                    
-                }else{
-                    profit = max(stocks[i]+ dp[i+1][1]-fee, 0 + dp[i+1][0]);
-                }
-                
-                
-                dp[i][buy] = profit;
-            }
-            
-        }
-        
-        //printDP(dp);
-        return dp[0][1];
+
         
         
     }
