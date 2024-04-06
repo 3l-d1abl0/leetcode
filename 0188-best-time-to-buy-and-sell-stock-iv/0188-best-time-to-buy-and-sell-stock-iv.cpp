c79e9fdd5d52405c053dcf00a24cf85b3dd9d25e
Sameer <sameer.barha12@gmail.com>
2024-04-06 16:21:35 +0530

    Time: 13 ms (40.19%), Space: 17.4 MB (13.00%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -36,30 +36,19 @@ public:
          */
          vector<vector<vector<int>>> dp(N+1, vector<vector<int>> (2, vector<int> (K+1, -1e7)));
          
-         //Basecase- when idx == N 
-         /*for(int buy=0; buy<=1; buy++){
-             for(int k=0; k<=K; k++){
-                 dp[N][buy][k] = 0;
-             }
-         }
-         //base case - when transaction =K
-         for(int idx=N-1; idx>=0; idx--){
-             for(int buy=0; buy<=1; buy++){
-                 dp[idx][buy][K] = 0;
-             }
-         }*/
-         
          for(int idx=N; idx>=0; idx--){
              
              
              for(int k=K; k>=0; k--){
                  for(int buy=0; buy<=1; buy++){
                      
-                     
+                    //Basecase- when idx == N             
                      if(idx==N){
                          dp[idx][buy][k] = 0;
                          continue;
                      }
+                     
+                     //base case - when transaction =K
                      if(k==K){
                          dp[idx][buy][k] = 0;
                          continue;
@@ -85,76 +74,61 @@ public:
          return dp[0][1][0];
     }
     
-    
-    int topDown(vector<int> &stocks, int k){
+     
+     int bottomUpOpti(vector<int> &prices, int K){
         
-        int N = stocks.size();
-        
-        vector<vector<vector<int>>> dp(N+1, vector<vector<int>> (k+1, vector<int> (2, 0)));
-        
-        //Base case N == 0 and K==0 all 0
-        
-        for(int idx = N-1; idx>=0; idx--){
-            for(int K=1; K <=k; K++){
-                for(int buy = 0; buy <=1; buy++){
-                        
-        
-                dp[idx][K][buy] = dp[idx+1][K][buy]; //recur(idx+1, buy, K, stocks, N, memo);  //skip
-
-                if(buy){
-                    dp[idx][K][buy] = max(dp[idx][K][buy] , -stocks[idx] +dp[idx+1][K][0]);
-
-                }else{
-                    dp[idx][K][buy] = max(dp[idx][K][buy] , stocks[idx] +dp[idx+1][K-1][1]);
-                }
-                    
-                    
-                    
-                }
-            }
-            
-        }//for
-        
-        return dp[0][k][1];
-        
+         int N = prices.size();
+         
+         /*
+         [N][buy][K]    --> K transaction remaining
+         
+         */
+         //vector<vector<vector<int>>> dp(N+1, vector<vector<int>> (2, vector<int> (K+1, -1e7)));
+         
+         vector<vector<int>> prev(2, vector<int> (K+1, -1e7));
+         
+         for(int idx=N; idx>=0; idx--){
+             
+             vector<vector<int>> curr(2, vector<int> (K+1, -1e7));
+             
+             for(int k=K; k>=0; k--){
+                 for(int buy=0; buy<=1; buy++){
+                     
+                    //Basecase- when idx == N             
+                     if(idx==N){
+                         curr[buy][k] = 0;
+                         continue;
+                     }
+                     
+                     //base case - when transaction =K
+                     if(k==K){
+                         curr[buy][k] = 0;
+                         continue;
+                     }
+                     
+                     int skipping = prev[buy][k];
+                     
+                     int transaction;
+                     //can buy
+                     if(buy){
+                         transaction = -prices[idx]+prev[0][k];
+                     }else{
+                         transaction = prices[idx]+prev[1][k+1];
+                     }
+                     
+                     curr[buy][k]= max(skipping, transaction);
+                 }
+             }
+             
+             prev=curr;
+         }
+         
+         
+         //can buy, and o transaction remaining
+         return prev[1][0];
     }
     
-    int topDownSpaceOpti(vector<int> &stocks, int k){
-        
-        int N = stocks.size();
-        
-        vector<vector<int>> prev (k+1, vector<int> (2, 0));
-        vector<vector<int>> curr (k+1, vector<int> (2, 0));
-        
-        //Base case N == 0 and K==0 all 0
-        
-        for(int idx = N-1; idx>=0; idx--){
-            for(int K=1; K <=k; K++){
-                for(int buy = 0; buy <=1; buy++){
-                        
-        
-                curr[K][buy] = prev[K][buy]; //recur(idx+1, buy, K, stocks, N, memo);  //skip
-
-                if(buy){
-                    curr[K][buy] = max(curr[K][buy] , -stocks[idx] +prev[K][0]);
-
-                }else{
-                    curr[K][buy] = max(curr[K][buy] , stocks[idx] +prev[K-1][1]);
-                }
-                    
-                    
-                    
-                }
-            }
-            
-            prev = curr;
-            
-        }//for
-        
-        return curr[k][1];
-        
-        
-    }
+    
     
     
         int altRecur(int idx, int txNo, vector<int> &stocks, int K, vector<vector<int>> &memo){
@@ -252,10 +226,10 @@ public:
         //return recMemo(0, true, 0, K, prices, memo);
         
         //2. bottomUp
-        return bottomUp(prices, K);
+        //return bottomUp(prices, K);
         
         //Optimizign to 2D DP
-        //return topDownSpaceOpti(prices, K);
+        return bottomUpOpti(prices, K);
         
         
         
