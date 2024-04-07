53cec5dc2acc4cd670abaf0293486a15b810c638
Sameer <sameer.barha12@gmail.com>
2024-04-08 01:44:07 +0530

    Time: 2 ms (90.72%), Space: 10.9 MB (76.61%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -26,8 +26,88 @@ public:
         
     }
     
+    void recur(int idx, vector<int>& nums, vector<vector<int>> &ans) {
+        
+        
+        if(idx==nums.size()){    
+            ans.push_back(nums);
+            return;
+        }
+        
+        
+        for(int i=idx; i<nums.size(); i++){
+            
+            if(i!= idx && nums[i]==nums[idx])
+                continue;
+            swap(nums[idx], nums[i]);
+            recur(idx+1, nums, ans);
+            swap(nums[idx], nums[i]);
+        }
+        
+    }
+    
+    void recurMap(int idx, vector<int>& nums, vector<vector<int>> &ans) {
+        
+        
+        if(idx==nums.size()){    
+            ans.push_back(nums);
+            return;
+        }
+        
+        //map at level idx
+        unordered_set<int> uniqueSet;
+        for(int i = idx; i < nums.size(); i++) {
+            
+            if(uniqueSet.find(nums[i])!= uniqueSet.end()) {
+                continue;
+            }
+            
+            uniqueSet.insert(nums[i]);
+            
+            swap(nums[i], nums[idx]);
+            recurMap(idx+1, nums, ans);
+            swap(nums[i], nums[idx]);
+            
+        }
+        
+    }
+    
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-     
+        
+        //1. Method1
+        /*vector<vector<int>> ans;
+        recur(0, nums, ans);
+        return ans;
+        This method fails because it is able to stop swapiing 
+        of an element of the same vale at idx.
+        
+        but it is not able to stop the swapping of multiple values different than
+        'idx'
+        
+        eg:
+        2211
+        
+        2[0] <-->1 [2]
+        2[0] <-->1[3]
+        
+        Instread use Map to keep check of the elemts
+        used at particular idx /level
+        
+        
+        ///////////////
+        
+        //2.Medthod2
+        vector<vector<int>> ans;
+        recurMap(0, nums, ans);
+        return ans;
+        */
+        
+        
+        //3.Method 3
+        /*
+            Create a frequency map
+            any any position (idx) place the unique chars
+        */
         int N = nums.size();
         
         vector<vector<int>> ans;
@@ -44,5 +124,6 @@ public:
         
         
         return ans;
+    
     }
 };
