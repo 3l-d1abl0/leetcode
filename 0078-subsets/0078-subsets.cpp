e3f9a7528210fcf8a76262e8fbba07bab9ab7217
Sameer <sameer.barha12@gmail.com>
2024-04-08 02:14:19 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (64.48%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -19,12 +19,38 @@ public:
         ans.pop_back();
     } 
     
+    void usingRecursion(int idx, vector<int> &arr, vector<int> &set, vector<vector<int>> &ans){
+        
+        if(idx == arr.size()){
+
+            ans.push_back(set);
+            return ;
+        }
+
+        //1. Include
+
+        set.push_back(arr[idx]);
+        usingRecursion(idx+1, arr, set, ans);
+
+        //2. Exclude
+        set.pop_back();
+        usingRecursion(idx+1, arr, set, ans);
+        
+    }
+    
     vector<vector<int>> subsets(vector<int>& nums) {
         
         vector<vector<int>> ans;
         
         //Method1
-        usingBits(nums, ans);
+        //usingBits(nums, ans);
+        //return ans;
+        
+        //Method2
+        vector<int> set;
+        usingRecursion(0, nums, set, ans);
+        
         return ans;
+        
     }
 };
