0b9f7fa813dd78c00047d85d8cac7ef16f0c0cd8
Sameer <sameer.barha12@gmail.com>
2024-04-15 00:37:00 +0530

    Time: 229 ms (53.33%), Space: 202.4 MB (7.02%) - LeetHub

--- 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
@@ -75,16 +75,86 @@ public:
         return memo[idx][part] = made;
     }
     
+    
+    int bottomUp(string &s, int K){
+        
+        int N = s.size();
+        
+        //Precompute the palindrome Table
+        vector<vector<int>> palin(N, vector<int> (N, false));
+        
+            //palindrome of 1 length
+            for(int i=0; i<N; i++)
+                palin[i][i] =true;
+
+            //palindrome of 2 length
+            for(int i=0; i<N-1; i++){
+                if(s[i]!=s[i+1])
+                    palin[i][i+1] =false;
+                else
+                    palin[i][i+1] =true;
+
+            }
+            //palindrome of length 3 and above
+            for(int len=3; len<=N; len++){
+
+                for(int j=0; j<N; j++){
+
+                    if(j+len-1>= N)
+                        continue;
+
+                    int k = j+len-1;
+                    if(s[j]!=s[k])
+                        palin[j][k] = false;
+                    else
+                        palin[j][k] = palin[j+1][k-1];
+                }
+            }
+        
+            vector<vector<int>> dp(N+1, vector<int> (K, false));
+            
+            for(int i=0; i<N; i++)
+                dp[i][K-1] = palin[i][N-1];
+        
+            for(int j=0; j<=K-1; j++)
+                dp[N][j] = false;
+        
+            
+            for(int idx=N-1; idx>=0; idx--){
+                
+                
+                for(int part =K-2; part>=0; part--){
+                    
+                    
+                    int made = false;
+                    for(int i=idx; i<N; i++){
+                        
+                        if(palin[idx][i])
+                            made = made || dp[i+1][part+1];
+                    }
+                    
+                    dp[idx][part] = made;
+                }
+            }//for idx
+        
+        
+        return dp[0][0];
+    }
+    
     bool checkPartitioning(string str) {
      
         //1. Memoization
-        int K = 3;
+        /*int K = 3;
         int N = str.size();
         vector<vector<int>> memo(N, vector<int> (K, -1));
         vector<vector<int>> palin(N, vector<int> (N, -1));
         
         return recur(0, 0, str, K, memo, palin);
+        */
         
+        
+        //2. BottomUp
+        return bottomUp(str, 3);
         
     }
 };
