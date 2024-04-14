3c7c4c9a0ca3c7cc535b18a300ff379df054c66b
Sameer <sameer.barha12@gmail.com>
2024-04-15 00:12:26 +0530

    Time: 135 ms (80.35%), Space: 202.5 MB (5.61%) - LeetHub

--- 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
@@ -14,8 +14,34 @@ public:
 
           return  true;
     }
+    
+    
+    int ispalindromeRec(int i, int j, string &str, vector<vector<int>> &palin) {
 
-    int recur(int idx, int part, string &str, int K, vector<vector<int>> &memo) {
+        if(i>j)
+            return 0;
+        
+        if(i==j)
+            return 1;
+        
+        if(abs(i-j)==1)
+            return str[i] == str[j];
+        
+        if(palin[i][j]!=-1)
+            return palin[i][j];
+        
+
+        if(str[i] != str[j])
+                return palin[i][j] = false;
+
+        
+        return palin[i][j]=ispalindromeRec(i+1, j-1, str, palin);
+
+
+    }
+    
+
+    int recur(int idx, int part, string &str, int K, vector<vector<int>> &memo, vector<vector<int>> &palin) {
 
         //1 partition remaining - char if its palin
         if(part==K-1){
@@ -24,7 +50,8 @@ public:
             if(idx==str.size())
                 return false;
             
-            return ispalindrome(idx, str.size()-1, str);
+            return ispalindromeRec(idx, str.size()-1, str, palin);
+            //return ispalindrome(idx, str.size()-1, str);
         }
         
         
@@ -37,9 +64,9 @@ public:
         int made = false;
         for (int i= idx; i<str.size(); i++) {
 
-            
-            if (ispalindrome(idx, i, str)) {
-                made = made || recur(i+1, part+1, str, K, memo);
+            if(ispalindromeRec(idx, i, str, palin)){
+            //if (ispalindrome(idx, i, str)) {
+                made = made || recur(i+1, part+1, str, K, memo, palin);
             }
 
         }
@@ -54,8 +81,9 @@ public:
         int K = 3;
         int N = str.size();
         vector<vector<int>> memo(N, vector<int> (K, -1));
+        vector<vector<int>> palin(N, vector<int> (N, -1));
         
-        return recur(0, 0, str, K, memo);
+        return recur(0, 0, str, K, memo, palin);
         
         
     }
