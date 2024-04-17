48b117556680f5dae4d7e48c9c77802e6993b14f
Sameer <sameer.barha12@gmail.com>
2024-04-17 09:01:49 +0530

    Time: 7 ms (33.57%), Space: 12.7 MB (54.60%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,31 +1,95 @@
 class Solution {
 public:
-    void solve(vector<int>& nums,int pos,int n,vector<int> &temp,vector<vector<int>> &ans,int target,int sum)
-    {
-        if(sum==target)
-        {
-            ans.push_back(temp);
-            return;
+    
+    void combRec(int idx, int target, vector<int> &candidates, vector<int> &comb, vector<vector<int>> &combList){
+            
+        if(target==0){
+            combList.push_back(comb);
+            return ;
         }
-        if(sum>target)
-            return;
-        if(pos>=n)
-            return;
-        temp.push_back(nums[pos]);
-        sum+=nums[pos];
-        solve(nums,pos+1,n,temp,ans,target,sum);
-        temp.pop_back();
-        sum-=nums[pos];
-        while(pos+1<n && nums[pos]==nums[pos+1]) pos++;
-        solve(nums,pos+1,n,temp,ans,target,sum);
+        
+        for(int i=idx; i<candidates.size(); i++){
+            
+            if(i>idx && candidates[i]==candidates[i-1])
+                continue;
+            if(candidates[i]<=target){
+                comb.push_back(candidates[i]);
+                combRec(i+1, target-candidates[i], candidates, comb, combList);
+                comb.pop_back();
+            }
+            
+        }
+        
+        return ;
         
     }
-    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>> ans;
-        vector<int> temp;
-        int n=nums.size();
-        solve(nums,0,n,temp,ans,target,0);
-        return ans;
+    
+    void includeExclude(int idx, int target,  vector<int> &candidates, vector<int> &comb, vector<vector<int>> &combList){
+        
+        if(target == 0){
+            combList.push_back(comb);
+            return ;   
+        }
+        
+        if(idx==0)
+            return;
+        
+        //Include
+        if(candidates[idx-1]<=target){
+            comb.push_back(candidates[idx-1]);
+            includeExclude(idx-1, target-candidates[idx-1], candidates, comb, combList);
+            comb.pop_back();
+        }
+        
+        //Exclude
+        /*
+            If excluding this elemnet ,
+            better exclude all occurance of this element, to
+            avoid creating other combination
+            
+            [ 5, 2, 1, 1] => [5, 2, 1] [5,2,1]
+            
+            current idx => idx-1
+        */
+        //reach at the last occurance of the curernt element
+        while(idx-2 >=0 && candidates[idx-2] == candidates[idx-1])
+            idx--;
+        
+        includeExclude(idx-1, target, candidates, comb, combList);
+    }
+    
+    vector<vector<int>> method1(vector<int> &candidates, int target){
+
+        sort(candidates.begin(), candidates.end()); //to bring single order
+        vector<vector<int>> combList;
+        vector<int> comb;
+        
+        int N=candidates.size();
+        includeExclude(N, target, candidates, comb, combList);
+        
+        
+        return combList;
+    }
+    
+    vector<vector<int>> method2(vector<int> &candidates, int target){
+        
+        sort(candidates.begin(), candidates.end()); //to bring single order
+        vector<vector<int>> combList;
+        vector<int> comb;
+        
+        combRec(0, target, candidates, comb, combList);
+        return combList;
+        
+    }
+    
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        
+        //1. Include + Exclude
+        return method1(candidates, target);
+        
+
+        //2. Recursion + Backtracking
+        //return method2(candidates, target);
+        
     }
 };
