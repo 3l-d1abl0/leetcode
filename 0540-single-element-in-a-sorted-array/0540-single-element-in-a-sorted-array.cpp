8db1af76550bf7fb6da2085dcacdabad3d5c3b1c
Sameer <sameer.barha12@gmail.com>
2024-04-30 10:33:18 +0530

    Time: 23 ms (20.96%), Space: 24.6 MB (85.45%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,53 +1,40 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
+     
+        /*
+            Even pos - 1st occur
+            Odd pos - 2nd Occur
+                   X  00 ev
+             0  1  2  3  4  5  6  7  8
+            [1, 1, 2, 3, 3, 4, 4, 8, 8]
+        */
+            int n=nums.size();
+            int low = 0;
+            int high = n - 2;
         
-        int N = nums.size();
-        
-        int l=0, r= N-1;
-        
-        if(r==0)    //arrary size 1
-            return nums[r];
-        else if(nums[l]!=nums[l+1])
-            return nums[l];
-        else if(nums[r-1]!=nums[r])
-            return nums[r];
-        
-        
-        while(l<=r){
-            
-            int mid = l+(r-l)/2;
-            
-            /*before:
-                even - 1st idx
-                odd- end idx
+            while (low <= high) {
+                int mid = (low + high) / 2;
                 
-            single -  even idx
-            
-            after:
-                even - 2nd idx 
-                odd - 1st Idx
-            */
-            
-            
-            if(nums[mid]!= nums[mid-1] && nums[mid]!=nums[mid+1])
-                return nums[mid];
-            
-            if(mid%2==0 && nums[mid] == nums[mid+1])
-                l= mid+1;
-            else if(mid%2==0 && nums[mid] != nums[mid+1])
-                r= mid-1;
-            
-            
-            if(mid%2==1 && nums[mid] == nums[mid-1])
-                l= mid+1;
-            else if(mid%2==1 && nums[mid] != nums[mid-1])
-                r= mid-1;
-            
-            
-        }//while
+                if (mid % 2 == 0) { //first element of the pair is at even index
+                    
+                    if (nums[mid] != nums[mid + 1]) 
+                    //Checking whether we are in right half
+
+                        high = mid - 1; //Shrinking the right half
+                    else
+                        low = mid + 1; //Shrinking the left half
+                } else {
+
+                    //Checking whether we are in right half
+                    if (nums[mid] == nums[mid + 1]) 
+                        high = mid - 1; //Shrinking the right half
+                    else
+                        low = mid + 1; //Shrinking the left half
+                }
+            }
+
+            return nums[low];
         
-        
-        return -1;
     }
 };
