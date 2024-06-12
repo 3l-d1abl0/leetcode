6b4cf8a35de5b5389900242083eb4438cdf5001c
Sameer <sameer.barha12@gmail.com>
2024-06-12 17:00:59 +0530

    Time: 68 ms (58.04%), Space: 14.5 MB (59.65%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -2,40 +2,40 @@ class Solution {
 public:
 
     
-int method1(vector<int> &arr, int n){
-   
-    /* Allocate memory for LIS[] and initialize LIS values as 1 for
-      all indexes */
-    vector<int> lis(n,1);
-  
-   /* Compute LIS values from left to right */
-   for (int i = 1; i < n; i++)
-      for (int j = 0; j < i; j++)
-         if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
-            lis[i] = lis[j] + 1;
-  
-   /* Allocate memory for lds and initialize LDS values for
-      all indexes */
-    vector<int> lds(n, 1);
-  
-   /* Compute LDS values from right to left */
-   for (int i = n-2; i >= 0; i--)
-      for (int j = n-1; j > i; j--)
-         if (arr[i] > arr[j] && lds[i] < lds[j] + 1)
-            lds[i] = lds[j] + 1;
-  
-  
-   /* Return the maximum value of lis[i] + lds[i] - 1*/
-   int maxx = 0;
-   for (int i = 1; i < n-1; i++){
-    
-       if(lis[i]>1 && lds[i]>1)
-         maxx = max( maxx, lis[i] + lds[i] - 1);
-   }
-    
-   return n-maxx;
-    
-}
+    int method1(vector<int> &arr, int n){
+
+        /* Allocate memory for LIS[] and initialize LIS values as 1 for
+          all indexes */
+        vector<int> lis(n,1);
+
+       /* Compute LIS values from left to right */
+       for (int i = 1; i < n; i++)
+          for (int j = 0; j < i; j++)
+             if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
+                lis[i] = lis[j] + 1;
+
+       /* Allocate memory for lds and initialize LDS values for
+          all indexes */
+        vector<int> lds(n, 1);
+
+       /* Compute LDS values from right to left */
+       for (int i = n-2; i >= 0; i--)
+          for (int j = n-1; j > i; j--)
+             if (arr[i] > arr[j] && lds[i] < lds[j] + 1)
+                lds[i] = lds[j] + 1;
+
+
+       /* Return the maximum value of lis[i] + lds[i] - 1*/
+       int maxx = 0;
+       for (int i = 1; i < n-1; i++){
+
+           if(lis[i]>1 && lds[i]>1)
+             maxx = max( maxx, lis[i] + lds[i] - 1);
+       }
+
+       return n-maxx;
+
+    }
 
     int ceilIdx(vector<int> &lis, int key){
         
@@ -129,10 +129,16 @@ int method1(vector<int> &arr, int n){
     
     int minimumMountainRemovals(vector<int>& nums) {
         
+        /*
+            Compute the length of the longest Bitonic Subsequence
+            subtract from N
+        
+        */
+        
         int N = nums.size();
         
         //
-        //return method1(nums, N); //O(n^2)
+        return method1(nums, N); //O(n^2)
         
         return method2(nums, N); //O(2NlonN + N)
     }
