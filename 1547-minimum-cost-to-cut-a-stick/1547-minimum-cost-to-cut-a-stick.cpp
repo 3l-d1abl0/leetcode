6e51173901a2423f41a188f99e6da744b6f3103b
Sameer <sameer.barha12@gmail.com>
2024-06-18 20:48:44 +0530

    Time: 38 ms (41.50%), Space: 12.3 MB (15.31%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -35,11 +35,53 @@ public:
         
     }
     
+    int bottomUp(int n, vector<int>& cuts) {
+        
+            cuts.push_back(0);
+            cuts.push_back(n);
+        
+            sort(cuts.begin(), cuts.end());
+        
+        
+            vector<vector<int>> dp(cuts.size(), vector<int> (cuts.size(), 0));
+        
+            int c = cuts.size()-2;
+        
+            for(int i=c; i>=1; i--){
+                
+                for(int j=1; j<=c; j++){
+                    
+                    
+                    if(i>j)
+                        continue;
+                    
+                    int minn = INT_MAX;
+                    for(int k=i; k<=j; k++){
+                        int cost = cuts[j+1]- cuts[i-1];
+                        cost += dp[i][k-1]+dp[k+1][j];
+
+                        minn = min(minn, cost);
+                    }
+                    
+                    dp[i][j] = minn;
+                    
+                }
+            }//for i
+        
+        
+            return dp[1][c];
+        
+    }
+    
     int minCost(int n, vector<int>& cuts) {
         
         
         //1. Rec+memo
-        return recMemo(n, cuts);
+        //return recMemo(n, cuts);
+        
+        
+        //2. BottomUp
+        return bottomUp(n, cuts);
         
     }
 };
