f51d102bb4aa4f8987238303bf101ddc8168ce2e
Sameer <sameer.barha12@gmail.com>
2024-08-27 00:28:11 +0530

    Time: 113 ms (75.11%), Space: 51.6 MB (88.66%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -22,7 +22,7 @@ public:
         
     }
     
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+    vector<int> method1(vector<vector<int>>& graph){
         
         int N = graph.size();
         
@@ -41,10 +41,105 @@ public:
             }
             
         }
-                
         
         return safe;
         
+    }
+    
+    //check if this node is involved in a Loop
+    bool dfsCheck(int node, vector<vector<int>> &adj, vector<bool> &visited, vector<bool> &currentPath, vector<bool> &safe) {
+		
+        	visited[node] = true;
+	        currentPath[node] = true;
+          safe[node] = false;
+
+
+        for(int neigh: adj[node]){
+
+            if(visited[neigh]==true){
+                //loop
+                if(currentPath[neigh]==true){
+                    //not a teminal Node
+                    safe[node] = false;
+                    return true;
+                }
+                
+            }else{
+                //loop
+                if(dfsCheck(neigh, adj, visited, currentPath, safe)){
+                    safe[node] = false;
+                    return true;
+                }
+            }
+
+        }//for
+
+	currentPath[node] = false;
+    safe[node] = true;
+	return false;
+	}
+    
+    vector<int> method2(vector<vector<int>>& graph){
+     
+        int N = graph.size();
+        
+    
+        vector<bool> visited(N, false);
+		vector<bool> currentPath(N, false);
+		vector<bool> safe(N, false);    //1-safe node, 0-unsafe
+        
+		vector<int> safeNodes;
+        
+		for (int i = 0; i < N; i++) {
+			if (!visited[i]) {
+				dfsCheck(i, graph, visited, currentPath, safe);
+                
+                
+			}
+		}
+        
+        for (int i = 0; i < N; i++) {
+			if (safe[i] == true)
+                safeNodes.push_back(i);
+		}
+		
+        
+        return safeNodes;
+    }
+    
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        
+        /*  Directed Graph
+            Safe Node - If every path from that node leads to a Terminal Node
+            Terminal Node - has no outgoing edges
+            
+            
+            Approach 1: 
+            Consider every node as Unsafe Node by default,
+            untill you find it is a terminal Node.
+            
+            mark a node as unsafe,
+            visite its neighbour ,
+            if any of the neighour returns unsafe, (happen only whne there is a cycle and it revisites again
+            return this node as unsafe
+            
+            other wise return this node as safe
+        */
+        
+        //return method1(graph);
+        
+        /*
+        Approach2:
+        
+        Check for cycles
+        
+        If a node is involved in cycle,
+        it is unsafe,
+        
+        If its not involved in cyle it is Safe.
+        */
+        
+        return method2(graph);
     }
     
     
