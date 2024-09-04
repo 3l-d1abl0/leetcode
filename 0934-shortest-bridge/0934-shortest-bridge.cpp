01eb59e035da6b946977c98c84b78b7e5051e9f2
Sameer <sameer.barha12@gmail.com>
2024-09-04 18:44:46 +0530

    Time: 199 ms (5.95%), Space: 63.1 MB (5.16%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.cpp
+++ 0934-shortest-bridge/0934-shortest-bridge.cpp
@@ -3,6 +3,9 @@ public:
     
     pair<int, int> getLandCoOrdinate(vector<vector<int>> &grid){
         
+        /* This function finds the cordinates of a point of the Island
+        */
+        
         int R = grid.size();
         int C = grid[0].size();
         
@@ -20,6 +23,10 @@ public:
     
     
     void dfsMakeIt2(int x, int y, vector<vector<int>> &grid, queue<pair<int, int>> &q){
+        /* This function take a x,y cooord
+            which is a valid poiunt in an Island.
+            Marks the entire Island as 2
+        */
         
         int N = grid.size();
         
@@ -46,6 +53,13 @@ public:
     
     int travelWaters(vector<vector<int>> &grid, queue< pair<int, int> > &q){
         
+        /*
+        Currently all the nodes in queue is of Value 2
+        Start a bfs from all the nodes , process entire level at once
+        so one you reach any node with value 1 
+        
+        that will the distance.
+        */
         
         int N = grid.size();
         
@@ -60,30 +74,30 @@ public:
             //cout<<"lvl "<<dist<<" : "<<sz<<" nodes"<<endl;
             for(int i=0; i<sz; i++){
                 
-                auto [x, y] = q.front();
-                q.pop();
-                
-             
-               for(auto row: dir){
-
-                    int new_x = x+row[0];
-                    int new_y = y+row[1];
-
-                    if(0<=new_x && new_x<N && 0<=new_y && new_y<N){ //within limits of boundary
+                    auto [x, y] = q.front();
+                    q.pop();
 
 
-                        if(grid[new_x][new_y]==1)
-                            return dist;
-                        
-                        if(grid[new_x][new_y]==0){
-                            grid[new_x][new_y] = -1; //water is visited
-                        
-                            q.push({new_x, new_y});
-                            
+                   for(auto row: dir){
+
+                        int new_x = x+row[0];
+                        int new_y = y+row[1];
+
+                        if(0<=new_x && new_x<N && 0<=new_y && new_y<N){ //within limits of boundary
+
+
+                            if(grid[new_x][new_y]==1)
+                                return dist;
+
+                            if(grid[new_x][new_y]==0){
+                                grid[new_x][new_y] = -1; //water is visited
+
+                                q.push({new_x, new_y});
+
+                            }
+
                         }
-
-                    }
-                }//for dir       
+                    }//for dir       
                 
                 
             }//for sz
@@ -100,8 +114,6 @@ public:
         
         
         //1. Find one Island, make its value 2 to differentiate
-        
-        
         pair<int, int> xy = getLandCoOrdinate(grid);
         
         if(xy.first==-1 || xy.second==-1)
@@ -114,7 +126,7 @@ public:
         dfsMakeIt2(xy.first, xy.second, grid, q);
         
         //printGrid(grid);
-        
+        //At this point one Island is marked as 1 and other is marked as 2
         //2. Start Bfs from each cell and check the min distance to reach each cell
         // If you reached land (1) , get the dist
         
@@ -137,4 +149,16 @@ public:
             cout<<endl;
         }
     }
+    
+    
+    /*
+    
+    [1,1,1,1,1]
+    [1,0,0,0,1]
+    [1,0,1,0,1]
+    [1,0,0,0,1]
+    [1,1,1,1,1]]
+    
+    
+    */
 };
