82f7562d78818b761f4f25eb5bd777c3c41d492b
Sameer <sameer.barha12@gmail.com>
2024-09-19 21:23:01 +0530

    Time: 41 ms (54.73%), Space: 25.3 MB (55.49%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
@@ -1,8 +1,88 @@
+class DSU{
+    
+    int N;
+    vector<int> size;
+    vector<int> parent;
+    int join;
+    public:
+    
+    DSU(int n){
+        join =0;
+        N = n;
+        size.resize(n, 1);
+        parent.resize(n, 0);
+        
+        for(int i=0; i<n; i++)
+            parent[i] =i;
+    }
+    
+    int findParent(int u){
+        
+        if(parent[u]==u)
+            return u;
+        
+        return parent[u] = findParent(parent[u]);
+    }
+    
+    void unionBySize(int u, int v){
+        
+        //cout<<"Joining : "<<u<<" "<<v<<endl;
+        
+        int pv = findParent(v);
+        int pu = findParent(u);
+        
+        // if(size[pu] == 0)
+        //     size[pu] =1;
+        // if(size[pv] == 0)
+        //     size[pv] =1;
+        
+        if(pv!=pu){
+            
+            if(size[pv] >= size[pu]){
+                parent[pu] = pv;
+                size[pv] += size[pu];
+            }else{
+                parent[pv] = pu;
+                size[pu] += size[pv];
+            }
+        }else{
+            join++;
+            //cout<<u<<", "<<v<<endl;
+        }
+        
+        //cout<<"Size : "<<size[pu]<<" "<<size[pv]<<endl;
+    }//unionBySize
+    
+    int findServersWithSize2(){
+        
+        /*
+        for(int i=0; i<N; i++){
+            cout<<size[i]<<" ";
+        }
+        cout<<endl;
+        
+        cout<<"join:: "<<join<<endl;
+        */
+        
+        int serverCountSize2 =0;
+        for(int i=0; i<N; i++){
+            if(i==parent[i] && size[i]==2){
+                
+                serverCountSize2++;
+                
+            }
+        }
+        
+        return serverCountSize2;
+    }
+    
+};
+
 class Solution {
 public:
-    int countServers(vector<vector<int>>& grid) {
-        
-        int R = grid.size();
+    
+    int method1(vector<vector<int>>& grid){
+                int R = grid.size();
         int C = grid[0].size();
         
         vector<int> hor(R, 0);
@@ -29,5 +109,47 @@ public:
         
         
         return count;
+    }
+    
+    int method2(vector<vector<int>>& grid){
+        
+        
+        int R = grid.size();
+        int C = grid[0].size();
+        
+        DSU ds(R+C);
+        
+        int serverCount=0;
+        for(int i=0; i<R; i++){
+            for(int j=0; j<C; j++){
+                
+                if(grid[i][j] ==1){
+                    serverCount++;
+                    ds.unionBySize(i, R+j);
+                }
+            }
+        }//for i
+        
+        return serverCount - ds.findServersWithSize2();
+    }
+    
+    int countServers(vector<vector<int>>& grid) {
+        
+        /*
+            method1: frequency
+            TC: O(M*N)
+            SC: O(M+N)
+        */
+        //return method1(grid);
+        
+        /*
+            method2 : DSU
+            count the total servers, remove the ones which are not connected
+            TC: O()
+            SC: ()
+        */
+        
+        return method2(grid);
+        
     }
 };
