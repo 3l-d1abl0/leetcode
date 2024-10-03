affa07a64ece685fc22b5c0e924031ae5fe1445b
Sameer <sameer.barha12@gmail.com>
2024-10-03 23:47:19 +0530

    Time: 43 ms (26.08%), Space: 38.6 MB (28.71%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -78,10 +78,10 @@ public:
     
     int numSubarraysWithSum(vector<int>& nums, int goal) {
   
-        //TC: O(N) SC O(N)
-        //return method1(nums, goal);
+        //TC: O(N) SC O(N)- Cumulative Sum - MAP
+        return method1(nums, goal);
         
-        //TC: O(N), SC: O(1)
-        return method2(nums, goal);
+        //TC: O(N), SC: O(1) - Sliding Window/2 Pointer - 21ms
+        //return method2(nums, goal);
     }
 };
