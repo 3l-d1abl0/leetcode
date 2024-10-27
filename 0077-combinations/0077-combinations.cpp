04fdc6aca21c7dd383b57616eb2ac801043818eb
Sameer <sameer.barha12@gmail.com>
2024-10-27 12:23:12 +0530

    Time: 47 ms (62.49%), Space: 62.2 MB (56.96%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -22,7 +22,8 @@ public:
         
     }
     
-    vector<vector<int>> combine(int n, int k) {
+    
+    vector<vector<int>> method1(int n, int k){
         
         //Include + Exclude
         vector<int> comb;
@@ -30,5 +31,47 @@ public:
         recur(n, k, comb, ans);
         
         return ans;
+    }
+    
+    void iter(int idx, int n, int k, vector<int> &comb, vector<vector<int>> &ans){
+     
+        
+        if(comb.size()==k){
+            ans.push_back(comb);
+        }
+        
+        if(idx>n)
+            return;
+        
+        for(int i=idx; i<=n; i++){
+            comb.push_back(i);
+            iter(i+1, n, k, comb, ans);
+            comb.pop_back();
+        }
+        
+    }
+    
+    vector<vector<int>> method2(int n, int k){
+        
+        vector<int> comb;
+        vector<vector<int>> ans;
+        
+        
+        iter(1, n, k, comb, ans);
+        
+        return ans;    
+    }
+    
+    
+    vector<vector<int>> combine(int n, int k) {
+        
+        
+        //Method1 - Recursion
+        //return method1(n, k);
+        
+        
+        //Method2 - Iteration
+        return method2(n, k);
+
     }
 };
