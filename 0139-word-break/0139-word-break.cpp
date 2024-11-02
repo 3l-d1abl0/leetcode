38e2b18949a7ef762af5c95e594dab5394c14e52
Sameer <sameer.barha12@gmail.com>
2024-11-02 21:43:34 +0530

    Time: 15 ms (35.37%), Space: 16.1 MB (45.85%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -2,7 +2,16 @@ class Solution {
 public:
 
         
-    bool wordBreakRecur(int idx, string &s, set <string> &word_set, vector<int> &memo){
+    bool method0(int idx, string &s, set <string> &word_set, vector<int> &memo){
+          /*
+            Start from index 0, and get substring till n-1,
+            check if any substring exit, recur for the next Part
+            
+            f(0)
+            
+            0-0 + f(1)  , 0-1 +f(2), 0-2+f(3)     ...........  0-(n-1)+f(n)
+          
+          */
           if (idx >= s.length())
             return true;
           
@@ -11,52 +20,34 @@ public:
                 return memo[idx];
         
           for (int end = idx + 1; end <= s.length(); end++) {
-            if (word_set.find(s.substr(idx, end - idx)) != word_set.end() and wordBreakRecur(end, s, word_set, memo)) {
+            if (word_set.find(s.substr(idx, end - idx)) != word_set.end() and method0(end, s, word_set, memo)) {
               return memo[idx] = true;
             }
           }
           return memo[idx] = false;
     }
     
-    bool method0(string s, vector<string> &wordDict){
-        
-      unordered_set < string > word_set(wordDict.begin(), wordDict.end());
-      vector < bool > dp(s.length() + 1);
-      dp[0] = true;
-
-      for (int st = 0; st <= s.length(); st++) {
-        for (int ed = st+1; ed <=s.length(); ed++) {
-            
-          //cout<<st<<" "<<ed<<" -> "<<s.substr(st, ed - st)<<endl;
-          if (dp[st] and word_set.find(s.substr(st, ed - st)) != word_set.end()) {
-            dp[ed] = true;
-              //cout<<"FOUND\n";
-              //st = ed-1;
-            //break;
-          }
-        }
-      }
-      
-        return dp[s.length()];
-        
-    }
     
     bool method1(string s, vector<string> &wordDict){
-        
+    
+      //Create a set of the words
       unordered_set < string > word_set(wordDict.begin(), wordDict.end());
-      vector < bool > dp(s.length() + 1);
-      dp[0] = true;
-
-      for (int i = 1; i <= s.length(); i++) {
-        for (int j = 0; j < i; j++) {
-          if (dp[j] and word_set.find(s.substr(j, i - j)) != word_set.end()) {
-            dp[i] = true;
-            break;
-          }
-        }
-      }
+      vector < bool > dp(s.length() + 1, false);
+        
+      //base  
+      dp[s.length()] = true;
       
-        return dp[s.length()];
+      //j is the boundary
+      for (int i = s.length()-1; i >=0 ; i--) {
+        for (int j = i+1; j <= s.length(); j++) {
+            if(word_set.find(s.substr(i, j-i)) != word_set.end() and dp[j]) {
+                dp[i] = true;
+                break;
+            }
+        }//j
+      }//i
+      
+        return dp[0];
         
     }
     
@@ -90,15 +81,14 @@ public:
     
     bool wordBreak(string s, vector < string > & wordDict) {
         
-        //Method1 - TLE - Set + Recursion - 35 / 47
-        vector<int> memo(s.size(), -1);
-        set < string > word_set(wordDict.begin(), wordDict.end());
-        return wordBreakRecur(0, s, word_set, memo);
+        //Method0 - Set + Recursion+Memoization -19ms - O(n^2)
+        // vector<int> memo(s.size(), -1);
+        // set < string > word_set(wordDict.begin(), wordDict.end());
+        // return method0(0, s, word_set, memo);
         
-        //Method2 - 
-        //return method0(s, wordDict);    //34ms - O(n^2) * O(nlogn)
+        //Method1 -
+        return method1(s, wordDict);    //27ms  - O(n^2) * O(nlogn)
         
-        //return method1(s, wordDict);    //27ms  - O(n^2) * O(nlogn)
         //vector<int> dp(s.size()+1,-1);
         //dp[s.size()] = 1;
         //return method2(0, s, wordDict, dp);     //8ms - memoization
