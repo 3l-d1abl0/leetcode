df36bf13c0af2b22c50aa7ae04b81f9e41d0a2fd
Sameer <sameer.barha12@gmail.com>
2024-11-07 22:59:03 +0530

    Time: 29 ms (88.12%), Space: 44.7 MB (87.27%) - LeetHub

--- 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.cpp
+++ 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.cpp
@@ -10,40 +10,36 @@ public:
         
         for(int neighbour = 0; neighbour<adjList[idx].size(); neighbour++){
             
-            
-            if( nodeColor[adjList[idx][neighbour]-1] == color ){
-                
+            if( nodeColor[adjList[idx][neighbour]-1] == color )
                 return false;
-            }
+            
         }
         
         return true;
         
     }
     
-    bool solve(int idx, int nodes, vector<int> &nodeColor, vector<vector<int>> &adjList){
+    bool solve(int idx, int totalNodes, vector<int> &nodeColor, vector<vector<int>> &adjList){
         
             //cout<<"idx :: "<<idx<<endl;
             
-            if(idx > nodes)
+            if(idx > totalNodes)
                 return true;
         
-        
+            //4 Types of Flower
             int colorType = 4;
         
         
             for(int color=1; color <=colorType; color++){
                 
                 //cout<<"Safe ?"<<idx<<" :: "<<color<<endl;
-                
-                
                 if(safe(idx, color, nodeColor, adjList)){ 
                     
                     nodeColor[idx-1] = color;
                     
                     //printVec(nodeColor);
                     
-                    if(solve(idx+1, nodes, nodeColor, adjList))
+                    if(solve(idx+1, totalNodes, nodeColor, adjList))
                         return true;
                     
                     nodeColor[idx-1] = 0;
@@ -61,21 +57,16 @@ public:
      
         vector<vector<int>> adjList (nodes+1);
         
+        
+        //Create adj list from the edges
         for(int i=0; i<paths.size(); i++){
-            
-            
             adjList[paths[i][0]].push_back(paths[i][1]);
-            
             adjList[paths[i][1]].push_back(paths[i][0]);
-            
-            
         }
-        
-        
+        //default color - 0
         vector<int> nodeColor(nodes, 0);
         
-        
-        
+        //start with node 1
         solve(1, nodes, nodeColor, adjList);
         
         return nodeColor;
