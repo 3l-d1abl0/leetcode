4bb3831fc22720dc6fbf11ff7643c2f6a51d8c67
Sameer <sameer.barha12@gmail.com>
2024-11-25 17:56:46 +0530

    Time: 3 ms (98.20%), Space: 11.9 MB (98.18%) - LeetHub

--- 0473-matchsticks-to-square/0473-matchsticks-to-square.cpp
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.cpp
@@ -1,16 +1,44 @@
 class Solution {
 public:
     
-    bool method1(vector<int> &matchsticks){
+    
+    bool method2(vector<int> &nums) {
         
+        if (nums.size() < 4) return false;
         int sum = 0;
-        sum = accumulate(matchsticks.begin(), matchsticks.end(), sum);
-        if (matchsticks.size() < 4 || sum % 4) return false;
-        
-        vector<int>visited(matchsticks.size(), false);
-        return backtrack(matchsticks, visited, sum / 4, 0, 0, 4);
+        for (const int val: nums) {
+            sum += val;
+        }
+        if (sum % 4 != 0) return false;
+        sort(nums.begin(), nums.end(), [](const int &l, const int &r){return l > r;}); // second
+        vector<int> sidesLength(4, 0);
+        return dfs(sidesLength, nums, 0, sum / 4);
         
     }
+    
+    bool dfs(vector<int> &sidesLength,const vector<int> &matches, int index, const int target) {
+        if (index == matches.size())
+            return sidesLength[0] == sidesLength[1] && sidesLength[1] == sidesLength[2] && sidesLength[2] == sidesLength[3];
+        for (int i = 0; i < 4; ++i) {
+            if (sidesLength[i] + matches[index] > target) // first
+                continue;
+            int j = i;
+            while (--j >= 0) // third
+                if (sidesLength[i] == sidesLength[j]) 
+                    break;
+            if (j != -1) continue;
+            sidesLength[i] += matches[index];
+            if (dfs(sidesLength, matches, index + 1, target))
+                return true;
+            sidesLength[i] -= matches[index];
+        }
+        return false;
+    }
+    
+    
+    
+
+    
     bool backtrack(vector<int>& matchsticks,vector<int> &visited, int target, int curr_sum, int idx, int side) {
     
         if (side == 1) // if k == 1 then we found all subsets, rest of the sides are Target
@@ -33,15 +61,20 @@ public:
         return false;
     }
     
-    
-    
     bool makesquare(vector<int> &matchsticks) {
         
         /*1. Method1
         This method fiils one side with possible stick till it reaches its target,
         Once it reaches its target it moves on to the other side
         */
-        return method1(matchsticks);
+        //return method1(matchsticks);
+        
+        
+        /*2. Method2
+        
+        */
+        
+        return method2(matchsticks);
         
     }
 
