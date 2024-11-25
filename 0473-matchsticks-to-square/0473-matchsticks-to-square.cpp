73d679d23f10a334f66a2f28d2441a6e8c950947
Sameer <sameer.barha12@gmail.com>
2024-11-25 17:29:52 +0530

    Time: 227 ms (43.56%), Space: 12.1 MB (66.41%) - LeetHub

--- 0473-matchsticks-to-square/0473-matchsticks-to-square.cpp
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.cpp
@@ -1,36 +1,48 @@
 class Solution {
 public:
     
-    bool dfs(vector<int> &sidesLength,const vector<int> &matches, int index, const int target) {
-        if (index == matches.size())
-            return sidesLength[0] == sidesLength[1] && sidesLength[1] == sidesLength[2] && sidesLength[2] == sidesLength[3];
-        for (int i = 0; i < 4; ++i) {
-            if (sidesLength[i] + matches[index] > target) // first
-                continue;
-            /*int j = i;
-            while (--j >= 0) // third
-                if (sidesLength[i] == sidesLength[j]) 
-                    break;
-            if (j != -1) continue;*/
-            sidesLength[i] += matches[index];
-            if (dfs(sidesLength, matches, index + 1, target))
+    bool method1(vector<int> &matchsticks){
+        
+        int sum = 0;
+        sum = accumulate(matchsticks.begin(), matchsticks.end(), sum);
+        if (matchsticks.size() < 4 || sum % 4) return false;
+        
+        vector<int>visited(matchsticks.size(), false);
+        return backtrack(matchsticks, visited, sum / 4, 0, 0, 4);
+        
+    }
+    bool backtrack(vector<int>& matchsticks,vector<int> &visited, int target, int curr_sum, int idx, int side) {
+    
+        if (side == 1) // if k == 1 then we found all subsets, rest of the sides are Target
+            return true;
+        
+        if (curr_sum == target) // we found one subset, go on to the next one starting from curr_sum = 0
+            return backtrack(matchsticks, visited, target, 0, 0, side-1);
+        
+        for (int j = idx; j < matchsticks.size(); j++) {
+            
+            if (visited[j] || curr_sum + matchsticks[j] > target)
+                continue; // if we visited this index already or curr_sum + matchsticks[j] > target then we can't use it
+            
+            visited[j] = true;
+            if (backtrack(matchsticks, visited, target, curr_sum + matchsticks[j], j+1, side))
                 return true;
-            sidesLength[i] -= matches[index];
+            visited[j] = false;
         }
+        
         return false;
     }
     
     
-    bool makesquare(vector<int>& nums) {
-        if (nums.size() < 4) return false;
+    
+    bool makesquare(vector<int> &matchsticks) {
         
-        int sum = accumulate(nums.begin(), nums.end(), 0);
+        /*1. Method1
+        This method fiils one side with possible stick till it reaches its target,
+        Once it reaches its target it moves on to the other side
+        */
+        return method1(matchsticks);
         
-        if (sum % 4 != 0)
-            return false;
-        
-        sort(nums.begin(), nums.end(), [](const int &l, const int &r){return l > r;}); // second
-        vector<int> sidesLength(4, 0);
-        return dfs(sidesLength, nums, 0, sum / 4);
     }
+
 };
