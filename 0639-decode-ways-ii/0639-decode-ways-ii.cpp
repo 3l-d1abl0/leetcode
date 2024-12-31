8d3846f0e4cbdea1c771311145d83ffb7cef6203
Sameer <sameer.barha12@gmail.com>
2024-12-31 22:12:03 +0530

    Time: 1235 ms (5.26%), Space: 66.8 MB (5.26%) - LeetHub

--- 0639-decode-ways-ii/0639-decode-ways-ii.cpp
+++ 0639-decode-ways-ii/0639-decode-ways-ii.cpp
@@ -87,11 +87,55 @@ private:
         vector<int> memo((int)s.size(), -1);
         return recur(0, s, memo);
     }
+
+    int method2(string &s){
+        const int M = 1e9 + 7;
+        int n = s.size();
+        vector<long long> dp(n + 1, -1); // Memoization array initialized to -1
+
+        // Helper function for recursion with memoization
+        function<long long(int)> decode = [&](int i) {
+            if (i < 0) return 1LL; // Base case
+            if (dp[i] != -1) return dp[i];
+
+            long long ans = 0;
+
+            if (s[i] == '*') {
+                ans = 9 * decode(i - 1) % M; // '*' can be any digit 1-9
+                if (i > 0) {
+                    if (s[i - 1] == '1') ans = (ans + 9 * decode(i - 2)) % M;
+                    else if (s[i - 1] == '2') ans = (ans + 6 * decode(i - 2)) % M;
+                    else if (s[i - 1] == '*') ans = (ans + 15 * decode(i - 2)) % M;
+                }
+            } else {
+                ans = s[i] != '0' ? decode(i - 1) : 0; // Current digit alone
+                if (i > 0) {
+                    string twoDigits = s.substr(i - 1, 2);
+                    if (s[i - 1] == '*' && s[i] <= '6') {
+                        ans = (ans + 2 * decode(i - 2)) % M; // '*' + '1-6'
+                    } else if (s[i - 1] == '*') {
+                        ans = (ans + decode(i - 2)) % M; // '*' + '7-9'
+                    } else if (s[i - 1] != '*' && "10" <= twoDigits && twoDigits <= "26") {
+                        ans = (ans + decode(i - 2)) % M;
+                    }
+                }
+            }
+
+            return dp[i] = ans;
+        };
+
+        return decode(n - 1) % M;
+    }
+
 public:
     int numDecodings(string s) {
         
 
         //1. Method1 - Recursion - top Down
-        return method1(s);
+        //return method1(s);
+
+
+        //2. Method2 - Dynamic Programming
+        return method2(s);
     }
 };
