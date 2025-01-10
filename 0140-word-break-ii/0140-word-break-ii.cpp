ddc1df5101740ad501102ae315a7d1528b4742b1
Sameer <sameer.barha12@gmail.com>
2025-01-10 17:21:20 +0530

    Time: 3 ms (11.07%), Space: 10.2 MB (41.59%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -1,35 +1,85 @@
 class Solution {
 public:
-    void solve(vector<string> &ans, int idx, string &s, string str, vector<string> &wd){
+    vector<string> method1(int idx, unordered_map<int, vector<string>> &memo, string &s, vector<string> &wd){
 
+        //End of String
+        vector<string> ans;
         if(idx==s.size()){
-            if(str.size()!=0){
-                str.pop_back();
-                ans.push_back(str);
-                return;
-            }
+            ans.push_back("");
+            return ans;
         }
         
+        if(memo.find(idx) != memo.end())
+            return memo[idx];
         
-
+        vector<string> result;
         for(int i=0; i<wd.size(); i++){
 
             int len = wd[i].size();
 
             if(wd[i].compare(s.substr(idx, len))==0){
 
-                solve(ans,idx+len, s, str+wd[i]+" ", wd);
+                vector<string> subResult = method1(idx+len, memo, s, wd);
+
+                for(const string &sub: subResult){
+                    if(sub.empty()){//previous call was valid and returned empty string
+                        result.push_back(wd[i]);
+                    }else{
+                        result.push_back(wd[i]+" "+sub);
+                    }
+                }//for subResult
             }
 
         }//for
 
+        return memo[idx] = result;
+        
+
     }
     
+
+    vector<string> method2(string s, vector<string>& wd){
+
+            int N = s.size();
+
+            vector <vector<string>> dp(N+1);
+            dp[N] = {""}; 
+
+            for(int idx=N-1; idx>=0; idx--){
+
+
+                    vector<string> result;
+                    for(int i=0; i<wd.size(); i++){
+
+                        int len = wd[i].size();
+
+                        if(idx + len <= N && wd[i].compare(s.substr(idx, len))==0 ){
+
+                            vector<string> subResult = dp[idx+len];
+
+                            for(const string &sentence: subResult){
+                                dp[idx].push_back(wd[i]+(sentence.empty()?"":" "+sentence));
+                            }//for subResult
+                        }
+
+                    }//for i
+
+            }//for idx
+
+
+            return dp[0];
+    }
+
+
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         
-        vector<string> ans;
-        solve(ans, 0, s, "", wordDict);
+        //TC -  O(n.k. m)
+        //unordered_map<int, vector<string>> memo;
+        //vector<string> ans;
+        //return method1(0, memo, s, wordDict);
         
-        return ans;
+
+        //TC - O(n×W),  SC - O(n×W) W is the number of words in the dictionary
+        return method2(s, wordDict);
     }
 };
