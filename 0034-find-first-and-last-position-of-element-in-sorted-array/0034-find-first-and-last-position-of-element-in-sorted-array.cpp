cf8d6feb038a540d0f1841fb1e6c01b3c1882562
Sameer <sameer.barha12@gmail.com>
2025-01-21 01:58:10 +0530

    Time: 0 ms (100%), Space: 17.6 MB (17.55%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -6,17 +6,58 @@ public:
         
         int N = nums.size();
         
-        ans[0] = findRange(nums, 0, N-1, -1, target, true); //true == look for first index
-        
+        //ans[0] = findRange(nums, 0, N-1, -1, target, true); //true == look for first index
+        ans[0] = findFirst(nums, target);
         if(ans[0]==-1)
             ans[1] = -1;
         else
-            ans[1] = findRange(nums, 0, N-1, -1, target, false);
+            ans[1] = findLast(nums, target);
+            //ans[1] = findRange(nums, 0, N-1, -1, target, false);
         
         
         return ans;
     }
     
+
+    int findFirst(vector<int> &nums, int target){
+
+            int l=-1, r=nums.size();
+
+            while(r-l>1){
+                int mid = l+(r-l)/2;
+
+                if(nums[mid]>=target){
+                    r=mid; //go left, find 1st occurance
+                }else{
+                    l=mid;
+                }
+            }//while
+
+            if(r==nums.size() || nums[r]!=target)
+                return -1;
+
+            return r;
+    }
+
+    int findLast(vector<int> &nums, int target){
+
+            int l=-1, r=nums.size();
+
+            while(r-l>1){
+                int mid = l+(r-l)/2;
+
+                if(nums[mid]<=target){
+                    l=mid; //go right, find last occurance
+                }else{
+                    r=mid;
+                }
+            }//while
+
+            if(l==-1 || nums[l]!=target)
+                return -1;
+                
+            return l;
+    }
     
     int findRange(vector<int> nums, int st, int ed, int prevFound, int target, bool firstIdx){
         
