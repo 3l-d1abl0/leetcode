2f96f28ee2295b799e82c15512b8623c2b407073
Sameer <sameer.barha12@gmail.com>
2025-01-23 01:19:09 +0530

    Time: 0 ms (100%), Space: 14.2 MB (47.04%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -21,14 +21,20 @@ public:
     
     int minBSItr(vector<int> &nums, int l, int r){
         
+
         while(nums[l]> nums[r]){
+            /* if [l] > [r] it means 
+                the rotation point is somewhere in the
+                middle, other wise the array [l - r] is already sorted
+                with [l] as the minimum
+            */
             
             int mid = l+(r-l)/2;
             
-            if(nums[mid]<=nums[r])
-                r=mid;
-            else
+            if(nums[l] <= nums[mid])    //if l and mid are the same index, for 2 size ele
                 l=mid+1;
+            else
+                r=mid;  //the mid itself can be the min ele, hence stayed
         }
         
         return nums[l];
@@ -41,7 +47,7 @@ public:
         int st =0;
         int N = nums.size();
         
-        return minBS(nums, st, N-1);
-        //return minBSItr(nums, st, N-1);
+        //return minBS(nums, st, N-1);
+        return minBSItr(nums, st, N-1);
     }
 };
