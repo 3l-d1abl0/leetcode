e57d1ee2403552c6d5ec15aa41e893dffc32afdf
Sameer <sameer.barha12@gmail.com>
2025-01-23 20:18:58 +0530

    Time: 0 ms (100%), Space: 26.3 MB (33.25%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,16 +1,45 @@
 class Solution {
 public:
 
-    bool onRight(int mid, vector<int> & nums){
-        //Check for condition to be on the Right
+    int method2(vector<int> &arr){
 
-        if(mid&1){
-            return nums[mid] == nums[mid+1];
-        }else{
-            return nums[mid] != nums[mid+1];
+        int n = arr.size(); //size of the array.
+
+        //Edge cases:
+        if (n == 1) return arr[0];
+        if (arr[0] != arr[1]) return arr[0];
+        if (arr[n - 1] != arr[n - 2]) return arr[n - 1];
+
+        int low = 1, high = n - 2;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            //if arr[mid] is the single element:
+            if (arr[mid] != arr[mid + 1] && arr[mid] != arr[mid - 1]) {
+                return arr[mid];
+            }
+
+            //we are in the left:
+            if ((mid % 2 == 1 && arr[mid] == arr[mid - 1])
+                    || (mid % 2 == 0 && arr[mid] == arr[mid + 1])) {
+                //eliminate the left half:
+                low = mid + 1;
+            }
+            //we are in the right:
+            else {
+                //eliminate the right half:
+                high = mid - 1;
+            }
         }
+
+        // dummy return statement:
+        return -1;
+
     }
+
     int singleNonDuplicate(vector<int>& nums) {
+
+        return method2(nums);
      
         /*
             Normal:
