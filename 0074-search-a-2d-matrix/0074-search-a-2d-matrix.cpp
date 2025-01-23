70fc0d34a2c8e0c4a30c428c8017483e1d297b6a
Sameer <sameer.barha12@gmail.com>
2025-01-23 23:48:32 +0530

    Time: 0 ms (100%), Space: 13.3 MB (41.05%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,23 +4,23 @@ public:
     int method1( vector<vector<int>> &matrix, int key){
             
         int R = matrix.size();
-	int C = matrix[0].size();
-	
-	int x= 0, y=C-1;
-	
-	while(x<R && y>-1){
-		
-		if(matrix[x][y] == key){
-			return true;
-		}else if(key> matrix[x][y]){
-			x++;
-		}else{
-			y--;
-		}
-		
-	}
-	
-	return false;
+        int C = matrix[0].size();
+        
+        int x= 0, y=C-1;
+        
+        while(x<R && y>-1){
+            
+            if(matrix[x][y] == key){
+                return true;
+            }else if(key> matrix[x][y]){
+                x++;
+            }else{
+                y--;
+            }
+            
+        }
+        
+        return false;
         
     }
     
