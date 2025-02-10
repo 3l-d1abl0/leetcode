038f039b253d6bcb76bdc23a0bb3b65d9e0fa242
Sameer <sameer.barha12@gmail.com>
2025-02-10 23:55:59 +0530

    Time: 3 ms (98.5%), Space: 31.6 MB (99.75%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -69,56 +69,27 @@ public:
     
     vector<vector<int>> method2(vector<vector<int>>& mat) {
         
-        int R= mat.size();
-        int C= mat[0].size();
-        
-        int MAXVAL = R+C;
-        vector<vector<int>> result(R, vector<int>(R, MAXVAL));
-        
-        
-        for(int i=0;i<R; i++){
-            for(int j=0; j<C; j++){
-                
-                if(mat[i][j]==0)
-                    continue;
-                
-                int left=0, up=0;
-                if(i-1>=0)
-                    left = mat[i-1][j];
-                
-                if(j-1>=0)
-                    up = mat[i][j-1];
-                
-                
-                result[i][j] = min(left, up)+1;
-                
+        int m = mat.size(), n = mat[0].size(), INF = m + n; // The distance of cells is up to (M+N)
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (mat[r][c] == 0) continue;
+                int top = INF, left = INF;
+                if (r - 1 >= 0) top = mat[r - 1][c];
+                if (c - 1 >= 0) left = mat[r][c - 1];
+                mat[r][c] = min(top, left) + 1;
             }
-        }//for i
-        
-        
-        for(int i=R-1; i>=0; i--){
-            for(int j=C-1; j>=0; j--){
-                
-                if(mat[i][j]==0)
-                    continue;
-                
-                int right=0, down =0;
-                
-                if(i+1 <=R)
-                    right = mat[i+1][j];
-                
-                if(j+1<=C)
-                    down = mat[i][j+1];
-                
-                
-                result[i][j] += (result[i][j], min(right, down)+1);
-                
+        }
+        for (int r = m - 1; r >= 0; r--) {
+            for (int c = n - 1; c >= 0; c--) {
+                if (mat[r][c] == 0) continue;
+                int bottom = INF, right = INF;
+                if (r + 1 < m) bottom = mat[r + 1][c];
+                if (c + 1 < n) right = mat[r][c + 1];
+                mat[r][c] = min(mat[r][c], min(bottom, right) + 1);
             }
-        }//for i
-        
-        
-        
-        return result;
+        }
+        return mat;
+
     }
     
     
@@ -136,14 +107,14 @@ public:
         
         //Mehthod1:BFS  TC:O(M*N)
         //SC: O(2*(M*N))
-        return method1(mat);
+        //return method1(mat);
         
         
         /*
         Method2: DP, TC: O(M*N)
         SC: O()
         */
-        //return method2(mat);
+        return method2(mat);
         
     }
 };
