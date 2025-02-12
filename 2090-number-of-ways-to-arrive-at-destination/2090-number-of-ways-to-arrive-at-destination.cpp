840d746455ad65d6e8475d2ab8c93d7c89ea9b61
Sameer <sameer.barha12@gmail.com>
2025-02-12 20:51:38 +0530

    Time: 20 ms (15.56%), Space: 37.9 MB (46.23%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
@@ -2,25 +2,27 @@ class Solution {
 public:
     int countPaths(int N, vector<vector<int>>& roads) {
         
+        //Descreasing by Default
+        //Ascending
         priority_queue<tuple<long long,int>, vector<tuple<long long,int>>, greater<tuple<long long,int>>> pq;
         //dist, index
         
         vector<vector<tuple<int, int>>> adj(N);
         for(auto row: roads){
-            adj[row[0]].push_back({row[1], row[2]});
-            adj[row[1]].push_back({row[0], row[2]});
+            adj[row[0]].push_back({row[1], row[2]});    //node2, time
+            adj[row[1]].push_back({row[0], row[2]});    //node1, time
         }
         
         const long long MOD = 1e9+7;
         vector<int> ways(N,0);
-        vector<long long> dist(N, 1e18);
+        vector<long long> timeTaken(N, 1e18);
         
         //Number of ways of reaching source is 1
         ways[0] =1;
-        dist[0] =0;
+        timeTaken[0] =0;
         
         
-        pq.push({dist[0], 0});//dust, idx
+        pq.push({timeTaken[0], 0});//dust, idx
         
         while(!pq.empty()){
             
@@ -30,14 +32,14 @@ public:
             for(auto &[node, nodeTime]: adj[vtx]){
                 
                 //current time+ time to reach node
-                if(time + nodeTime < dist[node]){
+                if(time + nodeTime < timeTaken[node]){
                     //found a Smaller - update
-                    dist[node] = time + nodeTime;
+                    timeTaken[node] = time + nodeTime;
                     ways[node] = ways[vtx];
                     
-                    pq.push({dist[node], node});
+                    pq.push({timeTaken[node], node});
                     
-                }else if(time + nodeTime == dist[node]){
+                }else if(time + nodeTime == timeTaken[node]){
                     //found another path with same time
                     ways[node] = (ways[node] + ways[vtx]) %MOD;
                 }
