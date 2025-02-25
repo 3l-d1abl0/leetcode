bf8017bb829ce2281875d11ece234fa1ce03af14
Sameer <sameer.barha12@gmail.com>
2025-02-25 10:47:36 +0530

    Time: 4 ms (36.35%), Space: 16.4 MB (18.52%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
+    /*
+        op - remaining opening Bracket
+        cl - remaining closing bracket
     
+    */
     void genParenth(int op, int cl, string str, vector<string> &ans , int N){
         
+        //no more bractes Left
         if(op==0 && cl==0){
             ans.push_back(str);
             return;
         }
-        
+        //have some opening brackets
         if(op>0){
             str.push_back('(');
             genParenth(op-1, cl, str, ans , N);
             str.pop_back();
         }
-        
+        //remaining opening bracket is less than remaining closing bracket
+        //if you have more closing brackets then opening bracket , use one
         if(cl>0 && op<cl){
             str.push_back(')');
             genParenth(op, cl-1, str, ans , N);
