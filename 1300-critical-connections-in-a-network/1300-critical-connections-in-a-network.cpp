f6b4c45891f51e856d730480dbde450cc85a0915
Sameer <sameer.barha12@gmail.com>
2025-02-28 23:46:42 +0530

    Time: 232 ms (29.83%), Space: 176 MB (77.2%) - LeetHub

--- 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
@@ -1,53 +1,38 @@
 class Solution {
 public:
-
-    void solve(int src,unordered_map<int,list<int>> &adjList,int parent,int &timer,vector<int> &tin,vector<int> &low,unordered_map<int,bool> &visited,vector<vector<int>> &ans){
-        visited[src] = true;
-        tin[src] = timer;
-        low[src] = timer;
-        timer++;
-
-        for(int nbr:adjList[src]){
-            if(nbr == parent){
-                continue;
-            }
-            if(!visited[nbr]){
-                solve(nbr,adjList,src,timer,tin,low,visited,ans);
-                low[src] = min(low[src],low[nbr]);
-                // checking bridge is there or not:
-                if(low[nbr] > tin[src]){
-                    vector<int> temp;
-                    temp.push_back(src);
-                    temp.push_back(nbr);
-                    ans.push_back(temp);
-                }
+    
+    void dfs(int node, int parent, vector<int>adj[], vector<int>&level, vector<int>&dp,                         vector<vector<int>> &bridges){
+         for(auto &c: adj[node]){
+            if(level[c] == 0){
+               level[c] = level[node] + 1;
+               dfs(c, node, adj, level, dp, bridges);
+               dp[node] += dp[c];
             }else{
-                low[src] = min(low[src],low[nbr]);
+                if(c != parent){
+                    if(level[c] < level[node]){
+                        dp[node]++;
+                    }else{
+                        dp[node]--;
+                    }
+                }
             }
-        }
+         }
+         if(dp[node] == 0 && level[node] > 1){
+            bridges.push_back({parent, node});
+         }
     }
+
+
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        // adjList;
-        unordered_map<int,list<int>> adjList;
-        for(vector<int> v:connections){
-            adjList[v[0]].push_back(v[1]);
-            adjList[v[1]].push_back(v[0]);
-        }
-        // traverse tin = 1 increement in each !visited node.
-        int timer = 1;
-        vector<int> tin(n,0);
-        vector<int> low(n,0);
-        vector<vector<int>> ans;
-        int src = 0;
-        int parent = -1;
-        unordered_map<int,bool> visited;
-
-        solve(src,adjList,parent,timer,tin,low,visited,ans);
-
-        return ans;
-        // give low  = tin in every unvisited node
-        // update low for every node
-        // during backtracking check the bridge.
-        // if there push the u,v in ans. vector.
+         vector<int>adj[n];
+         for(auto it: connections){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+         }
+         vector<vector<int>> bridges;
+         vector<int>dp(n + 1, 0), level(n + 1, 0);
+         level[0] = 1;
+         dfs(0, -1, adj, level, dp, bridges);
+         return bridges;
     }
 };
