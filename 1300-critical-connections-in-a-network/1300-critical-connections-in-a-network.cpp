90d524a60ffe7bb924aeb281118e20aa38d03391
Sameer <sameer.barha12@gmail.com>
2025-02-28 23:54:04 +0530

    Time: 187 ms (71.16%), Space: 183.9 MB (41.09%) - LeetHub

--- 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
@@ -1,38 +1,31 @@
 class Solution {
+    int timer = 1;
 public:
-    
-    void dfs(int node, int parent, vector<int>adj[], vector<int>&level, vector<int>&dp,                         vector<vector<int>> &bridges){
-         for(auto &c: adj[node]){
-            if(level[c] == 0){
-               level[c] = level[node] + 1;
-               dfs(c, node, adj, level, dp, bridges);
-               dp[node] += dp[c];
-            }else{
-                if(c != parent){
-                    if(level[c] < level[node]){
-                        dp[node]++;
-                    }else{
-                        dp[node]--;
-                    }
-                }
+    void dfs(int node,int parent, vector<int>&vis, vector<int>&time, vector<int>&low, vector<vector<int>>&adj, vector<vector<int>>&bridges){
+        vis[node]=1;
+        time[node]=low[node]=timer;
+        timer++;
+        for(auto it:adj[node]){
+            if(it==parent) continue;
+            if(!vis[it]){
+                dfs(it, node, vis, time, low, adj, bridges);
+                low[node]=min(low[node], low[it]);
+                if(low[it]>time[node]) bridges.push_back({it, node});
             }
-         }
-         if(dp[node] == 0 && level[node] > 1){
-            bridges.push_back({parent, node});
-         }
+            else low[node]=min(low[node], low[it]);
+        }
     }
-
-
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-         vector<int>adj[n];
-         for(auto it: connections){
+        vector<vector<int>> adj(n);
+        for(auto it:connections){
             adj[it[0]].push_back(it[1]);
             adj[it[1]].push_back(it[0]);
-         }
-         vector<vector<int>> bridges;
-         vector<int>dp(n + 1, 0), level(n + 1, 0);
-         level[0] = 1;
-         dfs(0, -1, adj, level, dp, bridges);
-         return bridges;
+        }
+        vector<int> vis(n, 0);
+        vector<int> time(n,0);
+        vector<int> low(n,0);
+        vector<vector<int>> bridges;
+        dfs(0, -1, vis, time, low, adj, bridges);
+        return bridges;
     }
 };
