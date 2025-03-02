cbae2ffab15247803d4fda9941a34b0d4c9905e1
Sameer <sameer.barha12@gmail.com>
2025-03-03 00:14:58 +0530

    Time: 113 ms (59.26%), Space: 67.1 MB (40.74%) - LeetHub

--- 2603-reward-top-k-students/2603-reward-top-k-students.cpp
+++ 2603-reward-top-k-students/2603-reward-top-k-students.cpp
@@ -1,54 +1,5 @@
 class Solution {
 public:
-    vector<int> topStudents(vector<string>& positive_feedback, vector<string>& negative_feedback, vector<string>& report, vector<int>& student_id, int k) {
-        //vector<pair<int, int>> pointStudent;
-        //min heap for topK
-        priority_queue<pair<int, int>, vector<pair<int,int>>, compareStudent> pq;
-        vector<int> topKStudents;
-
-        //order it
-        unordered_set<string> posSet(positive_feedback.begin(), positive_feedback.end());
-        unordered_set<string> negSet(negative_feedback.begin(), negative_feedback.end());
-
-        int numStudents = report.size();
-        for(int i = 0; i < numStudents; i++)
-        {
-            string studentReport = report[i];
-            //split report to words
-            stringstream ss(studentReport);
-            string word;
-            int currentScore = 0;
-            while(ss >> word)
-            {
-                if(posSet.count(word)) currentScore += 3;
-                else if(negSet.count(word)) currentScore -= 1;
-            }
-
-            pq.push({currentScore, student_id[i]});
-            // if(pq.size() > k) pq.pop();
-            // pointStudent.push_back({currentScore, student_id[i]});
-        }
-
-        // sort(pointStudent.begin(), pointStudent.end(), compareStudent);
-
-        int ansSize = min((int)k, (int)pq.size());
-        int count = 0;
-        while(topKStudents.size() != ansSize)
-        {
-            topKStudents.push_back(pq.top().second);
-            pq.pop();
-        }
-        
-        /*for(auto & scoreStudent : pointStudent)
-        {
-            topKStudents.push_back(scoreStudent.second);
-            count++;
-            if(count == ansSize) break;
-        }*/
-        
-        return topKStudents;
-    }
-
     struct compareStudent {
         bool operator()(pair<int,int> const & p1, pair<int,int> const & p2)
         {
@@ -70,4 +21,50 @@ public:
         //higher score first
         return p1.first > p2.first;
     }*/
+    vector<int> topStudents(vector<string>& positive_feedback, vector<string>& negative_feedback, vector<string>& report, vector<int>& student_id, int k) {
+        //vector<pair<int, int>> pointStudent;
+        //min heap for topK
+        priority_queue<pair<int, int>, vector<pair<int,int>>, compareStudent> pq;
+        vector<int> topKStudents;
+
+        //Create a Map Set of Words
+        unordered_set<string> posSet(positive_feedback.begin(), positive_feedback.end());
+        unordered_set<string> negSet(negative_feedback.begin(), negative_feedback.end());
+
+        int numStudents = report.size();
+        for(int i = 0; i < numStudents; i++){
+            string studentReport = report[i];
+            
+            //split report to words
+            stringstream ss(studentReport);
+            string word;
+            
+            int currentScore = 0;
+            while(ss >> word){
+                if(posSet.count(word)) currentScore += 3;
+                else if(negSet.count(word)) currentScore -= 1;
+            }
+
+            pq.push({currentScore, student_id[i]});
+            // if(pq.size() > k) pq.pop();
+            // pointStudent.push_back({currentScore, student_id[i]});
+        }
+
+        // sort(pointStudent.begin(), pointStudent.end(), compareStudent);
+
+        int count = 0;
+        for(int i=0; i< k && !pq.empty(); i++){
+            topKStudents.push_back(pq.top().second);
+            pq.pop();
+        }
+        
+        /*for(auto & scoreStudent : pointStudent){
+            topKStudents.push_back(scoreStudent.second);
+            count++;
+            if(count == ansSize) break;
+        }*/
+        
+        return topKStudents;
+    }
+
 };
