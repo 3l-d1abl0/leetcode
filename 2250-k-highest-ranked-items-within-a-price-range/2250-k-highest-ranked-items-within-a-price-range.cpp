a42691db7764e281b3711e0b69c2093f23c0fdf4
Sameer <sameer.barha12@gmail.com>
2025-03-02 23:18:56 +0530

    Time: 827 ms (10.84%), Space: 328.3 MB (7.23%) - LeetHub

---
+++ 2250-k-highest-ranked-items-within-a-price-range/2250-k-highest-ranked-items-within-a-price-range.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+    vector<vector<int>> highestRankedKItems(vector<vector<int>>& grid, vector<int>& pricing, vector<int>& start, int k) {
+
+        int R = grid.size();
+        int C = grid[0].size();
+
+        //set uuup visited - false by default
+        vector<vector<bool>> visited(R, vector<bool>(C, false));
+
+
+        priority_queue<tuple<int, int, int, int>, vector<tuple<int, int, int ,int>>, greater<tuple<int, int, int, int>>> pq;
+
+        vector<vector<int>> dir{{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+
+        int distance =0;
+        queue<tuple<int, int>> q;
+        q.push({start[0], start[1]});
+        
+
+        
+        while(!q.empty()){
+
+            int size = q.size();
+            //Loop through all points at a certain distance
+            for(int sz=0; sz<size; sz++){
+                auto [r, c] = q.front();    q.pop();
+
+                if(visited[r][c] == true)
+                    continue;
+
+                visited[r][c] = true;
+                if(grid[r][c]==0)
+                    continue;
+
+
+                //if grid is not a empty Cell, consider it
+                if(grid[r][c]!=1){
+
+                    //within pricing Range
+                    if(grid[r][c]>= pricing[0] && grid[r][c]<=pricing[1]){
+                        
+                        pq.push({distance, grid[r][c], r, c});
+                    }
+                }
+
+                //loop neighbours
+                for(auto xy: dir){
+
+                    int row = r+xy[0], col = c+xy[1];
+                    if(row>=0 && row<R && col>=0 && col<C){ //in board
+
+                        q.push({row, col});
+                    }
+                }
+
+            }//for
+
+            distance++;
+        }//while
+
+        cout<<"Heap: "<<pq.size()<<endl;
+        vector<vector<int>> ans;
+        //Top k from heap
+        while(!pq.empty() && ans.size() <k){
+
+            auto [dist, price, row, col] = pq.top(); pq.pop();
+            ans.push_back({row, col});
+
+            
+        }
+
+
+        return ans;
+    }
+};
