a423f6a2a83b4638e39eb35ac882747f6f9cbff2
Sameer <sameer.barha12@gmail.com>
2025-03-16 18:19:36 +0000

    Time: 64 ms (28.3%), Space: 27.3 MB (100%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,8 +1,44 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& arr) {
+    
+    vector<vector<int>> method2(vector<int> &arr){
         
         int N = arr.size();
+        set< vector<int> > triplets;
+        
+        for(int i=0; i<N; i++){
+            
+            
+            unordered_set<int> hashMap;
+            for(int j=i+1; j<N; j++){
+                
+                int sum = - (arr[i]+arr[j]);
+                if( hashMap.find(sum) != hashMap.end() ){
+                    
+                    vector<int> triplet = {arr[i], arr[j], sum};
+                    sort(triplet.begin(), triplet.end());
+                    
+                    triplets.insert(triplet);
+                }
+                
+                hashMap.insert(arr[j]);
+                
+            }
+            
+            
+        }//for i
+
+            
+        vector<vector<int>> ans (triplets.begin(), triplets.end());
+        
+        return ans;
+        
+    }
+    
+        
+        vector<vector<int>> method3(vector<int> &arr){
+        
+                    int N = arr.size();
         sort(arr.begin(), arr.end());
         
         
@@ -42,5 +78,30 @@ public:
         
         
         return ans;
+            
+            
+        }
+        
+
+    
+    
+    vector<vector<int>> threeSum(vector<int>& arr) {
+        
+        //1. Three nested loop with a set to store unique triplet
+        //return method1(arr);
+        
+        /*2. TC: O(n^2) + O(1) (hashMap) + O(1)(log(3)) 
+            SC: O(N)+ O(no of unique triplets)*2
+        
+        */
+        
+        //return method2(arr);
+        
+        
+        /*
+        */
+        return method3(arr);
+        //O(NlogN)+O(N2),
+        
     }
 };
