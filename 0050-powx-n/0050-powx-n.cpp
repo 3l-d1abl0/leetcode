a2796296d58ed09635f532db100491a2f3aac998
Sameer <sameer.barha12@gmail.com>
2025-04-05 19:23:47 +0000

    Time: 0 ms (100%), Space: 8.7 MB (45.13%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -86,24 +86,5 @@ public:
         //2. Iterative
         return method2(x, n);
 
-
-        
-
-        
-        //abs of long long
-//         long num = labs(n);
-        
-//         double pow = 1;
-        
-//         while(num){ // equivalent to while(num != 0)
-//             if(num & 1) { // equivalent to if((num & 1) != 0)
-//                 pow *= x;
-//             }
-            
-//             x *= x;
-//             num >>= 1;
-//         }
-        
-//         return pow;
     }
 };
