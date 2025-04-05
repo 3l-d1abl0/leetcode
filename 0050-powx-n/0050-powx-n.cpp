96c1255be9a6a3ba471a70bc3477e69408a50e8d
Sameer <sameer.barha12@gmail.com>
2025-04-05 19:14:54 +0000

    Time: 0 ms (100%), Space: 8.6 MB (71.86%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -17,27 +17,45 @@ public:
     }
     
     
-    double exponentModItr(double x, long int y){
+    double method2(double A, long int B){
+
+        /*
+            x^11
+            1 0 1 1
+            x^8  x^4  x^2   x^1
+
+            result = x^8 . x^2 . x^1 = x^11
+        */
+
+        if (A == 0) return 0; 
+
+        if(B < 0) {
+            B = B*-1L;
+            A = 1 / A;
+        } 
+        
+        //abs of long long
+        //long num = labs(B);
         
         double res = 1;	 // Initialize result
 
         // Update x if it is more than or// equal to p
         //x = x % p; 
 
-
         // In case x is divisible by p;
-        if (x == 0) return 0; 
+        if (A == 0) return 0; 
 
-        while (y > 0){
+        while (B > 0){
             
             // If y is odd, multiply x with result
-            if (y & 1)
-                res = (res*x);// % p;
+            if (B & 1)
+                res = (res*A);// % p;
 
             // half the y
-            y = y>>1; 
-            x = (x*x);// % p;
+            B = B>>1; 
+            A = (A*A);// % p;
         }
+
         return res;
     }
 
@@ -56,29 +74,21 @@ public:
     
     double myPow(double x, int n) {
 
+        /*
+            x^(-n)
+            1/(x^n) = (1/x)^n
+        */
 
         //1. Recursive
-        return method1(x, n);
+        //return method1(x, n);
+
+
+        //2. Iterative
+        return method2(x, n);
+
+
         
 
-        /*
-        
-        x^(-n)
-        1/(x^n) = (1/x)^n
-        */
-        
-        if(n < 0) {
-            x = 1 / x;
-        } 
-        
-        //abs of long long
-        long num = labs(n);
-        
-        return exponentModItr(x, num);
-        
-        if(n < 0) {
-            x = 1 / x;
-        } 
         
         //abs of long long
 //         long num = labs(n);
