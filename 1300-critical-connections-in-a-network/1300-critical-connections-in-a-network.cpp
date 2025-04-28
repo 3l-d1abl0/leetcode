3b89340ae2417164b2d6764906086951ddefc891
Sameer <sameer.barha12@gmail.com>
2025-04-28 17:00:29 +0530

    Time: 220 ms (37.72%), Space: 183.1 MB (47.06%) - LeetHub

--- 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
@@ -1,31 +1,46 @@
 class Solution {
     int timer = 1;
 public:
-    void dfs(int node,int parent, vector<int>&vis, vector<int>&time, vector<int>&low, vector<vector<int>>&adj, vector<vector<int>>&bridges){
-        vis[node]=1;
-        time[node]=low[node]=timer;
+
+    //dfs(-1, 0, adj, vis, time, low, bridges);
+    void dfs(int parent, int node, vector<vector<int>>&adj, vector<bool>&vis, vector<int>&discoveryTime, vector<int>&low, vector<vector<int>>&bridges){
+        
+        vis[node]=true;
+        discoveryTime[node]=low[node]=timer;
         timer++;
-        for(auto it:adj[node]){
-            if(it==parent) continue;
-            if(!vis[it]){
-                dfs(it, node, vis, time, low, adj, bridges);
-                low[node]=min(low[node], low[it]);
-                if(low[it]>time[node]) bridges.push_back({it, node});
+
+        for(auto neighbour:adj[node]){
+            
+            if(neighbour==parent) continue; //skip
+            if(!vis[neighbour]){//if neighbour not visted, visit it 
+
+                dfs(node, neighbour, adj, vis, discoveryTime, low, bridges);
+                
+                low[node]=min(low[node], low[neighbour]);
+                if(low[neighbour]>discoveryTime[node])
+                    bridges.push_back({neighbour, node});
+            }else{
+                /* If already visited , update the current 'low'
+                */
+                low[node]=min(low[node], low[neighbour]);
             }
-            else low[node]=min(low[node], low[it]);
         }
     }
+
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        vector<vector<int>> adj(n);
-        for(auto it:connections){
-            adj[it[0]].push_back(it[1]);
-            adj[it[1]].push_back(it[0]);
+        
+        vector<vector<int>> adjList(n);
+        for(auto edge:connections){
+            adjList[edge[0]].push_back(edge[1]);
+            adjList[edge[1]].push_back(edge[0]);
         }
-        vector<int> vis(n, 0);
-        vector<int> time(n,0);
-        vector<int> low(n,0);
+
+        vector<bool> vis(n, 0);
+        vector<int> discoveryTime(n,0);
+        vector<int> low(n,0);//low time-node reachable
         vector<vector<int>> bridges;
-        dfs(0, -1, vis, time, low, adj, bridges);
+
+        dfs(-1, 0, adjList, vis, discoveryTime, low, bridges);
         return bridges;
     }
 };
