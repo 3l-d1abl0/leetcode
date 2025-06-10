07a23782c40c05a89ba3c513e492f065069488d9
Sameer <sameer.barha12@gmail.com>
2025-06-10 23:42:54 +0530

    Time: 44 ms (65.29%), Space: 113.3 MB (54.43%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -1,36 +1,71 @@
 class RandomizedSet {
-    unordered_set<int> st;
+    //unordered_set<int> st;
+    unordered_map<int, int> mp;
+    vector<int> nums;
 public:
     RandomizedSet() {
         
     }
     
     bool insert(int val) {
-        if(st.find(val)!= st.end()){
+        // if(st.find(val)!= st.end()){
+        //     return false;
+        // }else{
+        //     st.insert(val);
+        //     return true;
+        // }
+
+        if(mp.find(val) != mp.end())
             return false;
-        }else{
-            st.insert(val);
-            return true;
-        }
+
+        //put at the end
+        nums.push_back(val);
+        mp[val] = nums.size()-1;//[val] ==> index
+
+        return true;
     }
     
     bool remove(int val) {
-        if(st.find(val)!= st.end()){
-            st.erase(val);
-            return true;
-        }else{
+        // if(st.find(val)!= st.end()){
+        //     st.erase(val);
+        //     return true;
+        // }else{
+        //     return false;
+        // }
+
+        //replace with last element, then remove the last element
+        if(mp.find(val) == mp.end())
             return false;
-        }
+
+        int valIdx = mp[val];
+        int lastIdx = nums.size()-1;
+
+        //cout<<valIdx<<" "<<lastIdx<<endl;
+
+        swap(nums[valIdx], nums[lastIdx]);
+        mp[nums[valIdx]] = valIdx;
+        mp.erase(val);
+        nums.pop_back();
+        // cout<<"removed ... "<<val<<endl;
+        //         for(int ele: nums)
+        //             cout<<mp[ele]<<" :"<<ele<<" ";
+        // cout<<"========="<<endl;
+        return true;
+
     }
     
     int getRandom() {
-        int lim = rand() % st.size();
-        unordered_set<int>::iterator it=st.begin();
-        int idx =0;
-        for (it = st.begin(); idx <lim && it != st.end(); ++it) {
-            idx++;
-        }
-        return *it;
+        // int lim = rand() % st.size();
+        // unordered_set<int>::iterator it=st.begin();
+        // int idx =0;
+        // for (it = st.begin(); idx <lim && it != st.end(); ++it) {
+        //     idx++;
+        // }
+        // return *it;
+        // for(int ele: nums)
+        //     cout<<ele<<" ";
+        // cout<<endl;
+        return nums[rand()%nums.size()];
     }
 };
 
