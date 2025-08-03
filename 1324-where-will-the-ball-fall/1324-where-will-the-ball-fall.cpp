10354a0110b9e1d470adf31068c7ed7024b4712c
Sameer <sameer.barha12@gmail.com>
2025-08-03 20:43:31 +0530

    Time: 0 ms (100%), Space: 17 MB (87.83%) - LeetHub

--- 1324-where-will-the-ball-fall/1324-where-will-the-ball-fall.cpp
+++ 1324-where-will-the-ball-fall/1324-where-will-the-ball-fall.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
         vector<int> findBall(vector<vector<int>>& grid) {
-        int R = grid.size(), C = grid[0].size();
-        vector<int> res;
-        for (int i = 0; i < C; ++i) {
-           
-            int col = i, next;
-            for (int row = 0; row < R; ++row) {
-                //cout<<row<<" "<<col<<endl;
-                next = col + grid[row][col];
 
-                if (next < 0 || next >= C || grid[row][next] != grid[row][col]) {
-                    col = -1;
-                    break;
+            //O(M*N)
+
+            int R = grid.size(), C = grid[0].size();
+            vector<int> res;
+            for (int i = 0; i < C; ++i) {
+            
+                int col = i, next;
+                for (int row = 0; row < R; ++row) {
+                    //cout<<row<<" "<<col<<endl;
+                    next = col + grid[row][col];
+
+                    if (next < 0 || next >= C || grid[row][next] != grid[row][col]) {
+                        col = -1;
+                        break;
+                    }
+                    //if the next colum has the same slope go down.
+                    col = next;
                 }
-                //if the next colum has the same slope go down.
-                col = next;
-            }
 
-           // cout<<"================="<<endl;
-            res.push_back(col);
-        }
-        return res;
+            // cout<<"================="<<endl;
+                res.push_back(col);
+            }
+            return res;
     }
 };
