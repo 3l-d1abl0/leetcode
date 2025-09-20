8b879c9e22e2ee16ad3538de5944671b2a5fdec2
Sameer <sameer.barha12@gmail.com>
2025-09-20 09:14:50 +0000

    Time: 8 ms (5.15%), Space: 33.2 MB (6.71%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -47,30 +47,23 @@ public:
         return l;
     }
     
-    int binarySearch(int k, vector<int> &nums){
+    int binarySearch(int target, vector<int> &nums){
         
         int N = nums.size();
         vector<int> prefixSum(N+1, 0);
         for(int i=1; i<=N; i++)
             prefixSum[i] = prefixSum[i-1]+nums[i-1];
         
-        /*for(int ele: prefixSum)
-            cout<<ele<<" ";
-        cout<<endl;*/
-        
         int minLen = INT_MAX;
         int prefix =0;
         for(int i=0; i<N; i++){
             
             prefix +=nums[i];
-            /*
-                prefixsum[i]-prefixsum[j-1] >= k  //i-j+1
-                prefixsum[i]-k  >= prefixsum[j-1]
-                find (greatest) num which is lesser than eq to prefixsum[i]-k
-            */
-            if(prefix>=k){
+            
+            if(prefix>=target){
                 //cout<<"i = "<<i<<" p ="<<prefixSum[i+1]<<" val= "<<prefix-k<<endl;
-                int idx = bSearch(prefixSum, prefix-k, N+1);
+                //Check if there exist position from the beg of array which sums to the exceeding sum
+                int idx = bSearch(prefixSum, prefix-target, N+1);
                 minLen = min(minLen, i-idx+1);
                 //cout<<"len= "<<minLen<<endl;
             }
