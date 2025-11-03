43592c2cb721716056633b5bfab62d4c8e635259
Sameer <sameer.barha12@gmail.com>
2025-11-03 19:44:36 +0530

    Time: 0 ms (100%), Space: 10.1 MB (74.1%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -21,19 +21,19 @@ public:
     
     bool safe(vector<string> &board, int row, int col, vector<bool> &rowCheck, vector<bool> &bottomleft, vector<bool> &upperLeft){
     
-    int N = board.size();
+        int N = board.size();
 
-    if(rowCheck[row]==true)
-        return false;
+        if(rowCheck[row]==true)
+            return false;
 
-    if(bottomleft[row+col]==true)
-        return false;
+        if(bottomleft[row+col]==true)
+            return false;
 
-    if(upperLeft[row+ (N-col)]== true)
-        return false;
+        if(upperLeft[N+row-col]== true)
+            return false;
 
-    //printBoard(board);
-    return true;
+        //printBoard(board);
+        return true;
 
     }
     
@@ -56,12 +56,12 @@ public:
                 if(safe(board, row, col, rowCheck, bottomleft, upperLeft)){
 
                     board[row][col]='Q';
-                    rowCheck[row] = true; bottomleft[row+col] = true; upperLeft[row+ (N-col)]=true;
+                    rowCheck[row] = true; bottomleft[row+col] = true; upperLeft[N+row-col]=true;
                     
                     NQueens(board, col+1, ans, rowCheck, bottomleft, upperLeft);
                     
                     board[row][col]='.';
-                    rowCheck[row] = false; bottomleft[row+col] = false; upperLeft[row+ (N-col)]=false;
+                    rowCheck[row] = false; bottomleft[row+col] = false; upperLeft[N+row-col]=false;
 
                 }
 
